c9b91ce90dd79273475af0be8bcf25522bb2e43e
Bernd Schoolmann <mail@quexten.com>
2024-12-10 13:06:29 -0800

    Replace tokio spawn with std::thread spawn

--- apps/desktop/desktop_native/proxy/src/windows.rs
+++ apps/desktop/desktop_native/proxy/src/windows.rs
@@ -6,7 +6,7 @@ use std::sync::{
 pub fn allow_foreground() -> Arc<AtomicBool> {
     let should_foreground = Arc::new(AtomicBool::new(false));
     let should_foreground_clone = should_foreground.clone();
-    let _ = tokio::task::spawn_blocking(move || loop {
+    let _ = std::thread::spawn(move || loop {
         if !should_foreground_clone.load(Ordering::Relaxed) {
             std::thread::sleep(std::time::Duration::from_millis(100));
             continue;
