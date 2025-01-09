65ea14e5013a4fded5bdac6bc7258ace21f1e567
Conner Turnbull <cturnbull@bitwarden.com>
2025-01-09 09:46:28 -0500

    resolved storybook errors

--- apps/web/src/app/vault/components/premium-badge.stories.ts
+++ apps/web/src/app/vault/components/premium-badge.stories.ts
@@ -2,9 +2,11 @@ import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
 import { of } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { BillingAccountProfileStateService } from "@bitwarden/common/billing/abstractions/account/billing-account-profile-state.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { MessageSender } from "@bitwarden/common/platform/messaging";
+import { UserId } from "@bitwarden/common/types/guid";
 import { BadgeModule, I18nMockService } from "@bitwarden/components";
 
 import { PremiumBadgeComponent } from "./premium-badge.component";
@@ -40,6 +42,17 @@ export default {
           provide: BillingAccountProfileStateService,
           useValue: {
             hasPremiumFromAnySource$: of(false),
+          },
+        },
+        {
+          provide: AccountService,
+          useValue: {
+            activeAccount$: of({
+              id: "test-user-id" as UserId,
+              name: "Test User 1",
+              email: "test@email.com",
+              emailVerified: true,
+            }),
           },
         },
       ],
