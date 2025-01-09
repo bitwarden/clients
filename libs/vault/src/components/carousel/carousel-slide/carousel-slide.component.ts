aeaf46a89cea06fa4738f931f5deb1a6f920ffc1
Nick Krantz <nick@livefront.com>
2025-01-09 10:47:04 -0600

    make carousel slide focusable when it does not contain focusable elements

--- libs/vault/src/components/carousel/carousel-slide/carousel-slide.component.ts
+++ libs/vault/src/components/carousel/carousel-slide/carousel-slide.component.ts
@@ -1,3 +1,4 @@
+import { coerceBooleanProperty } from "@angular/cdk/coercion";
 import { TemplatePortal } from "@angular/cdk/portal";
 import { Component, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from "@angular/core";
 
@@ -8,14 +9,14 @@ import { Component, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } fr
 })
 export class VaultCarouselSlideComponent implements OnInit {
   /**
-   * Optional tabIndex for the tabPanel that contains this tab's content.
+   * Should be set to true when the slide has no focusable elements.
    *
-   * If the tabpanel does not contain any focusable elements or the first element with content is not focusable,
+   * When the slide does not contain any focusable elements or the first element with content is not focusable,
    * this should be set to 0 to include it in the tab sequence of the page.
    *
    * @remarks See note 4 of https://www.w3.org/WAI/ARIA/apg/patterns/tabpanel/
    */
-  @Input() contentTabIndex: number | undefined;
+  @Input({ transform: coerceBooleanProperty }) noFocusableChildren?: true;
 
   @ViewChild(TemplateRef, { static: true }) implicitContent!: TemplateRef<unknown>;
 
--- libs/vault/src/components/carousel/carousel.stories.ts
+++ libs/vault/src/components/carousel/carousel.stories.ts
@@ -1,6 +1,6 @@
 import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
 
-import { TypographyModule } from "@bitwarden/components";
+import { ButtonComponent, TypographyModule } from "@bitwarden/components";
 
 import { VaultCarouselSlideComponent } from "./carousel-slide/carousel-slide.component";
 import { VaultCarouselComponent } from "./carousel.component";
@@ -10,7 +10,7 @@ export default {
   component: VaultCarouselComponent,
   decorators: [
     moduleMetadata({
-      imports: [VaultCarouselSlideComponent, TypographyModule],
+      imports: [VaultCarouselSlideComponent, TypographyModule, ButtonComponent],
     }),
   ],
 } as Meta;
@@ -32,6 +32,28 @@ export const Default: Story = {
           <div class="tw-flex tw-flex-col tw-items-center tw-gap-4">
             <h2 bitTypography="h2">Second Carousel Heading</h2>
             <p bitTypography="body1">Second Carousel Content</p>
+          </div>
+        </vault-carousel-slide>
+      </vault-carousel>
+    `,
+  }),
+};
+
+export const KeyboardNavigation: Story = {
+  render: (args: any) => ({
+    props: args,
+    template: `
+      <vault-carousel label="Storybook Demo">
+        <vault-carousel-slide>
+          <div class="tw-flex tw-flex-col tw-items-center tw-gap-4">
+            <h2 bitTypography="h2">First Carousel Heading</h2>
+            <button bitButton buttonType="primary">Button</button>
+          </div>
+        </vault-carousel-slide>
+        <vault-carousel-slide noFocusableChildren>
+          <div class="tw-flex tw-flex-col tw-items-center tw-gap-4">
+            <h2 bitTypography="h2">Second Carousel Heading</h2>
+            <p bitTypography="body1">With no focusable elements, the entire slide should be focusable</p>
           </div>
         </vault-carousel-slide>
       </vault-carousel>
