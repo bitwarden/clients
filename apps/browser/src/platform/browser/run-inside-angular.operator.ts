9ac590350882cf11007072cb450cb5112dd8ff0e
Matt Gibson <mgibson@bitwarden.com>
2025-01-09 10:03:11 -0800

    Eagerly load Desktop renderer SDK
    Note: If the main process ever requires an SDK, we'll need to load it there, too.
    In that event, it's probably a good idea to move to IPC for all SDK functions to avoid
    loading the SDK for every window.

--- apps/browser/src/platform/browser/run-inside-angular.operator.ts
+++ apps/browser/src/platform/browser/run-inside-angular.operator.ts
@@ -19,3 +19,55 @@ export function runInsideAngular<T>(ngZone: NgZone): MonoTypeOperatorFunction<T>
       return () => subscription.unsubscribe();
     });
 }
+
+// function now() {
+//   const userId = "userId" as UserId;
+//   const otherService: { someSetting: Observable<any> } = null;
+//   const sdkService: DefaultSdkService = null;
+
+//   combineLatest([otherService.someSetting, sdkService.userClient$(userId)]).subscribe(
+//     ([someSetting, sdkClient]) => {
+//       using client = sdkClient.take();
+//       if (someSetting) {
+//         client.value.echo("some setting");
+//       } else {
+//         client.value.echo("no setting");
+//       }
+//     },
+//   );
+// }
+
+// function callback() {
+//   const userId = "userId" as UserId;
+//   const otherService: { someSetting: Observable<any> } = null;
+//   const sdkService: DefaultSdkService = null;
+
+//   someSetting.pipe(
+//     switchMap((someSettings) =>
+//       sdkService.userClientWithCallback$(userId, (client) => {
+//         // take done in userClient With Callback while calling this callback
+//         if (someSettings) {
+//           client.echo("some setting");
+//         } else {
+//           client.echo("no setting");
+//         }
+//       }),
+//     ),
+//   );
+// }
+
+// function operator() {
+//   const userId = "userId" as UserId;
+//   const otherService: { someSetting: Observable<any> } = null;
+//   const sdkService: DefaultSdkService = null;
+
+//   someSetting.pipe(
+//     sdkService.withUserClient, // this transforms the stream into [value, client] tuple while ensuring the client it taken
+//   ).subscribe(([someSetting, client]) => {
+//     if (someSetting) {
+//       client.echo("some setting");
+//     } else {
+//       client.echo("no setting");
+//     }
+//   });
+// }
