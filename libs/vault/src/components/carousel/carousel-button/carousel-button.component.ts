9df30b5b42d9894ffb906656022186ad1936daa4
Nick Krantz <nick@livefront.com>
2025-01-10 10:51:04 -0600

    update interactive styles for the carousel icons

--- libs/vault/src/components/carousel/carousel-button/carousel-button.component.ts
+++ libs/vault/src/components/carousel/carousel-button/carousel-button.component.ts
@@ -1,26 +1,24 @@
 import { FocusableOption } from "@angular/cdk/a11y";
-import { NgIf } from "@angular/common";
+import { CommonModule } from "@angular/common";
 import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from "@angular/core";
 
 import { BitIconComponent } from "@bitwarden/components/src/icon/icon.component";
 
-import { ActiveCarouselIcon } from "../carousel-icons/active-carousel";
-import { InactiveCarouselIcon } from "../carousel-icons/inactive-carousel";
+import { CarouselIcon } from "../carousel-icons/carousel-icon";
 import { VaultCarouselSlideComponent } from "../carousel-slide/carousel-slide.component";
 
 @Component({
   selector: "vault-carousel-button",
   templateUrl: "carousel-button.component.html",
   standalone: true,
-  imports: [NgIf, BitIconComponent],
+  imports: [CommonModule, BitIconComponent],
 })
 export class VaultCarouselButtonComponent implements FocusableOption {
   /** Slide component that is associated with the individual button */
   @Input({ required: true }) slide!: VaultCarouselSlideComponent;
 
   @ViewChild("btn", { static: true }) button!: ElementRef<HTMLButtonElement>;
-  protected ActiveCarouselIcon = ActiveCarouselIcon;
-  protected InactiveCarouselIcon = InactiveCarouselIcon;
+  protected CarouselIcon = CarouselIcon;
 
   /** When set to true the button is shown in an active state. */
   @Input({ required: true }) isActive!: boolean;
@@ -31,5 +29,13 @@ export class VaultCarouselButtonComponent implements FocusableOption {
   /** Focuses the underlying button element. */
   focus(): void {
     this.button.nativeElement.focus();
+  }
+
+  protected get dynamicClasses() {
+    const activeClasses = ["[&_rect]:tw-fill-primary-600", "[&_rect]:tw-stroke-primary-600"];
+
+    const inactiveClasses = ["[&_rect]:tw-stroke-text-muted", "[&_rect]:hover:tw-fill-text-muted"];
+
+    return this.isActive ? activeClasses : inactiveClasses;
   }
 }
--- libs/vault/src/components/carousel/carousel-icons/active-carousel.ts
+++
@@ -1,7 +0,0 @@
-import { svgIcon } from "@bitwarden/components";
-
-export const ActiveCarouselIcon = svgIcon`
-  <svg class="tw-block" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" data-testid="active-carousel-icon">
-    <rect class="tw-fill-primary-600" x="0" width="12" height="12" rx="6" />
-  </svg>
-`;
