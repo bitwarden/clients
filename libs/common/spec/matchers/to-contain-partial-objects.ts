61f02553b3301a61115e8aa8d9633aa0374905bc
Thomas Rittson <trittson@bitwarden.com>
2025-01-13 13:38:50 +1000

    Provide more granular error messages for negative test case

--- libs/common/spec/matchers/to-contain-partial-objects.ts
+++ libs/common/spec/matchers/to-contain-partial-objects.ts
@@ -1,16 +1,31 @@
+import { EOL } from "os";
+
 import { diff } from "jest-diff";
 
 export const toContainPartialObjects: jest.CustomMatcher = function (
   received: Array<any>,
   expected: Array<any>,
 ) {
-  const pass = this.equals(
+  const matched = this.equals(
     received,
     expect.arrayContaining(expected.map((e) => expect.objectContaining(e))),
   );
 
+  if (matched) {
+    return {
+      message: () =>
+        "Expected the received array to NOT include partial matches for all expected objects." +
+        EOL +
+        diff(expected, received),
+      pass: true,
+    };
+  }
+
   return {
-    message: () => diff(expected, received) as string,
-    pass: pass,
+    message: () =>
+      "Expected the received array to contain partial matches for all expected objects." +
+      EOL +
+      diff(expected, received),
+    pass: false,
   };
 };
