5cbe0660e039ea8b598c12160feb3c71abd1e6b4
Matt Gibson <mgibson@bitwarden.com>
2025-01-13 08:38:04 -0800

    Fix type imports

--- apps/browser/src/platform/services/sdk/fallback.ts
+++ apps/browser/src/platform/services/sdk/fallback.ts
@@ -1,7 +1,7 @@
 import * as sdk from "@bitwarden/sdk-internal";
 import * as wasm from "@bitwarden/sdk-internal/bitwarden_wasm_internal_bg.wasm.js";
 
-import { GlobalWithWasmInit } from "src/platform/services/sdk/browser-sdk-load.service";
+import { GlobalWithWasmInit } from "./browser-sdk-load.service";
 
 (globalThis as GlobalWithWasmInit).initSdk = () => {
   (sdk as any).init(wasm);
