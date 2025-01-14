491f29a6aed2ec0c315f6047e0a2050a0761d3fc
Alec Rippberger <alec@livefront.com>
2025-01-14 12:48:56 -0600

    Revert "Add toast message on session timeout"
    This reverts commit 9a8c1434f89ab3a3d02bb1d29a42a96ed04ed503.

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -458,7 +458,6 @@ const safeProviders: SafeProvider[] = [
       VaultTimeoutSettingsServiceAbstraction,
       KdfConfigService,
       TaskSchedulerService,
-      ToastService,
     ],
   }),
   safeProvider({
--- libs/auth/src/common/services/login-strategies/login-strategy.service.ts
+++ libs/auth/src/common/services/login-strategies/login-strategy.service.ts
@@ -36,7 +36,6 @@ import { TaskSchedulerService, ScheduledTaskNames } from "@bitwarden/common/plat
 import { GlobalState, GlobalStateProvider } from "@bitwarden/common/platform/state";
 import { PasswordStrengthServiceAbstraction } from "@bitwarden/common/tools/password-strength";
 import { MasterKey } from "@bitwarden/common/types/key";
-import { ToastService } from "@bitwarden/components";
 import {
   KdfType,
   KeyService,
@@ -122,7 +121,6 @@ export class LoginStrategyService implements LoginStrategyServiceAbstraction {
     protected vaultTimeoutSettingsService: VaultTimeoutSettingsService,
     protected kdfConfigService: KdfConfigService,
     protected taskSchedulerService: TaskSchedulerService,
-    protected toastService: ToastService,
   ) {
     this.currentAuthnTypeState = this.stateProvider.get(CURRENT_LOGIN_STRATEGY_KEY);
     this.loginStrategyCacheState = this.stateProvider.get(CACHE_KEY);
@@ -272,11 +270,6 @@ export class LoginStrategyService implements LoginStrategyServiceAbstraction {
    */
   async logInNewDeviceVerification(deviceVerificationOtp: string): Promise<AuthResult> {
     if (!(await this.isSessionValid())) {
-      this.toastService.showToast({
-        variant: "error",
-        message: this.i18nService.t("sessionTimeout"),
-        title: "",
-      });
       throw new Error(this.i18nService.t("sessionTimeout"));
     }
 
