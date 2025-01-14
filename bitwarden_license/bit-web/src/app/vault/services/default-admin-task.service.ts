843a911c97b89035266da54f772863e34520d601
gbubemismith <gsmithwalter@gmail.com>
2025-01-14 17:50:50 -0500

    Implemented get all api

--- bitwarden_license/bit-web/src/app/vault/services/default-admin-task.service.ts
+++ bitwarden_license/bit-web/src/app/vault/services/default-admin-task.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@angular/core";
 
 import { ApiService } from "@bitwarden/common/src/abstractions/api.service";
+import { ListResponse } from "@bitwarden/common/src/models/response/list.response";
 import { OrganizationId } from "@bitwarden/common/src/types/guid";
 import { SecurityTaskStatus } from "@bitwarden/vault/src/tasks/enums";
 import { SecurityTask } from "@bitwarden/vault/src/tasks/models";
+import { SecurityTaskData } from "@bitwarden/vault/src/tasks/models/security-task.data";
+import { SecurityTaskResponse } from "@bitwarden/vault/src/tasks/models/security-task.response";
 
 import { AdminTaskService, CreateTasksRequest } from "./abstractions/admin-task.abstraction";
 
@@ -11,11 +14,27 @@ import { AdminTaskService, CreateTasksRequest } from "./abstractions/admin-task.
 export class DefaultAdminTaskService implements AdminTaskService {
   constructor(private apiService: ApiService) {}
 
-  getAllTasks(
+  async getAllTasks(
     organizationId: OrganizationId,
     status?: SecurityTaskStatus | undefined,
   ): Promise<SecurityTask[]> {
-    throw new Error("Method not implemented.");
+    const queryParams = new URLSearchParams();
+
+    queryParams.append("organizationId", organizationId);
+    if (status) {
+      queryParams.append("status", status.toString());
+    }
+
+    const r = await this.apiService.send(
+      "GET",
+      `/tasks/organization?${queryParams.toString()}`,
+      null,
+      true,
+      true,
+    );
+    const response = new ListResponse(r, SecurityTaskResponse);
+
+    return response.data.map((d) => new SecurityTask(new SecurityTaskData(d)));
   }
 
   async bulkCreateTasks(
