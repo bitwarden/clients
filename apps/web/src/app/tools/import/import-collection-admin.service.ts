be1ca83ed7b19894b3026caa1fd4ca6715be057d
Daniel James Smith <djsmith85@users.noreply.github.com>
2025-01-17 18:04:26 +0100

    Fix import path within ImportCollectionAdminService

--- apps/web/src/app/tools/import/import-collection-admin.service.ts
+++ apps/web/src/app/tools/import/import-collection-admin.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from "@angular/core";
 
 import { CollectionAdminService, CollectionAdminView } from "@bitwarden/admin-console/common";
-
-// FIXME: remove `src` and fix import
-// eslint-disable-next-line no-restricted-imports
-import { ImportCollectionServiceAbstraction } from "../../../../../../libs/importer/src/services/import-collection.service.abstraction";
+import { ImportCollectionServiceAbstraction } from "@bitwarden/importer/core";
 
 @Injectable()
 export class ImportCollectionAdminService implements ImportCollectionServiceAbstraction {
