2a3d53fab57f5381fc9b3ecaa270d974c56c92b1
voommen-livefront <voommen@livefront.com>
2025-01-22 13:59:15 -0600

    PM-15090 removed error in build

--- bitwarden_license/bit-common/src/tools/reports/risk-insights/services/index.ts
+++ bitwarden_license/bit-common/src/tools/reports/risk-insights/services/index.ts
@@ -1,6 +1,5 @@
 export * from "./member-cipher-details-api.service";
 export * from "./password-health.service";
-export * from "./unmark-critical-application-api.service";
 export * from "./critical-apps.service";
 export * from "./critical-apps-api.service";
 export * from "./risk-insights-report.service";
