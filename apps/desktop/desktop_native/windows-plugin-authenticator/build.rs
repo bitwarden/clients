937afca789fdd770c3e6e02bfa2645abac7279df
Colton Hurst <colton@coltonhurst.com>
2025-01-31 10:06:02 -0500

    PM-17619: Remove todo

--- apps/desktop/desktop_native/windows-plugin-authenticator/build.rs
+++ apps/desktop/desktop_native/windows-plugin-authenticator/build.rs
@@ -8,7 +8,6 @@ fn main() {
 fn windows() {
     let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR not set");
 
-    // TODO: Use .allowlist_x() to specify needed items
     let bindings = bindgen::Builder::default()
         .header("pluginauthenticator.hpp")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
