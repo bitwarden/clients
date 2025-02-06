deb70494d27abd5ff18d8ddbb4719557396e4980
✨ Audrey ✨ <audrey@audreyality.com>
2025-02-06 09:26:53 -0500

    remove settings from barrel file

--- libs/common/src/tools/log/index.ts
+++ libs/common/src/tools/log/index.ts
@@ -1,3 +1,2 @@
 export { disabledSemanticLoggerProvider, consoleSemanticLoggerProvider } from "./factory";
-export { SemanticLoggerSettings } from "./semantic-logger-settings";
 export { SemanticLogger } from "./semantic-logger.abstraction";
