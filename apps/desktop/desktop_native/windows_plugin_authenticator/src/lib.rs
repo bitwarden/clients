1eae033109a4a25ce923d4e2e26334be054fe04f
Colton Hurst <colton@coltonhurst.com>
2025-02-07 12:17:37 -0500

    PM-9126: Add unsafe tests

--- apps/desktop/desktop_native/windows_plugin_authenticator/src/lib.rs
+++ apps/desktop/desktop_native/windows_plugin_authenticator/src/lib.rs
@@ -9,16 +9,45 @@ use pa::{
     EXPERIMENTAL_PCWEBAUTHN_PLUGIN_OPERATION_REQUEST,
     EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
 };
+use std::ptr;
 use windows_core::*;
-use windows_sys::Win32::System::Com::CoInitialize;
+//use windows_sys::{core::GUID, Win32::System::Com::{CoInitialize, CoInitializeSecurity, CoRegisterClassObject, IClassFactory, EOAC_NONE, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE}};
+use windows::Win32::Foundation::*;
+use windows::Win32::System::Com::*;
 
 pub fn register() -> i32 {
-    // let com_object: PACOMObject;
-    // let com_object_ptr: *const PACOMObject = &com_object;
+    let com_object: PACOMObject;
 
     // let result: HRESULT = unsafe {
     //     HRESULT(CoInitialize(com_object_ptr))
     // };
+
+    // https://github.com/microsoft/Windows-classic-samples/blob/main/Samples/PasskeyManager/cpp/App.xaml.cpp
+    // https://learn.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-coinitializesecurity
+    unsafe {
+        CoInitializeSecurity(None, -1, None, None, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, None, EOAC_NONE, None)
+    }.unwrap();
+
+    // https://github.com/microsoft/Windows-classic-samples/blob/main/Samples/PasskeyManager/cpp/App.xaml.cpp
+    // https://learn.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-coregisterclassobject
+    // Random GUID: a98925d1-61f6-40de-9327-dc418fcb2ff4
+    // let random_plugin_guid_ptr: *const GUID = &GUID {
+    //     data1: 0xa98925d1 as u32,
+    //     data2: 0x61f6 as u16,
+    //     data3: 0x40de as u16,
+    //     data4: [0x93, 0x27, 0xdc, 0x41, 0x8f, 0xcb, 0x2f, 0xf4],
+    // };
+    // let random_plugin_guid_ptr: *const GUID = &GUID::from_u128(0xa98925d161f640de9327dc418fcb2ff4);
+    // let register_object_result: HRESULT = unsafe {
+    //     CoRegisterClassObject(random_plugin_guid_ptr, ))
+    // };
+
+    let random_plugin_guid_ptr: *const GUID = &GUID::from_u128(0xa98925d161f640de9327dc418fcb2ff4);
+    let mut f = Factory();
+    let mut f_ptr = &mut f as *mut _ as *mut core::ffi::c_void;
+    let result: u32 = unsafe {
+        CoRegisterClassObject(random_plugin_guid_ptr, &IUnknown::from_raw(f_ptr), CLSCTX_LOCAL_SERVER, REGCLS_MULTIPLEUSE)
+    }.unwrap();
 
     8
 }
@@ -36,12 +65,12 @@ unsafe trait EXPERIMENTAL_IPluginAuthenticator: IUnknown {
     fn EXPERIMENTAL_PluginMakeCredential(
         &self,
         request: EXPERIMENTAL_PCWEBAUTHN_PLUGIN_OPERATION_REQUEST,
-        response: &mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
+        response: *mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
     ) -> HRESULT;
     fn EXPERIMENTAL_PluginGetAssertion(
         &self,
         request: EXPERIMENTAL_PCWEBAUTHN_PLUGIN_OPERATION_REQUEST,
-        response: &mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
+        response: *mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
     ) -> HRESULT;
     fn EXPERIMENTAL_PluginCancelOperation(
         &self,
@@ -56,7 +85,7 @@ impl EXPERIMENTAL_IPluginAuthenticator_Impl for PACOMObject_Impl {
     unsafe fn EXPERIMENTAL_PluginMakeCredential(
         &self,
         request: EXPERIMENTAL_PCWEBAUTHN_PLUGIN_OPERATION_REQUEST,
-        response: &mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
+        response: *mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
     ) -> HRESULT {
         HRESULT(get_test_number())
     }
@@ -64,7 +93,7 @@ impl EXPERIMENTAL_IPluginAuthenticator_Impl for PACOMObject_Impl {
     unsafe fn EXPERIMENTAL_PluginGetAssertion(
         &self,
         request: EXPERIMENTAL_PCWEBAUTHN_PLUGIN_OPERATION_REQUEST,
-        response: &mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
+        response: *mut EXPERIMENTAL_PWEBAUTHN_PLUGIN_OPERATION_RESPONSE,
     ) -> HRESULT {
         HRESULT(get_test_number())
     }
@@ -74,5 +103,26 @@ impl EXPERIMENTAL_IPluginAuthenticator_Impl for PACOMObject_Impl {
         request: EXPERIMENTAL_PCWEBAUTHN_PLUGIN_CANCEL_OPERATION_REQUEST,
     ) -> HRESULT {
         HRESULT(get_test_number())
+    }
+}
+
+#[implement(IClassFactory)]
+struct Factory();
+
+impl IClassFactory_Impl for Factory_Impl {
+    fn CreateInstance(
+        &self,
+        outer: Ref<IUnknown>,
+        iid: *const GUID,
+        object: *mut *mut core::ffi::c_void,
+    ) -> Result<()> {
+        assert!(outer.is_null());
+        let unknown: IInspectable = PACOMObject.into();
+        unsafe { unknown.query(iid, object).ok() }
+    }
+
+    fn LockServer(&self, lock: BOOL) -> Result<()> {
+        assert!(lock.as_bool());
+        Ok(())
     }
 }
