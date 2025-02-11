b12f2b8da672553b24d483ad2efc56ed91442193
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-02-11 11:22:11 -0500

    Add way to convert TaskSchedulerService to RX Scheduler

--- apps/browser/src/platform/services/popup-view-cache-background.service.ts
+++ apps/browser/src/platform/services/popup-view-cache-background.service.ts
@@ -1,8 +1,10 @@
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
-import { switchMap, merge, delay, filter, concatMap, map, first, of } from "rxjs";
+import { switchMap, merge, delay, filter, concatMap, map, first, of, Subscription } from "rxjs";
 
 import { CommandDefinition, MessageListener } from "@bitwarden/common/platform/messaging";
+import { ScheduledTaskNames, TaskSchedulerService } from "@bitwarden/common/platform/scheduling";
+import { toScheduler } from "@bitwarden/common/platform/scheduling/task-scheduler.service";
 import {
   POPUP_VIEW_MEMORY,
   KeyDefinition,
@@ -45,53 +47,71 @@ export class PopupViewCacheBackgroundService {
   constructor(
     private messageListener: MessageListener,
     private globalStateProvider: GlobalStateProvider,
+    private readonly taskSchedulerService: TaskSchedulerService,
   ) {}
 
   startObservingTabChanges() {
-    this.messageListener
-      .messages$(SAVE_VIEW_CACHE_COMMAND)
-      .pipe(
-        concatMap(async ({ key, value }) =>
-          this.popupViewCacheState.update((state) => ({
-            ...state,
-            [key]: value,
-          })),
+    const combinedSubscription = new Subscription();
+
+    combinedSubscription.add(
+      this.messageListener
+        .messages$(SAVE_VIEW_CACHE_COMMAND)
+        .pipe(
+          concatMap(async ({ key, value }) =>
+            this.popupViewCacheState.update((state) => ({
+              ...state,
+              [key]: value,
+            })),
+          ),
+        )
+        .subscribe(),
+    );
+
+    combinedSubscription.add(
+      this.messageListener
+        .messages$(ClEAR_VIEW_CACHE_COMMAND)
+        .pipe(concatMap(() => this.popupViewCacheState.update(() => null)))
+        .subscribe(),
+    );
+
+    combinedSubscription.add(
+      merge(
+        // on tab changed, excluding extension tabs
+        fromChromeEvent(chrome.tabs.onActivated).pipe(
+          switchMap((tabs) => BrowserApi.getTab(tabs[0].tabId)),
+          switchMap((tab) => {
+            // FireFox sets the `url` to "about:blank" and won't populate the `url` until the `onUpdated` event
+            if (tab.url !== "about:blank") {
+              return of(tab);
+            }
+
+            return fromChromeEvent(chrome.tabs.onUpdated).pipe(
+              first(),
+              switchMap(([tabId]) => BrowserApi.getTab(tabId)),
+            );
+          }),
+          map((tab) => tab.url || tab.pendingUrl),
+          filter((url) => !url.startsWith(chrome.runtime.getURL(""))),
+        ),
+
+        // on popup closed, with 2 minute delay that is cancelled by re-opening the popup
+        fromChromeEvent(chrome.runtime.onConnect).pipe(
+          filter(([port]) => port.name === popupClosedPortName),
+          switchMap(([port]) =>
+            fromChromeEvent(port.onDisconnect).pipe(
+              delay(
+                1000 * 60 * 2,
+                toScheduler(this.taskSchedulerService, ScheduledTaskNames.clearPopupViewCache),
+              ),
+            ),
+          ),
         ),
       )
-      .subscribe();
+        .pipe(switchMap(() => this.clearState()))
+        .subscribe(),
+    );
 
-    this.messageListener
-      .messages$(ClEAR_VIEW_CACHE_COMMAND)
-      .pipe(concatMap(() => this.popupViewCacheState.update(() => null)))
-      .subscribe();
-
-    merge(
-      // on tab changed, excluding extension tabs
-      fromChromeEvent(chrome.tabs.onActivated).pipe(
-        switchMap((tabs) => BrowserApi.getTab(tabs[0].tabId)),
-        switchMap((tab) => {
-          // FireFox sets the `url` to "about:blank" and won't populate the `url` until the `onUpdated` event
-          if (tab.url !== "about:blank") {
-            return of(tab);
-          }
-
-          return fromChromeEvent(chrome.tabs.onUpdated).pipe(
-            first(),
-            switchMap(([tabId]) => BrowserApi.getTab(tabId)),
-          );
-        }),
-        map((tab) => tab.url || tab.pendingUrl),
-        filter((url) => !url.startsWith(chrome.runtime.getURL(""))),
-      ),
-
-      // on popup closed, with 2 minute delay that is cancelled by re-opening the popup
-      fromChromeEvent(chrome.runtime.onConnect).pipe(
-        filter(([port]) => port.name === popupClosedPortName),
-        switchMap(([port]) => fromChromeEvent(port.onDisconnect).pipe(delay(1000 * 60 * 2))),
-      ),
-    )
-      .pipe(switchMap(() => this.clearState()))
-      .subscribe();
+    return combinedSubscription;
   }
 
   async clearState() {
--- libs/common/src/platform/scheduling/task-scheduler.service.ts
+++ libs/common/src/platform/scheduling/task-scheduler.service.ts
@@ -1,8 +1,37 @@
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
-import { Subscription } from "rxjs";
+import { asyncScheduler, SchedulerLike, Subscription } from "rxjs";
 
 import { ScheduledTaskName } from "./scheduled-task-name.enum";
+
+/**
+ *
+ * @param taskScheduler
+ * @param taskName
+ * @returns
+ */
+export function toScheduler(
+  taskScheduler: TaskSchedulerService,
+  taskName: ScheduledTaskName,
+): SchedulerLike {
+  return new TaskSchedulerSheduler(taskScheduler, taskName);
+}
+
+class TaskSchedulerSheduler implements SchedulerLike {
+  constructor(
+    private readonly taskSchedulerService: TaskSchedulerService,
+    private readonly taskName: ScheduledTaskName,
+  ) {}
+
+  schedule<T>(work: (state?: T) => void, delay?: number, state?: T): Subscription {
+    this.taskSchedulerService.registerTaskHandler(this.taskName, () => work(state));
+    return this.taskSchedulerService.setTimeout(this.taskName, delay ?? 0);
+  }
+
+  now(): number {
+    return asyncScheduler.now();
+  }
+}
 
 export abstract class TaskSchedulerService {
   protected taskHandlers: Map<string, () => void>;
