f28be5c6a76ac4cbb6516fbfe8986bc24bf96eea
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-02-12 15:42:42 +0100

    chore: remove unused ping message

--- libs/common/src/platform/ipc/index.ts
+++ libs/common/src/platform/ipc/index.ts
@@ -1,3 +1,2 @@
 export * from "./ipc-message";
 export * from "./ipc.service";
-export * from "./ping.message";
--- libs/common/src/platform/ipc/ping.message.ts
+++
@@ -1,2 +0,0 @@
-export const PingMessagePayload = new Uint8Array([90, 1]);
-export const PongMessagePayload = new Uint8Array([90, 2]);
