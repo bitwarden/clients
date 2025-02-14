b87887bb8eecc4d4ffbe70181bd74ed0a88b4a1c
Alec Rippberger <alec@livefront.com>
2025-02-14 13:33:47 -0600

    Log user in and redirect with valid recovery code

--- apps/web/src/app/auth/recover-two-factor.component.ts
+++ apps/web/src/app/auth/recover-two-factor.component.ts
@@ -1,12 +1,14 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
-import { LoginStrategyServiceAbstraction } from "@bitwarden/auth/common";
+import { LoginStrategyServiceAbstraction, PasswordLoginCredentials } from "@bitwarden/auth/common";
 import { ApiService } from "@bitwarden/common/abstractions/api.service";
+import { TwoFactorProviderType } from "@bitwarden/common/auth/enums/two-factor-provider-type";
+import { TokenTwoFactorRequest } from "@bitwarden/common/auth/models/request/identity-token/token-two-factor.request";
 import { TwoFactorRecoveryRequest } from "@bitwarden/common/auth/models/request/two-factor-recovery.request";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { ToastService } from "@bitwarden/components";
@@ -16,12 +18,22 @@ import { KeyService } from "@bitwarden/key-management";
   selector: "app-recover-two-factor",
   templateUrl: "recover-two-factor.component.html",
 })
-export class RecoverTwoFactorComponent {
+export class RecoverTwoFactorComponent implements OnInit {
   protected formGroup = new FormGroup({
     email: new FormControl(null, [Validators.required]),
     masterPassword: new FormControl(null, [Validators.required]),
     recoveryCode: new FormControl(null, [Validators.required]),
   });
+
+  /**
+   * Message to display to the user about the recovery code
+   */
+  recoveryCodeMessage: string;
+
+  /**
+   * Whether the recovery code login feature flag is enabled
+   */
+  private recoveryCodeLoginFeatureFlagEnabled: boolean;
 
   constructor(
     private router: Router,
@@ -31,7 +43,17 @@ export class RecoverTwoFactorComponent {
     private keyService: KeyService,
     private loginStrategyService: LoginStrategyServiceAbstraction,
     private toastService: ToastService,
+    private configService: ConfigService,
   ) {}
+
+  async ngOnInit() {
+    this.recoveryCodeLoginFeatureFlagEnabled = await this.configService.getFeatureFlag(
+      FeatureFlag.RecoveryCodeLogin,
+    );
+    this.recoveryCodeMessage = this.recoveryCodeLoginFeatureFlagEnabled
+      ? this.i18nService.t("logInBelowUsingYourSingleUseRecoveryCode")
+      : this.i18nService.t("recoverAccountTwoStepDesc");
+  }
 
   get email(): string {
     return this.formGroup.value.email;
@@ -56,12 +78,69 @@ export class RecoverTwoFactorComponent {
     request.email = this.email.trim().toLowerCase();
     const key = await this.loginStrategyService.makePreloginKey(this.masterPassword, request.email);
     request.masterPasswordHash = await this.keyService.hashMasterKey(this.masterPassword, key);
-    await this.apiService.postTwoFactorRecover(request);
-    this.toastService.showToast({
-      variant: "success",
-      title: null,
-      message: this.i18nService.t("twoStepRecoverDisabled"),
-    });
-    await this.router.navigate(["/"]);
+
+    try {
+      await this.apiService.postTwoFactorRecover(request);
+      this.toastService.showToast({
+        variant: "success",
+        title: null,
+        message: this.i18nService.t("twoStepRecoverDisabled"),
+      });
+
+      if (!this.recoveryCodeLoginFeatureFlagEnabled) {
+        await this.router.navigate(["/"]);
+        return;
+      }
+
+      const twoFactorRequest: TokenTwoFactorRequest = {
+        provider: TwoFactorProviderType.RecoveryCode,
+        token: request.recoveryCode,
+        remember: false,
+      };
+
+      const credentials = new PasswordLoginCredentials(
+        request.email,
+        this.masterPassword,
+        null,
+        twoFactorRequest,
+      );
+
+      try {
+        await this.loginStrategyService.logIn(credentials);
+        this.toastService.showToast({
+          variant: "success",
+          title: null,
+          message: this.i18nService.t("youHaveBeenLoggedIn"),
+        });
+        await this.router.navigate(["/settings/security/two-factor"]);
+      } catch (e) {
+        this.toastService.showToast({
+          variant: "error",
+          title: this.i18nService.t("unexpectedError"),
+          message: e?.message,
+        });
+        // Redirect to login page if there are errors
+        await this.router.navigate(["/login"], { queryParams: { email: request.email } });
+      }
+    } catch (e) {
+      // Extract an error message from validationErrors
+      let errorMessage: string = this.i18nService.t("unexpectedError");
+      if (e?.validationErrors && typeof e.validationErrors === "object") {
+        errorMessage = Object.keys(e.validationErrors)
+          .map((key) => {
+            const messages = e.validationErrors[key];
+            return Array.isArray(messages) ? messages.join(" ") : messages;
+          })
+          .join(" ");
+      } else {
+        errorMessage = e?.message;
+      }
+
+      this.toastService.showToast({
+        variant: "error",
+        title: this.i18nService.t("error"),
+        message: errorMessage,
+      });
+    }
   };
 }
