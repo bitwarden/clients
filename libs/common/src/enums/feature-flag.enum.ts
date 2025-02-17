fe7754d521a52b7e3a241e7219177a45e5e8aa41
Daniel James Smith <djsmith85@users.noreply.github.com>
2025-02-17 15:34:18 +0100

    Remove extension-refresh feature flag from clients

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -29,7 +29,6 @@ export enum FeatureFlag {
   EnableRiskInsightsNotifications = "enable-risk-insights-notifications",
 
   AC1795_UpdatedSubscriptionStatusSection = "AC-1795_updated-subscription-status-section",
-  ExtensionRefresh = "extension-refresh",
   PersistPopupView = "persist-popup-view",
   PM4154_BulkEncryptionService = "PM-4154-bulk-encryption-service",
   TwoFactorComponentRefactor = "two-factor-component-refactor",
@@ -89,7 +88,6 @@ export const DefaultFeatureFlagValue = {
   [FeatureFlag.EnableRiskInsightsNotifications]: FALSE,
 
   [FeatureFlag.AC1795_UpdatedSubscriptionStatusSection]: FALSE,
-  [FeatureFlag.ExtensionRefresh]: FALSE,
   [FeatureFlag.PersistPopupView]: FALSE,
   [FeatureFlag.PM4154_BulkEncryptionService]: FALSE,
   [FeatureFlag.TwoFactorComponentRefactor]: FALSE,
