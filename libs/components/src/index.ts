4a05f0ca203b15bcf9edb49f564e5d4b75a337f3
jaasen-livefront <jaasen@livefront.com>
2025-02-19 13:25:22 -0800

    revert changes to index and tw-theme

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -35,7 +35,6 @@ export * from "./search";
 export * from "./section";
 export * from "./select";
 export * from "./shared/compact-mode.service";
-export * from "./shared";
 export * from "./table";
 export * from "./tabs";
 export * from "./toast";
