f338d0ca59796e69c516018c422f327f034d4b72
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-02-24 16:25:41 -0800

    add methods to abstract and implementation

--- libs/common/src/auth/abstractions/master-password-api.service.abstraction.ts
+++ libs/common/src/auth/abstractions/master-password-api.service.abstraction.ts
@@ -1,1 +1,13 @@
-export abstract class MasterPasswordApiService {}
+import { PasswordRequest } from "../models/request/password.request";
+import { SetPasswordRequest } from "../models/request/set-password.request";
+import { UpdateTdeOffboardingPasswordRequest } from "../models/request/update-tde-offboarding-password.request";
+import { UpdateTempPasswordRequest } from "../models/request/update-temp-password.request";
+
+export abstract class MasterPasswordApiService {
+  abstract setPassword: (request: SetPasswordRequest) => Promise<any>;
+  abstract postPassword: (request: PasswordRequest) => Promise<any>;
+  abstract putUpdateTempPassword: (request: UpdateTempPasswordRequest) => Promise<any>;
+  abstract putUpdateTdeOffboardingPassword: (
+    request: UpdateTdeOffboardingPasswordRequest,
+  ) => Promise<any>;
+}
