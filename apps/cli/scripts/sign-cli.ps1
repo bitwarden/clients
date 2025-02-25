d77346cfc75b07e0d7b70f817a77564fea3f877f
Andy Pixley <3723676+pixman20@users.noreply.github.com>
2025-02-25 10:45:59 -0500

    Testing windows signing script

--- apps/cli/scripts/sign-cli.ps1
+++ apps/cli/scripts/sign-cli.ps1
@@ -1,1 +1,35 @@
-azuresigntool sign -v $ENV:SIGNING_VAULT_URL -kvi $ENV:SIGNING_CLIENT_ID -kvt $ENV:SIGNING_TENANT_ID -kvs $ENV:SIGNING_CLIENT_SECRET -kvc $ENV:SIGNING_CERT_NAME -tr http://timestamp.digicert.com $ENV:EXE_PATH
+
+
+function SignExe {
+        param (
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({![string]::IsNullOrEmpty($_)})]
+        [string]$vaultUrl,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateScript({![string]::IsNullOrEmpty($_)})]
+        [string]$clientId,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateScript({![string]::IsNullOrEmpty($_)})]
+        [string]$tenantId,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateScript({![string]::IsNullOrEmpty($_)})]
+        [string]$clientSecret,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateScript({![string]::IsNullOrEmpty($_)})]
+        [string]$certName,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateScript({Test-Path $_})]
+        [string] $exePath
+    )
+
+    echo "Signing $exePath ..."
+    azuresigntool sign -v $vaultUrl -kvi $clientId -kvt $tenantId -kvs $clientSecret -kvc $certName -tr http://timestamp.digicert.com $exePath
+}
+
+
+SignExe -vaultUrl $env:SIGNING_VAULT_URL -clientId $env:SIGNING_CLIENT_ID -tenantId $env:SIGNING_TENANT_ID -clientSecret $env:SIGNING_CLIENT_SECRET -certName $env:SIGNING_CERT_NAME -exePath $env:EXE_PATH
