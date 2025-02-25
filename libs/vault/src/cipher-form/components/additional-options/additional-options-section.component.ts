b9228259a2833994b2dde5926181dcf3155e616a
Vicki League <vleague@bitwarden.com>
2025-02-25 14:02:01 -0500

    fix add/edit item page

--- libs/vault/src/cipher-form/components/additional-options/additional-options-section.component.ts
+++ libs/vault/src/cipher-form/components/additional-options/additional-options-section.component.ts
@@ -1,7 +1,7 @@
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
 import { CommonModule } from "@angular/common";
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from "@angular/core";
+import { ChangeDetectorRef, Component, Input, OnInit, ViewChild } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
 import { shareReplay } from "rxjs";
@@ -19,6 +19,7 @@ import {
 } from "@bitwarden/components";
 
 import { PasswordRepromptService } from "../../../services/password-reprompt.service";
+import { CipherFormConfig } from "../../abstractions/cipher-form-config.service";
 import { CipherFormContainer } from "../../cipher-form-container";
 import { CustomFieldsComponent } from "../custom-fields/custom-fields.component";
 
@@ -58,6 +59,8 @@ export class AdditionalOptionsSectionComponent implements OnInit {
 
   /** True when the form is in `partial-edit` mode */
   isPartialEdit = false;
+
+  @Input() mode: CipherFormConfig["mode"];
 
   constructor(
     private cipherFormContainer: CipherFormContainer,
