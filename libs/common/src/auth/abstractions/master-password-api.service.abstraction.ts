7055cc8b7ab9f0d6a514fc2c64c86a2b543d9c57
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-02-24 16:42:14 -0800

    add jsdocs to abstract

--- libs/common/src/auth/abstractions/master-password-api.service.abstraction.ts
+++ libs/common/src/auth/abstractions/master-password-api.service.abstraction.ts
@@ -4,9 +4,24 @@ import { UpdateTdeOffboardingPasswordRequest } from "../models/request/update-td
 import { UpdateTempPasswordRequest } from "../models/request/update-temp-password.request";
 
 export abstract class MasterPasswordApiService {
+  /**
+   * POSTs a SetPasswordRequest to "/accounts/set-password"
+   */
   abstract setPassword: (request: SetPasswordRequest) => Promise<any>;
+
+  /**
+   * POSTs a PasswordRequest to "/accounts/password"
+   */
   abstract postPassword: (request: PasswordRequest) => Promise<any>;
+
+  /**
+   * PUTs an UpdateTempPasswordRequest to "/accounts/update-temp-password"
+   */
   abstract putUpdateTempPassword: (request: UpdateTempPasswordRequest) => Promise<any>;
+
+  /**
+   * PUTs an UpdateTdeOffboardingPasswordRequest to "/accounts/update-tde-offboarding-password"
+   */
   abstract putUpdateTdeOffboardingPassword: (
     request: UpdateTdeOffboardingPasswordRequest,
   ) => Promise<any>;
