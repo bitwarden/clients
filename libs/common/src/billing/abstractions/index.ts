ded2c9023c05f215d074f25d326691a3b4599dc0
Jonas Hendrickx <jhendrickx@bitwarden.com>
2025-03-04 18:02:18 +0100

    r

--- libs/common/src/billing/abstractions/index.ts
+++ libs/common/src/billing/abstractions/index.ts
@@ -1,4 +1,3 @@
 export * from "./account/billing-account-profile-state.service";
-export * from "./organizations/organization-upselling.service.abstraction";
 export * from "./billing-api.service.abstraction";
 export * from "./organization-billing.service";
