8c4ee14d86991772539acc1d70a0f052879e85aa
jaasen-livefront <jaasen@livefront.com>
2025-03-07 17:04:33 -0800

    refresh vault item list after sharing cipher

--- apps/desktop/src/vault/app/vault/vault.component.ts
+++ apps/desktop/src/vault/app/vault/vault.component.ts
@@ -571,7 +571,7 @@ export class VaultComponent implements OnInit, OnDestroy {
       // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
       this.viewCipher(cipher);
-      await this.vaultItemsComponent.refresh();
+      await this.vaultItemsComponent.load(this.activeFilter.buildFilter());
     });
     // eslint-disable-next-line rxjs-angular/prefer-takeuntil, rxjs/no-async-subscribe
     this.modal.onClosed.subscribe(async () => {
