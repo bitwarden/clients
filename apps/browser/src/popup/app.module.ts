014acd72817bca918ee89f8ba8e0b9ee2392bc54
Alec Rippberger <alec@livefront.com>
2025-03-12 17:19:56 -0500

    Remove LoginDecryptionOptionsComponentV1 and related

--- apps/browser/src/popup/app.module.ts
+++ apps/browser/src/popup/app.module.ts
@@ -22,7 +22,6 @@ import { CurrentAccountComponent } from "../auth/popup/account-switching/current
 import { EnvironmentComponent } from "../auth/popup/environment.component";
 import { ExtensionAnonLayoutWrapperComponent } from "../auth/popup/extension-anon-layout-wrapper/extension-anon-layout-wrapper.component";
 import { HintComponent } from "../auth/popup/hint.component";
-import { LoginDecryptionOptionsComponentV1 } from "../auth/popup/login-decryption-options/login-decryption-options-v1.component";
 import { RemovePasswordComponent } from "../auth/popup/remove-password.component";
 import { SetPasswordComponent } from "../auth/popup/set-password.component";
 import { AccountSecurityComponent } from "../auth/popup/settings/account-security.component";
@@ -95,7 +94,6 @@ import "../platform/popup/locales";
     ColorPasswordCountPipe,
     EnvironmentComponent,
     HintComponent,
-    LoginDecryptionOptionsComponentV1,
     SetPasswordComponent,
     SsoComponentV1,
     TabsV2Component,
