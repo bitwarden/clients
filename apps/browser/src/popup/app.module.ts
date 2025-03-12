60dc20e3946295ef995bc8114408212c3455e31f
Alec Rippberger <alec@livefront.com>
2025-03-12 16:30:52 -0500

    Removing HintComponent and related

--- apps/browser/src/popup/app.module.ts
+++ apps/browser/src/popup/app.module.ts
@@ -21,7 +21,6 @@ import { AccountComponent } from "../auth/popup/account-switching/account.compon
 import { CurrentAccountComponent } from "../auth/popup/account-switching/current-account.component";
 import { EnvironmentComponent } from "../auth/popup/environment.component";
 import { ExtensionAnonLayoutWrapperComponent } from "../auth/popup/extension-anon-layout-wrapper/extension-anon-layout-wrapper.component";
-import { HintComponent } from "../auth/popup/hint.component";
 import { LoginDecryptionOptionsComponentV1 } from "../auth/popup/login-decryption-options/login-decryption-options-v1.component";
 import { LoginViaAuthRequestComponentV1 } from "../auth/popup/login-via-auth-request-v1.component";
 import { RemovePasswordComponent } from "../auth/popup/remove-password.component";
@@ -95,7 +94,6 @@ import "../platform/popup/locales";
     ColorPasswordPipe,
     ColorPasswordCountPipe,
     EnvironmentComponent,
-    HintComponent,
     LoginViaAuthRequestComponentV1,
     LoginDecryptionOptionsComponentV1,
     SetPasswordComponent,
