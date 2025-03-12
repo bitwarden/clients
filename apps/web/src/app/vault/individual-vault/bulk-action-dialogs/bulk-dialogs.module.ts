01f989e4eacae7a0dcca96c20db586236e4a863a
jaasen-livefront <jaasen@livefront.com>
2025-03-12 14:42:55 -0700

    Revert "remove bulk share dialog components and related modules and i18n keys"
    This reverts commit 40060b6011a99fdeea77573c1e3d3a2bff272abd.

--- apps/web/src/app/vault/individual-vault/bulk-action-dialogs/bulk-dialogs.module.ts
+++ apps/web/src/app/vault/individual-vault/bulk-action-dialogs/bulk-dialogs.module.ts
@@ -4,10 +4,11 @@ import { SharedModule } from "../../../shared";
 
 import { BulkDeleteDialogComponent } from "./bulk-delete-dialog/bulk-delete-dialog.component";
 import { BulkMoveDialogComponent } from "./bulk-move-dialog/bulk-move-dialog.component";
+import { BulkShareDialogComponent } from "./bulk-share-dialog/bulk-share-dialog.component";
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [BulkDeleteDialogComponent, BulkMoveDialogComponent],
-  exports: [BulkDeleteDialogComponent, BulkMoveDialogComponent],
+  declarations: [BulkDeleteDialogComponent, BulkMoveDialogComponent, BulkShareDialogComponent],
+  exports: [BulkDeleteDialogComponent, BulkMoveDialogComponent, BulkShareDialogComponent],
 })
 export class BulkDialogsModule {}
