892a5b3c2cbc66ce7277abf14446a485b416208f
Alec Rippberger <alec@livefront.com>
2025-03-12 14:00:10 -0500

    Remove HomeComponent from popup app.module.ts

--- apps/browser/src/popup/app.module.ts
+++ apps/browser/src/popup/app.module.ts
@@ -22,7 +22,6 @@ import { CurrentAccountComponent } from "../auth/popup/account-switching/current
 import { EnvironmentComponent } from "../auth/popup/environment.component";
 import { ExtensionAnonLayoutWrapperComponent } from "../auth/popup/extension-anon-layout-wrapper/extension-anon-layout-wrapper.component";
 import { HintComponent } from "../auth/popup/hint.component";
-import { HomeComponent } from "../auth/popup/home.component";
 import { LoginDecryptionOptionsComponentV1 } from "../auth/popup/login-decryption-options/login-decryption-options-v1.component";
 import { LoginViaAuthRequestComponentV1 } from "../auth/popup/login-via-auth-request-v1.component";
 import { RemovePasswordComponent } from "../auth/popup/remove-password.component";
@@ -97,7 +96,6 @@ import "../platform/popup/locales";
     ColorPasswordCountPipe,
     EnvironmentComponent,
     HintComponent,
-    HomeComponent,
     LoginViaAuthRequestComponentV1,
     LoginDecryptionOptionsComponentV1,
     SetPasswordComponent,
