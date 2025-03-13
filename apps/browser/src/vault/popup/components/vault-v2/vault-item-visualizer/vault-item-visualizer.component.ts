8b67b7dae079e2c610676a46e03435c3f2492c70
Miles Blackwood <mrobinson@bitwarden.com>
2025-03-13 14:50:24 -0400

    [WIP] Add todo.

--- apps/browser/src/vault/popup/components/vault-v2/vault-item-visualizer/vault-item-visualizer.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/vault-item-visualizer/vault-item-visualizer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, DestroyRef, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import {
@@ -11,6 +12,8 @@ import {
   FormFieldModule,
   SelectModule,
 } from "@bitwarden/components";
+
+import { generateWiFiQRCode } from "../../../../../../../../libs/vault/src/utils/visual-vault-items";
 
 import { PopupFooterComponent } from "../../../../../platform/popup/layout/popup-footer.component";
 import { PopupHeaderComponent } from "../../../../../platform/popup/layout/popup-header.component";
@@ -35,12 +38,44 @@ import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-p
   standalone: true,
   templateUrl: "./vault-item-visualizer.component.html",
 })
-export class VaultItemVisualizerComponent {
+export class VaultItemVisualizerComponent implements OnInit {
+  constructor(private destroyRef: DestroyRef) {}
+
+  @ViewChild("visualizer", { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+
+  ctx: CanvasRenderingContext2D;
   headerText: string;
+  wiFiQRCode: string;
 
   dataToShare = new FormGroup({
     qrCodeType: new FormControl("Wi-Fi"),
     fieldWithSSID: new FormControl("Username"),
     fieldWithPassword: new FormControl("Password"),
   });
+
+  async ngOnInit() {
+    // TODO use IMG or ideally SVG (scalable)
+    this.ctx = this.canvas.nativeElement.getContext("2d");
+
+    await this.renderVisualization();
+
+    this.dataToShare.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((values) => {
+      void this.renderVisualization();
+      console.log(values);
+    });
+  }
+
+  async renderVisualization() {
+    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    // TODO: get the value from the cipher instead of using the form value
+    this.wiFiQRCode = await generateWiFiQRCode(
+      this.dataToShare.controls.fieldWithSSID.value,
+      this.dataToShare.controls.fieldWithPassword.value,
+    );
+    const img = new Image();
+    img.onload = () => {
+      this.ctx.drawImage(img, 0, 0);
+    };
+    img.src = this.wiFiQRCode;
+  }
 }
