f2420952093cf6285d277ce728887c26ee9bfff8
Miles Blackwood <mrobinson@bitwarden.com>
2025-03-14 11:48:03 -0400

    [WIP] Cleanup and abstract qrCodeOptions.

--- apps/browser/src/vault/popup/components/vault-v2/vault-item-visualizer/vault-item-visualizer.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/vault-item-visualizer/vault-item-visualizer.component.ts
@@ -1,8 +1,10 @@
-import { Component, DestroyRef, effect, OnInit, signal, WritableSignal } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, effect, OnInit, signal, WritableSignal } from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 
+import { QRCodeOption } from "@bitwarden/common/platform/enums";
 import {
   TypographyModule,
   ButtonModule,
@@ -13,14 +15,14 @@ import {
   FormFieldModule,
   SelectModule,
 } from "@bitwarden/components";
+import { generateWiFiQRCode } from "@bitwarden/vault";
 
-import { generateWiFiQRCode } from "../../../../../../../../libs/vault/src/utils/visual-vault-items";
-import { PopupFooterComponent } from "../../../../../platform/popup/layout/popup-footer.component";
 import { PopupHeaderComponent } from "../../../../../platform/popup/layout/popup-header.component";
 import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-page.component";
 
 @Component({
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     TypographyModule,
     ButtonModule,
@@ -30,7 +32,6 @@ import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-p
     CardComponent,
     PopupPageComponent,
     PopupHeaderComponent,
-    PopupFooterComponent,
     FormsModule,
     FormFieldModule,
     SelectModule,
@@ -39,39 +40,47 @@ import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-p
   templateUrl: "./vault-item-visualizer.component.html",
 })
 export class VaultItemVisualizerComponent implements OnInit {
-  constructor(
-    private destroyRef: DestroyRef,
-    private sanitizer: DomSanitizer,
-  ) {
-    effect(async () => {
-      const values = this.dataToShareValues();
-      if (typeof values !== "undefined") {
-        const wiFiQRCode = await generateWiFiQRCode(values.fieldWithPassword, values.fieldWithSSID);
-        this.wiFiQRCode.set(wiFiQRCode);
-      }
-    });
-  }
-
   // TODO strings + translations
-  headerText: string;
-  private wiFiQRCode: WritableSignal<string> = signal("");
+  // headerText: string;
+
+  qrCodeOptions: { name: string; value: QRCodeOption }[];
 
   dataToShareForm = new FormGroup({
-    qrCodeType: new FormControl(""),
+    qrCodeType: new FormControl<QRCodeOption>(null),
     fieldWithSSID: new FormControl(""),
     fieldWithPassword: new FormControl(""),
   });
 
   dataToShareValues = toSignal(this.dataToShareForm.valueChanges);
 
+  private wiFiQRCode: WritableSignal<string> = signal("");
+
+  constructor(private sanitizer: DomSanitizer) {
+    /* Set initial QR Code options */
+    this.qrCodeOptions = [
+      { name: "Wi-Fi", value: "wifi" },
+      { name: "Contact", value: "contact" },
+      { name: "URL", value: "url" },
+      // { name: i18nService.t("example"), value: example },
+    ];
+    effect(async () => {
+      /* Retriggers whenever form changes */
+      const values = this.dataToShareValues();
+      if (typeof values !== "undefined" && values.qrCodeType !== null) {
+        const wiFiQRCode = await generateWiFiQRCode(values.fieldWithPassword, values.fieldWithSSID);
+        this.wiFiQRCode.set(wiFiQRCode);
+      }
+    });
+  }
+
   sanitizeSVG(): SafeHtml {
     return this.sanitizer.bypassSecurityTrustHtml(this.wiFiQRCode());
   }
 
   async ngOnInit() {
-    // Set values in init triggers the QR Code generation effect
+    /* Set default QR Code Type and field options after inference (TODO) */
     this.dataToShareForm.setValue({
-      qrCodeType: "Wi-Fi",
+      qrCodeType: "wifi",
       fieldWithSSID: "Username",
       fieldWithPassword: "Password",
     });
