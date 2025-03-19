e5f7f1b1fa08677b7e3fbb4cddec6a242fe21402
Thomas Avery <tavery@bitwarden.com>
2025-03-18 19:45:39 -0500

    Wire up broadcasting in dependency injection
    Co-authored-by: Matt Gibson <mgibson@bitwarden.com>

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1334,6 +1334,8 @@ export default class MainBackground {
     this.syncServiceListener?.listener$().subscribe();
     await this.autoSubmitLoginBackground.init();
 
+    this.configService.broadcastConfigChangesTo(this.encryptService, this.bulkEncryptService);
+
     if (
       BrowserApi.isManifestVersion(2) &&
       (await this.configService.getFeatureFlag(FeatureFlag.PM4154_BulkEncryptionService))
