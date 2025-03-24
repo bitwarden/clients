5a03f69db72f662d43174eba1325065b2cc721ce
jng <jng@bitwarden.com>
2025-03-24 12:42:10 -0400

    update vault nudges. add spec file

---
+++ libs/vault/src/services/custom-nudges-services/vault-onboarding-nudges.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from "@angular/core";
+import { map, Observable, of, switchMap } from "rxjs";
+
+import { UserId } from "@bitwarden/common/types/guid";
+import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
+
+import { DefaultSingleNudgeService, VaultNudgeType } from "../vault-nudges.service";
+
+/**
+ * Custom Nudge Service to use for the Onboarding Nudges in the Vault
+ */
+@Injectable({
+  providedIn: "root",
+})
+export class VaultOnboardingNudgesService extends DefaultSingleNudgeService {
+  cipherService = inject(CipherService);
+
+  shouldShowNudge$(nudgeType: VaultNudgeType, userId: UserId): Observable<boolean> {
+    return this.isDismissed$(nudgeType, userId).pipe(
+      switchMap((dismissed) =>
+        dismissed
+          ? of(false)
+          : this.cipherService
+              .cipherViews$(userId)
+              .pipe(map((ciphers) => ciphers == null || ciphers.length === 0)),
+      ),
+    );
+  }
+}
---
+++ libs/vault/src/services/vault-nudges.service.ts
@@ -0,0 +1,116 @@
+import { inject, Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+
+import {
+  BANNERS_DISMISSED_DISK,
+  StateProvider,
+  UserKeyDefinition,
+} from "@bitwarden/common/platform/state";
+import { UserId } from "@bitwarden/common/types/guid";
+
+import { VaultOnboardingNudgesService } from "./custom-nudges-services/vault-onboarding-nudges.service";
+
+/**
+ * Enum to list the various nudge types, to be used by components/badges to show/hide the nudge
+ */
+export enum VaultNudgeType {
+  /** Nudge to show when user has no items in their vault */
+  HasVaultItems = "has-vault-items",
+  IntroCarouselDismissal = "intro-carousel-dismissal",
+}
+
+export const VAULT_NUDGE_DISMISSED_DISK_KEY = new UserKeyDefinition<VaultNudgeType[]>(
+  BANNERS_DISMISSED_DISK,
+  "vaultNudgeDismissed",
+  {
+    deserializer: (nudgeDismissed) => nudgeDismissed,
+    clearOn: [], // Do not clear dismissals
+  },
+);
+
+/**
+ * Base interface for handling a nudge's status
+ */
+export interface SingleNudgeService {
+  shouldShowNudge$(nudgeType: VaultNudgeType, userId: UserId): Observable<boolean>;
+  setNudgeStatus(nudgeType: VaultNudgeType, dismissed: boolean, userId: UserId): Promise<void>;
+}
+
+/**
+ * The basic implementation of a nudge service. Simply stores the dismissed state in the user's state.
+ */
+@Injectable({
+  providedIn: "root",
+})
+export class DefaultSingleNudgeService implements SingleNudgeService {
+  stateProvider = inject(StateProvider);
+
+  protected isDismissed$(nudgeType: VaultNudgeType, userId: UserId): Observable<boolean> {
+    return this.stateProvider
+      .getUser(userId, VAULT_NUDGE_DISMISSED_DISK_KEY)
+      .state$.pipe(map((nudges) => nudges?.includes(nudgeType) ?? false));
+  }
+
+  shouldShowNudge$(nudgeType: VaultNudgeType, userId: UserId): Observable<boolean> {
+    return this.isDismissed$(nudgeType, userId).pipe(map((dismissed) => !dismissed));
+  }
+
+  async setNudgeStatus(
+    nudgeType: VaultNudgeType,
+    dismissed: boolean,
+    userId: UserId,
+  ): Promise<void> {
+    await this.stateProvider.getUser(userId, VAULT_NUDGE_DISMISSED_DISK_KEY).update((nudges) => {
+      nudges ??= [];
+      if (dismissed) {
+        nudges.push(nudgeType);
+      } else {
+        nudges = nudges.filter((n) => n !== nudgeType);
+      }
+      return nudges;
+    });
+  }
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class VaultNudgesService {
+  /**
+   * Custom nudge services to use for specific nudge types
+   * Each nudge type can have its own service to determine when to show the nudge
+   * @private
+   */
+  private customNudgeServices: any = {
+    [VaultNudgeType.HasVaultItems]: inject(VaultOnboardingNudgesService),
+  };
+
+  /**
+   * Default nudge service to use when no custom service is available
+   * Simply stores the dismissed state in the user's state
+   * @private
+   */
+  private defaultNudgeService = inject(DefaultSingleNudgeService);
+
+  private getNudgeService(nudge: VaultNudgeType): SingleNudgeService {
+    return this.customNudgeServices[nudge] ?? this.defaultNudgeService;
+  }
+
+  /**
+   * Check if a nudge should be shown to the user
+   * @param nudge
+   * @param userId
+   */
+  showNudge$(nudge: VaultNudgeType, userId: UserId) {
+    return this.getNudgeService(nudge).shouldShowNudge$(nudge, userId);
+  }
+
+  /**
+   * Dismiss a nudge for the user so that it is not shown again
+   * @param nudge
+   * @param userId
+   */
+  dismissNudge(nudge: VaultNudgeType, userId: UserId) {
+    return this.getNudgeService(nudge).setNudgeStatus(nudge, true, userId);
+  }
+}
