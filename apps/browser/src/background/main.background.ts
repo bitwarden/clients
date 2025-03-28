f8145d8f6014c782ae965b662dbab76b0f8e80ce
Nick Krantz <nick@livefront.com>
2025-03-28 11:19:17 -0500

    navigate directly to the at risk passwords page

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1612,6 +1612,17 @@ export default class MainBackground {
     }
   }
 
+  /** Opens the `/at-risk-passwords` page within the popup */
+  async openAtRisksPasswordsPage() {
+    const browserAction = BrowserApi.getBrowserAction();
+    // Set route of the popup before attempting to open it.
+    // If the vault is locked, this won't have an effect as the auth guards will
+    // redirect the user to the login page.
+    await browserAction.setPopup({ popup: "popup/index.html#/at-risk-passwords" });
+
+    await this.openPopup();
+  }
+
   async reseedStorage() {
     if (
       !this.platformUtilsService.isChrome() &&
