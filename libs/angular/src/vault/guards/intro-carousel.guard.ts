31e233043196a962cf9282c29694a272f7008e77
jng <jng@bitwarden.com>
2025-03-28 13:17:17 -0400

    add flag check to guard

--- libs/angular/src/vault/guards/intro-carousel.guard.ts
+++ libs/angular/src/vault/guards/intro-carousel.guard.ts
@@ -1,7 +1,20 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateFn, Router } from "@angular/router";
 
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
+
 export const IntroCarouselGuard: CanActivateFn = async (route: ActivatedRouteSnapshot) => {
   const router = inject(Router);
+  const configService = inject(ConfigService);
+
+  const hasOnboardingNudgesFlag = await configService.getFeatureFlag(
+    FeatureFlag.PM8851_BrowserOnboardingNudge,
+  );
+
+  if (!hasOnboardingNudgesFlag) {
+    return true;
+  }
+
   return router.createUrlTree(["/intro-carousel"]);
 };
