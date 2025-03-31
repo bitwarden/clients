b75987f110e49e86f901b1f34b1a501ea6ff9fb7
Daniel Riera <driera@livefront.com>
2025-03-31 14:13:17 -0400

    custom signal when needed on option select to track individual select values

--- apps/browser/src/autofill/content/components/option-selection/option-selection.ts
+++ apps/browser/src/autofill/content/components/option-selection/option-selection.ts
@@ -6,7 +6,6 @@ import { Theme, ThemeTypes } from "@bitwarden/common/platform/enums";
 
 import { OptionSelectionButton } from "../buttons/option-selection-button";
 import { Option } from "../common-types";
-import { selectedFolder as selectedFolderSignal } from "../signals/selected-folder";
 
 import { optionItemIconWidth } from "./option-item";
 import { OptionItems, optionsMenuItemMaxWidth } from "./option-items";
@@ -32,6 +31,9 @@ export class OptionSelection extends LitElement {
 
   @property({ type: (selectedOption: Option["value"]) => selectedOption })
   handleSelectionUpdate?: (args: any) => void;
+
+  @property({ attribute: false })
+  selectedSignal?: { set: (value: any) => void };
 
   @state()
   private showMenu = false;
@@ -78,7 +80,7 @@ export class OptionSelection extends LitElement {
   private handleOptionSelection = (selectedOption: Option) => {
     this.showMenu = false;
     this.selection = selectedOption;
-    selectedFolderSignal.set(selectedOption.value);
+    this.selectedSignal?.set(selectedOption.value);
 
     // Any side-effects that should occur from the selection
     this.handleSelectionUpdate?.(selectedOption.value);
