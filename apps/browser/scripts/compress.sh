60540a33dd97c72104c45e06991a6db5183c7cc8
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2025-04-03 10:47:04 -0400

    Change logic

--- apps/browser/scripts/compress.sh
+++ apps/browser/scripts/compress.sh
@@ -14,20 +14,19 @@ FILENAME=$1
 SCRIPT_ROOT="$(dirname "$0")"
 BUILD_DIR="$SCRIPT_ROOT/../build"
 
-# Change into build directory
-cd $BUILD_DIR
-
-DIST_DIR="../dist"
-
-# Create dist directory if it doesn't exist
-mkdir -p $DIST_DIR
-
-DIST_PATH="$DIST_DIR/$FILENAME"
-
-rm -f $DIST_PATH
-
-# Compress build directory
+# Check if build directory exists
 if [ -d "$BUILD_DIR" ]; then
-  zip -r $DIST_PATH .
+  cd $BUILD_DIR
+  
+  # Create dist directory if it doesn't exist
+  DIST_DIR="../dist"
+  mkdir -p $DIST_DIR
+
+  # Remove existing dist zip file
+  DIST_PATH="$DIST_DIR/$FILENAME"
+  rm -f $DIST_PATH
+
+  # Compress build directory
+  zip -r $DIST_PATH ./
   echo "Zipped $BUILD_DIR into $DIST_PATH"
 fi
