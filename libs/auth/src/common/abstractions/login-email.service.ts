c56edf292968fc4b7c1885c13189418eb625fe14
Jared Snider <jsnider@bitwarden.com>
2025-04-08 13:32:38 -0400

    PM-11502 - LoginEmailSvcAbstraction - mark methods as abstract to satisfy strict ts.

--- libs/auth/src/common/abstractions/login-email.service.ts
+++ libs/auth/src/common/abstractions/login-email.service.ts
@@ -5,25 +5,25 @@ export abstract class LoginEmailServiceAbstraction {
    * An observable that monitors the loginEmail.
    * The loginEmail is the email that is being used in the current login process.
    */
-  loginEmail$: Observable<string | null>;
+  abstract loginEmail$: Observable<string | null>;
   /**
    * An observable that monitors the remembered email.
    * This will return null if an account is being added.
    */
-  rememberedEmail$: Observable<string | null>;
+  abstract rememberedEmail$: Observable<string | null>;
   /**
    * Sets the loginEmail in memory.
    * The loginEmail is the email that is being used in the current login process.
    * Consumed through `loginEmail$` observable.
    */
-  setLoginEmail: (email: string) => Promise<void>;
+  abstract setLoginEmail: (email: string) => Promise<void>;
   /**
    * Persist the user's choice of whether to remember their email on subsequent login attempts.
    * Consumed through `rememberedEmail$` observable.
    */
-  setRememberedEmailChoice: (email: string, remember: boolean) => Promise<void>;
+  abstract setRememberedEmailChoice: (email: string, remember: boolean) => Promise<void>;
   /**
    * Clears the in-progress login email, to be used after a successful login.
    */
-  clearLoginEmail: () => Promise<void>;
+  abstract clearLoginEmail: () => Promise<void>;
 }
