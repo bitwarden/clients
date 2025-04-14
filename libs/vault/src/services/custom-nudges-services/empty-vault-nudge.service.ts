d16255590cdf6a2baefee3bed3e757770e444888
jng <jng@bitwarden.com>
2025-04-14 11:07:53 -0400

    added org manage and create check to empty vault nudge service

--- libs/vault/src/services/custom-nudges-services/empty-vault-nudge.service.ts
+++ libs/vault/src/services/custom-nudges-services/empty-vault-nudge.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from "@angular/core";
-import { map, Observable, of, switchMap } from "rxjs";
+import { combineLatest, Observable, of, switchMap } from "rxjs";
 
+import { CollectionService } from "@bitwarden/admin-console/common";
+import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { UserId } from "@bitwarden/common/types/guid";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 
@@ -15,16 +17,38 @@ import { VaultNudgeType } from "../vault-nudges.service";
 })
 export class EmptyVaultNudgeService extends DefaultSingleNudgeService {
   cipherService = inject(CipherService);
+  organizationService = inject(OrganizationService);
+  collectionService = inject(CollectionService);
 
   shouldShowNudge$(nudgeType: VaultNudgeType, userId: UserId): Observable<boolean> {
-    return this.isDismissed$(nudgeType, userId).pipe(
-      switchMap((dismissed) =>
-        dismissed
-          ? of(false)
-          : this.cipherService
-              .cipherViews$(userId)
-              .pipe(map((ciphers) => ciphers == null || ciphers.length === 0)),
-      ),
+    return combineLatest([
+      this.isDismissed$(nudgeType, userId),
+      this.cipherService.cipherViews$(userId),
+      this.organizationService.organizations$(userId),
+      this.collectionService.decryptedCollections$,
+    ]).pipe(
+      switchMap(([dismissed, ciphers, orgs, collections]) => {
+        const orgIds = new Set(orgs.map((org) => org.id));
+        const canCreateCollections = orgs.filter((org) => {
+          return org.canCreateNewCollections;
+        });
+        const managedCollections = collections.filter((collection) => {
+          if (orgIds.has(collection.organizationId) && collection.manage) {
+            return of(true);
+          }
+        });
+        // Do not show nudge when
+        // user has previously dismissed
+        // user belongs to an organization and then cannot create collections || manage collections
+        if (
+          dismissed ||
+          (orgs.length !== 0 &&
+            (managedCollections.length === 0 || canCreateCollections.length === 0))
+        ) {
+          return of(false);
+        }
+        return of(ciphers == null || ciphers.length === 0);
+      }),
     );
   }
 }
