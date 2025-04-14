a7a0e7551a0dbf86cbea65e0703e9fa19ca9209d
jaasen-livefront <jaasen@livefront.com>
2025-04-14 09:41:22 -0700

    fix tests

--- apps/browser/src/popup/tabs-v2.component.spec.ts
+++ apps/browser/src/popup/tabs-v2.component.spec.ts
@@ -1,11 +1,16 @@
 import { Component } from "@angular/core";
 import { ComponentFixture, TestBed, fakeAsync, flush, tick } from "@angular/core/testing";
-import { RouterModule } from "@angular/router";
-import { BehaviorSubject } from "rxjs";
+import { ActivatedRoute, RouterModule } from "@angular/router";
+import { mock } from "jest-mock-extended";
+import { BehaviorSubject, of } from "rxjs";
 
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { UserId } from "@bitwarden/common/types/guid";
 import { HasNudgeService } from "@bitwarden/vault";
+
+import { PopupTabNavigationComponent } from "../platform/popup/layout/popup-tab-navigation.component";
 
 import { TabsV2Component } from "./tabs-v2.component";
 
@@ -15,17 +20,12 @@ import { TabsV2Component } from "./tabs-v2.component";
 })
 class DummyVaultBerryComponent {}
 
-@Component({
-  selector: "popup-tab-navigation",
-  template: `<ng-content></ng-content>`,
-})
-class DummyPopupTabNavigationComponent {}
-
 describe("TabsV2Component", () => {
   let fixture: ComponentFixture<TabsV2Component>;
 
   let accountSubject: BehaviorSubject<any>;
   let hasNudgeSubject: BehaviorSubject<boolean>;
+  const configServiceMock = mock<ConfigService>();
 
   const accountServiceStub = {
     activeAccount$: undefined as any,
@@ -36,12 +36,16 @@ describe("TabsV2Component", () => {
   };
 
   beforeEach(async () => {
+    jest.spyOn(configServiceMock, "getFeatureFlag$").mockReturnValue(of(true));
     await TestBed.configureTestingModule({
-      imports: [RouterModule],
-      declarations: [TabsV2Component, DummyVaultBerryComponent, DummyPopupTabNavigationComponent],
+      imports: [RouterModule, PopupTabNavigationComponent],
+      declarations: [TabsV2Component, DummyVaultBerryComponent],
       providers: [
         { provide: AccountService, useValue: accountServiceStub },
         { provide: HasNudgeService, useValue: hasNudgeServiceStub },
+        { provide: ConfigService, useValue: configServiceMock },
+        { provide: ActivatedRoute, useValue: { snapshot: { params: {} } } },
+        { provide: I18nService, useValue: { t: (key: string) => key } },
       ],
     })
       .overrideComponent(TabsV2Component, {
