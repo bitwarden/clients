d19aab15e8d48a004e445592da24d1a680ad472c
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-04-15 12:22:16 +0200

    feat: implement userClient mocking

--- libs/common/src/platform/services/sdk/mock-sdk.service.ts
+++ libs/common/src/platform/services/sdk/mock-sdk.service.ts
@@ -1,28 +1,63 @@
-import { BehaviorSubject, Observable } from "rxjs";
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  map,
+  Observable,
+  takeWhile,
+  throwIfEmpty,
+} from "rxjs";
 
 import { BitwardenClient } from "@bitwarden/sdk-internal";
 
 import { UserId } from "../../../types/guid";
-import { SdkService } from "../../abstractions/sdk/sdk.service";
+import { SdkService, UserNotLoggedInError } from "../../abstractions/sdk/sdk.service";
 import { Rc } from "../../misc/reference-counting/rc";
 
 import { DeepMockProxy, mockDeep } from "./mock-deep";
 
 export class MockSdkService implements SdkService {
+  private userClients$ = new BehaviorSubject<{
+    [userId: UserId]: Rc<BitwardenClient> | undefined;
+  }>({});
+
   private _client$ = new BehaviorSubject(mockDeep<BitwardenClient>());
   client$ = this._client$.asObservable();
 
-  version$: Observable<string>;
+  version$ = new BehaviorSubject("0.0.1-test").asObservable();
 
-  userClient$(userId: UserId): Observable<Rc<BitwardenClient> | undefined> {
-    throw new Error("Method not implemented.");
+  userClient$(userId: UserId): Observable<Rc<BitwardenClient>> {
+    return this.userClients$.pipe(
+      takeWhile((clients) => clients[userId] !== undefined, false),
+      map((clients) => clients[userId] as Rc<BitwardenClient>),
+      distinctUntilChanged(),
+      throwIfEmpty(() => new UserNotLoggedInError(userId)),
+    );
   }
 
-  setClient(userId: UserId, client: BitwardenClient | undefined): void {
-    throw new Error("Method not implemented.");
+  setClient(): void {
+    throw new Error("Not supported in mock service");
   }
 
   get clientMock(): DeepMockProxy<BitwardenClient> {
-    return this._client$.getValue();
+    return this._client$.value;
   }
+
+  readonly simulate = {
+    userLogin: (userId: UserId) => {
+      const client = mockDeep<BitwardenClient>();
+      this.userClients$.next({
+        ...this.userClients$.getValue(),
+        [userId]: new Rc(client),
+      });
+      return client;
+    },
+    userLogout: (userId: UserId) => {
+      const clients = this.userClients$.value;
+      clients[userId]?.markForDisposal();
+      this.userClients$.next({
+        ...clients,
+        [userId]: undefined,
+      });
+    },
+  };
 }
