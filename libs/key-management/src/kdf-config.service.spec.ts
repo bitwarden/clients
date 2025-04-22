bb071bce82bda375efea20a54229a3340fd4c3ec
Thomas Avery <tavery@bitwarden.com>
2025-04-22 17:03:52 -0500

    cleanup KdfConfigService unit tests

--- libs/key-management/src/kdf-config.service.spec.ts
+++ libs/key-management/src/kdf-config.service.spec.ts
@@ -26,68 +26,43 @@ describe("KdfConfigService", () => {
     sutKdfConfigService = new DefaultKdfConfigService(fakeStateProvider);
   });
 
-  it("setKdfConfig(): should set the PBKDF2KdfConfig config", async () => {
-    const kdfConfig: KdfConfig = new PBKDF2KdfConfig(500_000);
-    await sutKdfConfigService.setKdfConfig(mockUserId, kdfConfig);
-    expect(fakeStateProvider.mock.setUserState).toHaveBeenCalledWith(
-      KDF_CONFIG,
-      kdfConfig,
-      mockUserId,
-    );
-  });
+  describe("setKdfConfig", () => {
+    it("sets the PBKDF2KdfConfig config", async () => {
+      const kdfConfig: KdfConfig = new PBKDF2KdfConfig(500_000);
+      await sutKdfConfigService.setKdfConfig(mockUserId, kdfConfig);
+      expect(fakeStateProvider.mock.setUserState).toHaveBeenCalledWith(
+        KDF_CONFIG,
+        kdfConfig,
+        mockUserId,
+      );
+    });
 
-  it("setKdfConfig(): should set the Argon2KdfConfig config", async () => {
-    const kdfConfig: KdfConfig = new Argon2KdfConfig(2, 63, 3);
-    await sutKdfConfigService.setKdfConfig(mockUserId, kdfConfig);
-    expect(fakeStateProvider.mock.setUserState).toHaveBeenCalledWith(
-      KDF_CONFIG,
-      kdfConfig,
-      mockUserId,
-    );
-  });
+    it("sets the Argon2KdfConfig config", async () => {
+      const kdfConfig: KdfConfig = new Argon2KdfConfig(2, 63, 3);
+      await sutKdfConfigService.setKdfConfig(mockUserId, kdfConfig);
+      expect(fakeStateProvider.mock.setUserState).toHaveBeenCalledWith(
+        KDF_CONFIG,
+        kdfConfig,
+        mockUserId,
+      );
+    });
 
-  it("setKdfConfig(): should throw error KDF cannot be null", async () => {
-    try {
-      await sutKdfConfigService.setKdfConfig(mockUserId, null as unknown as KdfConfig);
-    } catch (e) {
-      expect(e).toEqual(new Error("kdfConfig cannot be null"));
-    }
-  });
+    it("throws error KDF cannot be null", async () => {
+      try {
+        await sutKdfConfigService.setKdfConfig(mockUserId, null as unknown as KdfConfig);
+      } catch (e) {
+        expect(e).toEqual(new Error("kdfConfig cannot be null"));
+      }
+    });
 
-  it("setKdfConfig(): should throw error userId cannot be null", async () => {
-    const kdfConfig: KdfConfig = new Argon2KdfConfig(3, 64, 4);
-    try {
-      await sutKdfConfigService.setKdfConfig(null as unknown as UserId, kdfConfig);
-    } catch (e) {
-      expect(e).toEqual(new Error("userId cannot be null"));
-    }
-  });
-
-  it("getKdfConfig$(UserId): should get KdfConfig of provided user", async () => {
-    await expect(firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId))).resolves.toBeNull();
-    const kdfConfig: KdfConfig = new PBKDF2KdfConfig(500_000);
-    await fakeStateProvider.setUserState(KDF_CONFIG, kdfConfig, mockUserId);
-    await expect(firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId))).resolves.toEqual(
-      kdfConfig,
-    );
-  });
-
-  it("getKdfConfig$(UserId): should get KdfConfig of provided user after changed", async () => {
-    await expect(firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId))).resolves.toBeNull();
-    await fakeStateProvider.setUserState(KDF_CONFIG, new PBKDF2KdfConfig(500_000), mockUserId);
-    const kdfConfigChanged: KdfConfig = new PBKDF2KdfConfig(500_001);
-    await fakeStateProvider.setUserState(KDF_CONFIG, kdfConfigChanged, mockUserId);
-    await expect(firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId))).resolves.toEqual(
-      kdfConfigChanged,
-    );
-  });
-
-  it("getKdfConfig$(UserId): should throw error userId cannot be null", async () => {
-    try {
-      sutKdfConfigService.getKdfConfig$(null as unknown as UserId);
-    } catch (e) {
-      expect(e).toEqual(new Error("userId cannot be null"));
-    }
+    it("throws error userId cannot be null", async () => {
+      const kdfConfig: KdfConfig = new Argon2KdfConfig(3, 64, 4);
+      try {
+        await sutKdfConfigService.setKdfConfig(null as unknown as UserId, kdfConfig);
+      } catch (e) {
+        expect(e).toEqual(new Error("userId cannot be null"));
+      }
+    });
   });
 
   describe("getKdfConfig", () => {
@@ -106,6 +81,39 @@ describe("KdfConfigService", () => {
       const kdfConfig: KdfConfig = new PBKDF2KdfConfig(500_000);
       await fakeStateProvider.setUserState(KDF_CONFIG, kdfConfig, mockUserId);
       await expect(sutKdfConfigService.getKdfConfig(mockUserId)).resolves.toEqual(kdfConfig);
+    });
+  });
+
+  describe("getKdfConfig$", () => {
+    it("gets KdfConfig of provided user", async () => {
+      await expect(
+        firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId)),
+      ).resolves.toBeNull();
+      const kdfConfig: KdfConfig = new PBKDF2KdfConfig(500_000);
+      await fakeStateProvider.setUserState(KDF_CONFIG, kdfConfig, mockUserId);
+      await expect(firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId))).resolves.toEqual(
+        kdfConfig,
+      );
+    });
+
+    it("gets KdfConfig of provided user after changed", async () => {
+      await expect(
+        firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId)),
+      ).resolves.toBeNull();
+      await fakeStateProvider.setUserState(KDF_CONFIG, new PBKDF2KdfConfig(500_000), mockUserId);
+      const kdfConfigChanged: KdfConfig = new PBKDF2KdfConfig(500_001);
+      await fakeStateProvider.setUserState(KDF_CONFIG, kdfConfigChanged, mockUserId);
+      await expect(firstValueFrom(sutKdfConfigService.getKdfConfig$(mockUserId))).resolves.toEqual(
+        kdfConfigChanged,
+      );
+    });
+
+    it("throws error userId cannot be null", async () => {
+      try {
+        sutKdfConfigService.getKdfConfig$(null as unknown as UserId);
+      } catch (e) {
+        expect(e).toEqual(new Error("userId cannot be null"));
+      }
     });
   });
 });
