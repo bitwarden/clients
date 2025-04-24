a76a0ff9d02c2c56f6ccacacaa5214c6ed8a26a4
voommen-livefront <voommen@livefront.com>
2025-04-24 12:57:48 -0500

    PM-20505 remove unwanted lines of code

--- apps/web/src/app/tools/reports/pages/cipher-report.component.ts
+++ apps/web/src/app/tools/reports/pages/cipher-report.component.ts
@@ -205,10 +205,8 @@ export class CipherReportComponent implements OnDestroy {
       return;
     }
 
-    await this.refresh(result, cipher, formConfig);
     // If the dialog was closed by deleting the cipher, refresh the report.
     if (result === VaultItemDialogResult.Deleted || result === VaultItemDialogResult.Saved) {
-      // await this.load();
       await this.refresh(result, cipher, formConfig);
     }
   }
