e460dbd43128cca13a6407e55f70400750695d05
Cy Okeke <cokeke@bitwarden.com>
2025-04-28 12:58:53 +0100

    remove the FIXME comment

--- libs/common/src/billing/abstractions/organizations/organization-sponsorship-api.service.abstraction.ts
+++ libs/common/src/billing/abstractions/organizations/organization-sponsorship-api.service.abstraction.ts
@@ -1,6 +1,3 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
-
 import { ListResponse } from "../../../models/response/list.response";
 import { OrganizationSponsorshipInvitesResponse } from "../../models/response/organization-sponsorship-invites.response";
 
