fbb3cd8f1f3f74611e51e58f5249176549c6986e
jng <jng@bitwarden.com>
2025-04-29 15:59:33 -0400

    wrapping the has-items-nudge logic in a try/catch

--- libs/vault/src/services/custom-nudges-services/has-items-nudge.service.ts
+++ libs/vault/src/services/custom-nudges-services/has-items-nudge.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import { combineLatest, Observable, switchMap } from "rxjs";
 
 import { VaultProfileService } from "@bitwarden/angular/vault/services/vault-profile.service";
+import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { UserId } from "@bitwarden/common/types/guid";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 
@@ -17,6 +18,7 @@ import { NudgeStatus, VaultNudgeType } from "../vault-nudges.service";
 export class HasItemsNudgeService extends DefaultSingleNudgeService {
   cipherService = inject(CipherService);
   vaultProfileService = inject(VaultProfileService);
+  logService = inject(LogService);
 
   nudgeStatus$(nudgeType: VaultNudgeType, userId: UserId): Observable<NudgeStatus> {
     return combineLatest([
@@ -24,17 +26,22 @@ export class HasItemsNudgeService extends DefaultSingleNudgeService {
       this.getNudgeStatus$(nudgeType, userId),
     ]).pipe(
       switchMap(async ([ciphers, nudgeStatus]) => {
-        const creationDate = await this.vaultProfileService.getProfileCreationDate(userId);
-        const thirtyDays = new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000);
-        const isRecentAcct = creationDate >= thirtyDays;
+        try {
+          const creationDate = await this.vaultProfileService.getProfileCreationDate(userId);
+          const thirtyDays = new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000);
+          const isRecentAcct = creationDate >= thirtyDays;
 
-        if (!isRecentAcct || nudgeStatus.hasSpotlightDismissed) {
+          if (!isRecentAcct || nudgeStatus.hasSpotlightDismissed) {
+            return nudgeStatus;
+          } else {
+            return {
+              hasBadgeDismissed: ciphers == null || ciphers.length === 0,
+              hasSpotlightDismissed: ciphers == null || ciphers.length === 0,
+            };
+          }
+        } catch (error) {
+          this.logService.error("Failed to fetch profile creation date: ", error);
           return nudgeStatus;
-        } else {
-          return {
-            hasBadgeDismissed: ciphers == null || ciphers.length === 0,
-            hasSpotlightDismissed: ciphers == null || ciphers.length === 0,
-          };
         }
       }),
     );
