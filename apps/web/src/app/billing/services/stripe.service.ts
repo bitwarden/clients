302cedbdae66c7f1c0474d55336575cc9d244f3b
Bryan Cunningham <bryan.cunningham@me.com>
2025-05-01 15:53:21 -0400

    Remove quotes around Roboto declaration

--- apps/web/src/app/billing/services/stripe.service.ts
+++ apps/web/src/app/billing/services/stripe.service.ts
@@ -148,7 +148,7 @@ export class StripeService {
         base: {
           color: null,
           fontFamily:
-            '"Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif, ' +
+            'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif, ' +
             '"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
           fontSize: "16px",
           fontSmoothing: "antialiased",
