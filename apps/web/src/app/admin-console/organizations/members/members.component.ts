b47ee8635810c7f8179616f8e867249a09a7218e
Hinton <hinton@users.noreply.github.com>
2025-05-05 12:11:34 +0200

    Cleanup migrated components

--- apps/web/src/app/admin-console/organizations/members/members.component.ts
+++ apps/web/src/app/admin-console/organizations/members/members.component.ts
@@ -1,6 +1,6 @@
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
-import { Component, OnInit, ViewChild, ViewContainerRef } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { ActivatedRoute, Router } from "@angular/router";
 import {
@@ -92,9 +92,6 @@ class MembersTableDataSource extends PeopleTableDataSource<OrganizationUserView>
   templateUrl: "members.component.html",
 })
 export class MembersComponent extends BaseMembersComponent<OrganizationUserView> implements OnInit {
-  @ViewChild("resetPasswordTemplate", { read: ViewContainerRef, static: true })
-  resetPasswordModalRef: ViewContainerRef;
-
   userType = OrganizationUserType;
   userStatusType = OrganizationUserStatusType;
   memberTab = MemberDialogTab;
