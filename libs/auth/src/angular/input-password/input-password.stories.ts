b4cfffc842ec353bf89724601e6a44d2477f26a0
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-05-06 15:18:28 -0700

    [PM-18721] update Stories and enum comment

--- libs/auth/src/angular/input-password/input-password.stories.ts
+++ libs/auth/src/angular/input-password/input-password.stories.ts
@@ -152,15 +152,6 @@ export default {
 
 type Story = StoryObj<InputPasswordComponent>;
 
-export const ChangePasswordDelegation: Story = {
-  render: (args) => ({
-    props: args,
-    template: `
-      <auth-input-password [flow]="InputPasswordFlow.ChangePasswordDelegation"></auth-input-password>
-    `,
-  }),
-};
-
 export const SetInitialPasswordAccountRegistration: Story = {
   render: (args) => ({
     props: args,
@@ -199,12 +190,23 @@ export const ChangePasswordWithOptionalUserKeyRotation: Story = {
   }),
 };
 
+export const ChangePasswordDelegation: Story = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <auth-input-password [flow]="InputPasswordFlow.ChangePasswordDelegation"></auth-input-password>
+      <br />
+      <div>Note: no buttons here as this flow is expected to be used in a dialog, which will have its own buttons</div>
+    `,
+  }),
+};
+
 export const WithPolicies: Story = {
   render: (args) => ({
     props: args,
     template: `
       <auth-input-password
-        [flow]="InputPasswordFlow.SetInitialPasswordAuthedUser"
+        [flow]="InputPasswordFlow.SetInitialPasswordAccountRegistration"
         [masterPasswordPolicyOptions]="masterPasswordPolicyOptions"
       ></auth-input-password>
     `,
