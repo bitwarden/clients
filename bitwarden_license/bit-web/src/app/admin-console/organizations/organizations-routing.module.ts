da91bfcd3b3ddb6f8e13f034590294f7e6ac56e2
Ike Kottlowski <ikottlowski@bitwarden.com>
2025-05-07 22:14:39 -0400

    fix: deep-link directory path for imports.

--- bitwarden_license/bit-web/src/app/admin-console/organizations/organizations-routing.module.ts
+++ bitwarden_license/bit-web/src/app/admin-console/organizations/organizations-routing.module.ts
@@ -8,7 +8,7 @@ import { ConfigService } from "@bitwarden/common/platform/abstractions/config/co
 import { isEnterpriseOrgGuard } from "@bitwarden/web-vault/app/admin-console/organizations/guards/is-enterprise-org.guard";
 import { organizationPermissionsGuard } from "@bitwarden/web-vault/app/admin-console/organizations/guards/org-permissions.guard";
 import { OrganizationLayoutComponent } from "@bitwarden/web-vault/app/admin-console/organizations/layouts/organization-layout.component";
-import { deepLinkGuard } from "@bitwarden/web-vault/app/auth/guards/deep-link.guard";
+import { deepLinkGuard } from "@bitwarden/web-vault/app/auth/guards/deep-link/deep-link.guard";
 
 import { SsoComponent } from "../../auth/sso/sso.component";
 
