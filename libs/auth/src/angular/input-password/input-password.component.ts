4885f04fe6348038791a3fd880a7610033d7952e
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-05-14 03:37:23 -0700

    [Account Recovery][PM-18721] pass message as @Input

--- libs/auth/src/angular/input-password/input-password.component.ts
+++ libs/auth/src/angular/input-password/input-password.component.ts
@@ -125,6 +125,7 @@ export class InputPasswordComponent implements OnInit {
   @Input() userId?: UserId;
   @Input() loading = false;
   @Input() masterPasswordPolicyOptions: MasterPasswordPolicyOptions | null = null;
+  @Input() message: string = "";
 
   @Input() inlineButtons = false;
   @Input() primaryButtonText?: Translation;
