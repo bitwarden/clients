c6e48684939c411db18ab74737d81136d9f5056f
Bryan Cunningham <bryan.cunningham@me.com>
2025-05-15 10:44:50 -0400

    Enhance avatar docs

--- libs/components/src/avatar/avatar.component.ts
+++ libs/components/src/avatar/avatar.component.ts
@@ -16,6 +16,24 @@ const SizeClasses: Record<SizeTypes, string[]> = {
   xsmall: ["tw-h-6", "tw-w-6"],
 };
 
+/**
+  * Avatars display a unique color that helps a user visually recognize their logged in account.
+
+  * A variance in color across the avatar component is important as it is used in Account Switching as a
+  * visual indicator to recognize which of a personal or work account a user is logged into.
+  * 
+  * Example usage
+  * 
+  * ```html
+  * <bit-avatar
+  *   [color]="#175ddc"
+  *   [id]="some-id"
+  *   [text]="Walt Walterson"
+  *   [size]="default"
+  * >
+  * </bit-avatar>
+  * ```
+*/
 @Component({
   selector: "bit-avatar",
   template: `@if (src) {
