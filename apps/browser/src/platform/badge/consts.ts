6c499efd0fb70a55cbbaf7e2b98f5be1cc668e98
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-05-15 15:51:42 +0200

    fix: default icon

--- apps/browser/src/platform/badge/consts.ts
+++ apps/browser/src/platform/badge/consts.ts
@@ -4,7 +4,7 @@ export const DefaultBadgeState: RawBadgeState = {
   text: "",
   backgroundColor: "#294e5f",
   icon: {
-    19: "DefaultIcon19",
-    38: "DefaultIcon38",
+    19: "/images/icon19_gray.png",
+    38: "/images/icon38_gray.png",
   },
 };
