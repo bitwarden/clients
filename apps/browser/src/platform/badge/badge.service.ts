76d41abe4b7eef61dd1d71ac9ea12a0ff9d1ce09
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-05-15 12:53:26 +0200

    feat: add docs to badge service functions

--- apps/browser/src/platform/badge/badge.service.ts
+++ apps/browser/src/platform/badge/badge.service.ts
@@ -8,12 +8,32 @@ export class BadgeService {
 
   constructor() {}
 
+  /**
+   * Inform badge service of a new state that the badge should reflect.
+
+   * This will merge the new state with any existing states.
+   * If the new state has a higher priority, it will override any lower priority states.
+   * If the new state has a lower priority, it will be ignored.
+   * If the name of the state is already in use, it will be updated.
+   *
+   * @param name The name of the state. This is used to identify the state and will be used to clear it later.
+   * @param priority The priority of the state (higher numbers are higher priority, but setting arbitrary numbers is not supported).
+   * @param state The state to set.
+   */
   async setState(name: string, priority: BadgeStatePriority, state: BadgeState) {
     this.states[name] = { priority, state };
 
     await BadgeBrowserApi.setState(this.calculateState());
   }
 
+  /**
+   * Clear the state with the given name.
+   *
+   * This will remove the state from the badge service and clear it from the badge.
+   * If the state is not found, nothing will happen.
+   *
+   * @param name The name of the state to clear.
+   */
   async clearState(name: string) {
     delete this.states[name];
 
