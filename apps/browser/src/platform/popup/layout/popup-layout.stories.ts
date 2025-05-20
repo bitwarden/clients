3631c561b6dd9bdb2a60e4901d092219b4266d33
Vicki League <vleague@bitwarden.com>
2025-05-20 09:57:02 -0400

    remove strict mode warning

--- apps/browser/src/platform/popup/layout/popup-layout.stories.ts
+++ apps/browser/src/platform/popup/layout/popup-layout.stories.ts
@@ -1,5 +1,3 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
 import { ScrollingModule } from "@angular/cdk/scrolling";
 import { CommonModule } from "@angular/common";
 import { Component, importProvidersFrom } from "@angular/core";
