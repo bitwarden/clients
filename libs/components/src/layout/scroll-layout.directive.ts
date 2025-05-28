ad1259776be7177fa462a604ac899ace9f272ba9
Will Martin <contact@willmartian.com>
2025-05-28 11:43:40 -0400

    use directives instead of querySelector (#14950)
    * remove references to main el
    
    * wip
    
    * banish querySelector to the shadow realm
    
    * revert apps/ files

--- libs/components/src/layout/scroll-layout.directive.ts
+++ libs/components/src/layout/scroll-layout.directive.ts
@@ -1,39 +1,77 @@
 import { Directionality } from "@angular/cdk/bidi";
 import { CdkVirtualScrollable, ScrollDispatcher, VIRTUAL_SCROLLABLE } from "@angular/cdk/scrolling";
-import { Directive, ElementRef, NgZone, Optional } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  Injectable,
+  NgZone,
+  OnDestroy,
+  OnInit,
+  Optional,
+  inject,
+} from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+export class ScrollLayoutService {
+  private _scrollableRef: ElementRef<HTMLElement> | null = null;
+
+  get scrollableRef(): ElementRef<HTMLElement> | null {
+    if (!this._scrollableRef) {
+      // eslint-disable-next-line no-console
+      console.error("No scrollable ref provided by ScrollLayoutHostDirective.");
+    }
+    return this._scrollableRef;
+  }
+
+  set scrollableRef(value: ElementRef<HTMLElement> | null) {
+    this._scrollableRef = value;
+  }
+}
+
+@Directive({
+  selector: "[bitScrollLayoutHost]",
+  standalone: true,
+  host: {
+    class: "cdk-virtual-scrollable",
+  },
+})
+export class ScrollLayoutHostDirective implements OnDestroy {
+  private ref = inject(ElementRef);
+  private service = inject(ScrollLayoutService);
+
+  constructor() {
+    this.service.scrollableRef = this.ref as ElementRef<HTMLElement>;
+  }
+
+  ngOnDestroy(): void {
+    this.service.scrollableRef = null;
+  }
+}
 
 @Directive({
   selector: "[bitScrollLayout]",
   standalone: true,
   providers: [{ provide: VIRTUAL_SCROLLABLE, useExisting: ScrollLayoutDirective }],
 })
-export class ScrollLayoutDirective extends CdkVirtualScrollable {
-  private scrollableRef: ElementRef<HTMLElement>;
-
-  constructor(scrollDispatcher: ScrollDispatcher, ngZone: NgZone, @Optional() dir: Directionality) {
-    const scrollableEl = document.querySelector<HTMLElement>(
-      ".bit-virtual-scrollable.cdk-virtual-scrollable",
-    )!;
-    if (!scrollableEl) {
-      // eslint-disable-next-line no-console
-      console.error(
-        "Element with `.bit-virtual-scrollable.cdk-virtual-scrollable` must be an ancestor of [bitScrollLayout]",
-      );
-    }
-    const scrollableRef = new ElementRef(scrollableEl);
-    super(scrollableRef, scrollDispatcher, ngZone, dir);
-    this.scrollableRef = scrollableRef;
+export class ScrollLayoutDirective extends CdkVirtualScrollable implements OnInit {
+  constructor(
+    scrollDispatcher: ScrollDispatcher,
+    ngZone: NgZone,
+    @Optional() dir: Directionality,
+    private service: ScrollLayoutService,
+  ) {
+    super(service.scrollableRef!, scrollDispatcher, ngZone, dir);
   }
 
   override getElementRef(): ElementRef<HTMLElement> {
-    return this.scrollableRef;
+    return this.service.scrollableRef!;
   }
 
   override measureBoundingClientRectWithScrollOffset(
     from: "left" | "top" | "right" | "bottom",
   ): number {
     return (
-      this.scrollableRef.nativeElement.getBoundingClientRect()[from] -
+      this.service.scrollableRef!.nativeElement.getBoundingClientRect()[from] -
       this.measureScrollOffset(from)
     );
   }
