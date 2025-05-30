65919543608e93d87687e4eb0132d50ddba48b75
William Martin <contact@willmartian.com>
2025-05-30 01:50:56 -0400

    run eslint

--- libs/components/src/layout/layout.component.ts
+++ libs/components/src/layout/layout.component.ts
@@ -8,6 +8,7 @@ import { DrawerService } from "../drawer/drawer.service";
 import { LinkModule } from "../link";
 import { SideNavService } from "../navigation/side-nav.service";
 import { SharedModule } from "../shared";
+
 import { ScrollLayoutHostDirective } from "./scroll-layout.directive";
 
 @Component({
