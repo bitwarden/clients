663d3388405ffa34621c5df4cddf9cbd9bea5665
Mark Youssef <myoussef@bitwarden.com>
2025-06-02 16:44:11 -0700

    Close side nav when breakpoint changes

--- libs/components/src/navigation/side-nav.component.ts
+++ libs/components/src/navigation/side-nav.component.ts
@@ -1,6 +1,9 @@
 import { CdkTrapFocus } from "@angular/cdk/a11y";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 import { CommonModule } from "@angular/common";
-import { Component, ElementRef, input, viewChild } from "@angular/core";
+import { Component, ElementRef, OnInit, OnDestroy, input, viewChild } from "@angular/core";
+import { Subject } from "rxjs";
+import { takeUntil, distinctUntilChanged } from "rxjs/operators";
 
 import { I18nPipe } from "@bitwarden/ui-common";
 
@@ -16,12 +19,34 @@ export type SideNavVariant = "primary" | "secondary";
   templateUrl: "side-nav.component.html",
   imports: [CommonModule, CdkTrapFocus, NavDividerComponent, BitIconButtonComponent, I18nPipe],
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit, OnDestroy {
   readonly variant = input<SideNavVariant>("primary");
 
   private readonly toggleButton = viewChild("toggleButton", { read: ElementRef });
 
-  constructor(protected sideNavService: SideNavService) {}
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    protected sideNavService: SideNavService,
+    private breakpointObserver: BreakpointObserver,
+  ) {}
+
+  ngOnInit() {
+    // Monitor breakpoint changes and close nav when switching to small viewport
+    this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.XSmall])
+      .pipe(distinctUntilChanged(), takeUntil(this.destroy$))
+      .subscribe((result) => {
+        if (result.matches) {
+          this.sideNavService.setClose();
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   protected handleKeyDown = (event: KeyboardEvent) => {
     if (event.key === "Escape") {
