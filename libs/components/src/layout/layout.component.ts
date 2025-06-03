b0b2ccb3cf770f28080f77d97b37a1166d7f8a36
William Martin <contact@willmartian.com>
2025-06-03 00:14:39 -0400

    fix skip links bug

--- libs/components/src/layout/layout.component.ts
+++ libs/components/src/layout/layout.component.ts
@@ -25,14 +25,42 @@ import { ScrollLayoutHostDirective } from "./scroll-layout.directive";
     CdkTrapFocus,
     ScrollLayoutHostDirective,
   ],
+  host: {
+    "(document:keydown)": "handleInitialTabPress($event)",
+  },
 })
 export class LayoutComponent {
   protected sideNavService = inject(SideNavService);
   protected drawerPortal = inject(DrawerService).portal;
 
   private mainContent = viewChild.required<ElementRef<HTMLElement>>("main");
-
   protected focusMainContent() {
     this.mainContent().nativeElement.focus();
   }
+
+  /**
+   * Angular CDK's focus trap utility is silly and will not focus respect focus order.
+   * This is a workaround to explicitly focus the skip link when tab is first pressed, if no other item already has focus.
+   *
+   * @see https://github.com/angular/components/issues/10247#issuecomment-384060265
+   **/
+  private skipLink = viewChild.required<ElementRef<HTMLElement>>("skipLink");
+  private didInitialFocus = false;
+  handleInitialTabPress(ev: KeyboardEvent) {
+    if (this.didInitialFocus) {
+      return;
+    }
+
+    if (isNothingFocused()) {
+      ev.preventDefault();
+      this.skipLink().nativeElement.focus();
+    }
+    this.didInitialFocus = true;
+  }
 }
+
+const isNothingFocused = (): boolean => {
+  return [document.documentElement, document.body, null].includes(
+    document.activeElement as HTMLElement,
+  );
+};
