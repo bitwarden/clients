3a5ee07bf0fc25340d0894d17f8d0e7095e4ab97
Daniel James Smith <djsmith85@users.noreply.github.com>
2025-06-05 19:46:48 +0200

    Rename encrypted-file.xml to noop-encrypted-file due to a source type with the same name

--- libs/importer/src/importers/spec-data/password-depot-xml/index.ts
+++ libs/importer/src/importers/spec-data/password-depot-xml/index.ts
@@ -1,7 +1,7 @@
 export { InvalidRootNodeData } from "./missing-root-node.xml";
 export { MissingPasswordsNodeData } from "./missing-passwords-node.xml";
 export { InvalidVersionData } from "./wrong-version.xml";
-export { EncryptedFileData } from "./encrypted-file.xml";
+export { EncryptedFileData } from "./noop-encrypted-file.xml";
 export { PasswordTestData } from "./password.xml";
 export { CreditCardTestData } from "./credit-card.xml";
 export { IdentityTestData } from "./identity.xml";
