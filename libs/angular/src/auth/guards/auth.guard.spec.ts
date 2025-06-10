1faca790bdac44c2e9ae62ad6bc422fff89c6c0b
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-06-10 13:32:24 -0700

    [PM-18457] (PR feedback) add tests to authGuard

--- libs/angular/src/auth/guards/auth.guard.spec.ts
+++ libs/angular/src/auth/guards/auth.guard.spec.ts
@@ -13,8 +13,10 @@ import {
 import { AuthService } from "@bitwarden/common/auth/abstractions/auth.service";
 import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
 import { ForceSetPasswordReason } from "@bitwarden/common/auth/models/domain/force-set-password-reason";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
 import { KeyConnectorService } from "@bitwarden/common/key-management/key-connector/abstractions/key-connector.service";
 import { MasterPasswordServiceAbstraction } from "@bitwarden/common/key-management/master-password/abstractions/master-password.service.abstraction";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { MessagingService } from "@bitwarden/common/platform/abstractions/messaging.service";
 import { UserId } from "@bitwarden/common/types/guid";
 
@@ -25,6 +27,7 @@ describe("AuthGuard", () => {
     authStatus: AuthenticationStatus,
     forceSetPasswordReason: ForceSetPasswordReason,
     keyConnectorServiceRequiresAccountConversion: boolean = false,
+    featureFlag: FeatureFlag | null = null,
   ) => {
     const authService: MockProxy<AuthService> = mock<AuthService>();
     authService.getAuthStatus.mockResolvedValue(authStatus);
@@ -32,6 +35,7 @@ describe("AuthGuard", () => {
     const keyConnectorService: MockProxy<KeyConnectorService> = mock<KeyConnectorService>();
     keyConnectorService.convertAccountRequired$ = of(keyConnectorServiceRequiresAccountConversion);
     const accountService: MockProxy<AccountService> = mock<AccountService>();
+    const configService: MockProxy<ConfigService> = mock<ConfigService>();
     const activeAccountSubject = new BehaviorSubject<Account | null>(null);
     accountService.activeAccount$ = activeAccountSubject;
     activeAccountSubject.next(
@@ -44,6 +48,12 @@ describe("AuthGuard", () => {
         { id: "test-id" as UserId },
       ),
     );
+
+    if (featureFlag) {
+      configService.getFeatureFlag.mockResolvedValue(true);
+    } else {
+      configService.getFeatureFlag.mockResolvedValue(false);
+    }
 
     const forceSetPasswordReasonSubject = new BehaviorSubject<ForceSetPasswordReason>(
       forceSetPasswordReason,
@@ -59,6 +69,7 @@ describe("AuthGuard", () => {
           { path: "guarded-route", component: EmptyComponent, canActivate: [authGuard] },
           { path: "lock", component: EmptyComponent },
           { path: "set-password", component: EmptyComponent },
+          { path: "set-initial-password", component: EmptyComponent },
           { path: "update-temp-password", component: EmptyComponent },
           { path: "remove-password", component: EmptyComponent },
         ]),
@@ -68,6 +79,7 @@ describe("AuthGuard", () => {
         { provide: MessagingService, useValue: messagingService },
         { provide: KeyConnectorService, useValue: keyConnectorService },
         { provide: AccountService, useValue: accountService },
+        { provide: ConfigService, useValue: configService },
         { provide: MasterPasswordServiceAbstraction, useValue: masterPasswordService },
       ],
     });
@@ -118,6 +130,28 @@ describe("AuthGuard", () => {
 
     await router.navigate(["guarded-route"]);
     expect(router.url).toContain("/set-password");
+  });
+
+  it("should redirect to set-initial-password when user is TDE user without password and has password reset permission", async () => {
+    const { router } = setup(
+      AuthenticationStatus.Unlocked,
+      ForceSetPasswordReason.TdeUserWithoutPasswordHasPasswordResetPermission,
+      false,
+      FeatureFlag.PM16117_SetInitialPasswordRefactor,
+    );
+
+    await router.navigate(["guarded-route"]);
+    expect(router.url).toContain("/set-initial-password");
+  });
+
+  it("should allow navigation to set-initial-password when the user is unlocked, is a TDE user without password, and has password reset permission", async () => {
+    const { router } = setup(
+      AuthenticationStatus.Unlocked,
+      ForceSetPasswordReason.TdeUserWithoutPasswordHasPasswordResetPermission,
+    );
+
+    await router.navigate(["/set-initial-password"]);
+    expect(router.url).toContain("/set-initial-password");
   });
 
   it("should redirect to update-temp-password when user has force set password reason", async () => {
