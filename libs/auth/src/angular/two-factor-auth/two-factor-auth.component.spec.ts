7d509276728c9102a9ac4c8f97178bad761a16e9
Todd Martin <tmartin@bitwarden.com>
2025-06-11 13:09:50 -0400

    Fixed test.

--- libs/auth/src/angular/two-factor-auth/two-factor-auth.component.spec.ts
+++ libs/auth/src/angular/two-factor-auth/two-factor-auth.component.spec.ts
@@ -120,7 +120,6 @@ describe("TwoFactorAuthComponent", () => {
 
     mockTwoFactorAuthCompCacheService = mock<TwoFactorAuthComponentCacheService>();
     mockTwoFactorAuthCompCacheService.getCachedData.mockReturnValue(null);
-    mockTwoFactorAuthCompCacheService.init.mockResolvedValue();
 
     mockUserDecryptionOpts = {
       noMasterPassword: new UserDecryptionOptions({
