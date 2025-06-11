f22f28eb973043bc0f1ddc42baae68e6c6436388
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-06-11 16:15:13 -0700

    [PM-18457] add clarifying comment to WebSetInitialPasswordService

--- apps/web/src/app/auth/core/services/password-management/set-initial-password/web-set-initial-password.service.ts
+++ apps/web/src/app/auth/core/services/password-management/set-initial-password/web-set-initial-password.service.ts
@@ -56,8 +56,27 @@ export class WebSetInitialPasswordService
   ) {
     await super.setInitialPassword(credentials, userType, userId);
 
-    // SSO JIT accepts org invites when setting their MP, meaning
-    // we can clear the deep linked url for accepting it.
+    /**
+     * TODO: Investigate refactoring the following logic in https://bitwarden.atlassian.net/browse/PM-22615
+     * ---
+     * When a user has been invited to an org, they can be accepted into the org in two different ways:
+     *
+     *  1) By clicking the email invite link, which triggers the normal AcceptOrganizationComponent flow
+     *     a. This flow sets an org invite in state
+     *     b. However, if the user does not already have an account AND the org has SSO enabled AND the require
+     *        SSO policy enabled, the AcceptOrganizationComponent will send the user to /sso to accelerate
+     *        the user through the SSO JIT provisioning process (see #2 below)
+     *
+     *  2) By logging in via SSO, which triggers the JIT provisioning process
+     *     a. This flow does NOT (itself) set an org invite in state
+     *     b. The set initial password process on the server accepts the user into the org after successfully
+     *        setting the password (see server - SetInitialMasterPasswordCommand.cs)
+     *
+     * If a user clicks the email link but gets accelerated through the SSO JIT process (see 1b),
+     * the SSO JIT process will accept the user into the org upon setting their initial password (see 2b),
+     * at which point we must remember to clear the deep linked URL used for accepting the org invite, as well
+     * as clear the org invite itself that was originally set in state by the AcceptOrganizationComponent.
+     */
     await this.routerService.getAndClearLoginRedirectUrl();
     await this.acceptOrganizationInviteService.clearOrganizationInvitation();
   }
