ed58ff94c614c6d2b9ffbb886bec0b5d6c81b0c7
Hinton <hinton@users.noreply.github.com>
2025-06-12 19:15:52 +0200

    Ignore existing violations, will be handled by teams

--- apps/browser/src/key-management/lock/services/extension-lock-component.service.spec.ts
+++ apps/browser/src/key-management/lock/services/extension-lock-component.service.spec.ts
@@ -18,7 +18,11 @@ import {
 import { UnlockOptions } from "@bitwarden/key-management-ui";
 
 import { BrowserApi } from "../../../platform/browser/browser-api";
+// FIXME (PM-22628): Popup imports are forbidden in background
+// eslint-disable-next-line no-restricted-imports
 import BrowserPopupUtils from "../../../platform/popup/browser-popup-utils";
+// FIXME (PM-22628): Popup imports are forbidden in background
+// eslint-disable-next-line no-restricted-imports
 import { BrowserRouterService } from "../../../platform/popup/services/browser-router.service";
 
 import { ExtensionLockComponentService } from "./extension-lock-component.service";
--- apps/browser/src/key-management/lock/services/extension-lock-component.service.ts
+++ apps/browser/src/key-management/lock/services/extension-lock-component.service.ts
@@ -15,7 +15,11 @@ import { LockComponentService, UnlockOptions } from "@bitwarden/key-management-u
 
 import { BiometricErrors, BiometricErrorTypes } from "../../../models/biometricErrors";
 import { BrowserApi } from "../../../platform/browser/browser-api";
+// FIXME (PM-22628): Popup imports are forbidden in background
+// eslint-disable-next-line no-restricted-imports
 import BrowserPopupUtils from "../../../platform/popup/browser-popup-utils";
+// FIXME (PM-22628): Popup imports are forbidden in background
+// eslint-disable-next-line no-restricted-imports
 import { BrowserRouterService } from "../../../platform/popup/services/browser-router.service";
 
 export class ExtensionLockComponentService implements LockComponentService {
