7c954bcf4d5cb8752cf308aa9ebb1903f40eedf7
jaasen-livefront <jaasen@livefront.com>
2025-06-12 14:13:26 -0700

    use CIPHER_MENU_ITEMS

--- apps/desktop/src/vault/app/vault/vault-filter/filters/type-filter.component.ts
+++ apps/desktop/src/vault/app/vault/vault-filter/filters/type-filter.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { map } from "rxjs";
 
 import { TypeFilterComponent as BaseTypeFilterComponent } from "@bitwarden/angular/vault/vault-filter/components/type-filter.component";
-import { CipherType } from "@bitwarden/sdk-internal";
+import { CIPHER_MENU_ITEMS } from "@bitwarden/common/vault/types/cipher-menu-items";
 import { RestrictedItemTypesService } from "@bitwarden/vault";
 
 @Component({
@@ -11,43 +11,10 @@ import { RestrictedItemTypesService } from "@bitwarden/vault";
   standalone: false,
 })
 export class TypeFilterComponent extends BaseTypeFilterComponent {
-  typeFilters = [
-    {
-      id: "login",
-      name: "typeLogin",
-      type: CipherType.Login,
-      icon: "bwi-globe",
-    },
-    {
-      id: "card",
-      name: "typeCard",
-      type: CipherType.Card,
-      icon: "bwi-credit-card",
-    },
-    {
-      id: "identity",
-      name: "typeIdentity",
-      type: CipherType.Identity,
-      icon: "bwi-id-card",
-    },
-    {
-      id: "note",
-      name: "typeSecureNote",
-      type: CipherType.SecureNote,
-      icon: "bwi-sticky-note",
-    },
-    {
-      id: "sshKey",
-      name: "typeSshKey",
-      type: CipherType.SshKey,
-      icon: "bwi-key",
-    },
-  ];
-
   protected typeFilters$ = this.restrictedItemTypesService.restricted$.pipe(
     map((restrictedItemTypes) =>
       // Filter out restricted item types from the typeFilters array
-      this.typeFilters.filter(
+      CIPHER_MENU_ITEMS.filter(
         (typeFilter) =>
           !restrictedItemTypes.some(
             (restrictedType) =>
