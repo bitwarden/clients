cd7afe8aa303de1e39f21835fe5e23137a578157
Bryan Cunningham <bryan.cunningham@me.com>
2025-06-17 11:42:47 -0400

    add new logo lockups

---
+++ libs/components/src/icon/logos/bitwarden/index.ts
@@ -0,0 +1,3 @@
+export { default as AdminConsoleLogo } from "./admin-console";
+export { default as PasswordManagerLogo } from "./password-manager";
+export { default as SecretsManagerLogo } from "./secrets-manager";
