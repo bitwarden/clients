4faca92387a75c5fbf10b3daee26bf30b4467d76
Nick Krantz <nick@livefront.com>
2025-06-23 13:41:21 -0500

    add initial loading state

--- apps/web/src/app/vault/components/setup-extension/setup-extension.component.spec.ts
+++ apps/web/src/app/vault/components/setup-extension/setup-extension.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 
 import { SetupExtensionComponent } from "./setup-extension.component";
 
 describe("SetupExtensionComponent", () => {
   let fixture: ComponentFixture<SetupExtensionComponent>;
-  let component: SetupExtensionComponent;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({}).compileComponents();
+    await TestBed.configureTestingModule({
+      providers: [{ provide: I18nService, useValue: { t: (key: string) => key } }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SetupExtensionComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it("creates", () => {
-    expect(component).toBeTruthy();
+  it("initially shows the loading spinner", () => {
+    const spinner = fixture.debugElement.query(By.css("i"));
+
+    expect(spinner.nativeElement.title).toBe("loading");
   });
 });
--- apps/web/src/app/vault/components/setup-extension/setup-extension.component.ts
+++ apps/web/src/app/vault/components/setup-extension/setup-extension.component.ts
@@ -1,7 +1,21 @@
+import { NgIf } from "@angular/common";
 import { Component } from "@angular/core";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { UnionOfValues } from "@bitwarden/common/vault/types/union-of-values";
+
+const SetupExtensionState = {
+  Loading: "loading",
+} as const;
+
+type SetupExtensionState = UnionOfValues<typeof SetupExtensionState>;
 
 @Component({
   selector: "vault-setup-extension",
   templateUrl: "./setup-extension.component.html",
+  imports: [NgIf, JslibModule],
 })
-export class SetupExtensionComponent {}
+export class SetupExtensionComponent {
+  protected state: SetupExtensionState = SetupExtensionState.Loading;
+  protected SetupExtensionState = SetupExtensionState;
+}
