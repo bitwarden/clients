3fe165e406444387f7e7a2af1fc95d59cb0c0ba9
Nick Krantz <nick@livefront.com>
2025-06-23 14:53:59 -0500

    conditionally redirect the user upon initialization

--- apps/web/src/app/vault/components/setup-extension/setup-extension.component.spec.ts
+++ apps/web/src/app/vault/components/setup-extension/setup-extension.component.spec.ts
@@ -1,19 +1,35 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
+import { Router } from "@angular/router";
 
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { Utils } from "@bitwarden/common/platform/misc/utils";
 
 import { SetupExtensionComponent } from "./setup-extension.component";
 
 describe("SetupExtensionComponent", () => {
   let fixture: ComponentFixture<SetupExtensionComponent>;
+  let component: SetupExtensionComponent;
+
+  const getFeatureFlag = jest.fn().mockResolvedValue(false);
+  const navigate = jest.fn().mockResolvedValue(true);
 
   beforeEach(async () => {
+    navigate.mockClear();
+    getFeatureFlag.mockClear().mockResolvedValue(true);
+
     await TestBed.configureTestingModule({
-      providers: [{ provide: I18nService, useValue: { t: (key: string) => key } }],
+      providers: [
+        { provide: I18nService, useValue: { t: (key: string) => key } },
+        { provide: ConfigService, useValue: { getFeatureFlag } },
+        { provide: Router, useValue: { navigate } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SetupExtensionComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
@@ -21,5 +37,38 @@ describe("SetupExtensionComponent", () => {
     const spinner = fixture.debugElement.query(By.css("i"));
 
     expect(spinner.nativeElement.title).toBe("loading");
+  });
+
+  describe("initialization", () => {
+    it("redirects to the vault if the feature flag is disabled", async () => {
+      Utils.isMobileBrowser = false;
+      getFeatureFlag.mockResolvedValue(false);
+      navigate.mockClear();
+
+      await component.ngOnInit();
+
+      expect(navigate).toHaveBeenCalledWith(["/vault"]);
+    });
+
+    it("redirects to the vault if the user is on a mobile browser", async () => {
+      Utils.isMobileBrowser = true;
+      getFeatureFlag.mockResolvedValue(true);
+      navigate.mockClear();
+
+      await component.ngOnInit();
+
+      expect(navigate).toHaveBeenCalledWith(["/vault"]);
+    });
+
+    it("does not redirect the user", async () => {
+      Utils.isMobileBrowser = false;
+      getFeatureFlag.mockResolvedValue(true);
+      navigate.mockClear();
+
+      await component.ngOnInit();
+
+      expect(getFeatureFlag).toHaveBeenCalledWith(FeatureFlag.PM19315EndUserActivationMvp);
+      expect(navigate).not.toHaveBeenCalled();
+    });
   });
 });
--- apps/web/src/app/vault/components/setup-extension/setup-extension.component.ts
+++ apps/web/src/app/vault/components/setup-extension/setup-extension.component.ts
@@ -1,7 +1,11 @@
 import { NgIf } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
+import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { UnionOfValues } from "@bitwarden/common/vault/types/union-of-values";
 
 const SetupExtensionState = {
@@ -15,7 +19,28 @@ type SetupExtensionState = UnionOfValues<typeof SetupExtensionState>;
   templateUrl: "./setup-extension.component.html",
   imports: [NgIf, JslibModule],
 })
-export class SetupExtensionComponent {
-  protected state: SetupExtensionState = SetupExtensionState.Loading;
+export class SetupExtensionComponent implements OnInit {
+  private configService = inject(ConfigService);
+  private router = inject(Router);
+
   protected SetupExtensionState = SetupExtensionState;
+  /**
+   * The current state of the setup extension component.
+   */
+  protected state: SetupExtensionState = SetupExtensionState.Loading;
+
+  async ngOnInit() {
+    await this.conditionallyRedirectUser();
+  }
+
+  async conditionallyRedirectUser() {
+    const isFeatureEnabled = await this.configService.getFeatureFlag(
+      FeatureFlag.PM19315EndUserActivationMvp,
+    );
+    const isMobile = Utils.isMobileBrowser;
+
+    if (!isFeatureEnabled || isMobile) {
+      await this.router.navigate(["/vault"]);
+    }
+  }
 }
