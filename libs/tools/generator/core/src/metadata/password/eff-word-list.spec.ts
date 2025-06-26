53511e44dbda04e78a6b9e3c70cfa8cea54c23aa
adudek-bw <adudek@bitwarden.com>
2025-06-26 09:59:46 -0400

    fix unit tests

--- libs/tools/generator/core/src/metadata/password/eff-word-list.spec.ts
+++ libs/tools/generator/core/src/metadata/password/eff-word-list.spec.ts
@@ -23,7 +23,7 @@ describe("password - eff words generator metadata", () => {
   });
 
   describe("engine.create", () => {
-    var nonSdkDependencyProvider = dependencyProvider;
+    const nonSdkDependencyProvider = dependencyProvider;
     nonSdkDependencyProvider.sdk = undefined;
     it("returns an email randomizer", () => {
       expect(effPassphrase.engine.create(nonSdkDependencyProvider)).toBeInstanceOf(
