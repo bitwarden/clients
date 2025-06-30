6b9eb30d9f3630dd19d50848b76c6a48ec979e92
Nick Krantz <nick@livefront.com>
2025-06-30 15:28:57 -0500

    add support for mobile viewports

--- apps/web/src/app/vault/components/setup-extension/setup-extension.component.ts
+++ apps/web/src/app/vault/components/setup-extension/setup-extension.component.ts
@@ -1,5 +1,5 @@
-import { NgIf } from "@angular/common";
-import { Component, DestroyRef, inject, OnInit } from "@angular/core";
+import { DOCUMENT, NgIf } from "@angular/common";
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Router, RouterModule } from "@angular/router";
 import { pairwise, startWith } from "rxjs";
@@ -46,13 +46,14 @@ type SetupExtensionState = UnionOfValues<typeof SetupExtensionState>;
     AddExtensionVideosComponent,
   ],
 })
-export class SetupExtensionComponent implements OnInit {
+export class SetupExtensionComponent implements OnInit, OnDestroy {
   private webBrowserExtensionInteractionService = inject(WebBrowserInteractionService);
   private configService = inject(ConfigService);
   private router = inject(Router);
   private destroyRef = inject(DestroyRef);
   private platformUtilsService = inject(PlatformUtilsService);
   private dialogService = inject(DialogService);
+  private document = inject(DOCUMENT);
 
   protected SetupExtensionState = SetupExtensionState;
   protected PartyIcon = VaultIcons.Party;
@@ -65,8 +66,21 @@ export class SetupExtensionComponent implements OnInit {
 
   /** Reference to the add it later dialog */
   protected dialogRef: DialogRef | null = null;
+  private viewportContent: string | null = null;
 
   async ngOnInit() {
+    // It is not be uncommon for users to hit this page from smaller viewports.
+    // There are global styles that set a min-width for the page which cause it to render poorly.
+    // Remove them here.
+    // https://github.com/bitwarden/clients/blob/main/apps/web/src/scss/base.scss#L6
+    this.document.body.style.minWidth = "auto";
+
+    const viewportMeta = this.document.querySelector('meta[name="viewport"]');
+
+    // Save the current viewport content to reset it when the component is destroyed
+    this.viewportContent = viewportMeta?.getAttribute("content") ?? null;
+    viewportMeta?.setAttribute("content", "width=device-width, initial-scale=1.0");
+
     await this.conditionallyRedirectUser();
 
     this.webStoreUrl = getWebStoreUrl(this.platformUtilsService.getDevice());
@@ -92,6 +106,17 @@ export class SetupExtensionComponent implements OnInit {
       });
   }
 
+  ngOnDestroy(): void {
+    // Reset the body min-width when the component is destroyed
+    this.document.body.style.minWidth = "";
+
+    if (this.viewportContent !== null) {
+      this.document
+        .querySelector('meta[name="viewport"]')
+        ?.setAttribute("content", this.viewportContent);
+    }
+  }
+
   /** Conditionally redirects the user to the vault upon landing on the page. */
   async conditionallyRedirectUser() {
     const isFeatureEnabled = await this.configService.getFeatureFlag(
@@ -100,7 +125,7 @@ export class SetupExtensionComponent implements OnInit {
     const isMobile = Utils.isMobileBrowser;
 
     if (!isFeatureEnabled || isMobile) {
-      await this.router.navigate(["/vault"]);
+      // await this.router.navigate(["/vault"]);
     }
   }
 
