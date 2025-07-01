2a45fee7587b65cc74238f5d5121eca462548257
Cy Okeke <cokeke@bitwarden.com>
2025-07-01 18:18:07 +0100

    resolve the unit test failing

--- libs/common/src/billing/models/request/change-plan-frequency.request.ts
+++ libs/common/src/billing/models/request/change-plan-frequency.request.ts
@@ -2,4 +2,8 @@ import { PlanType } from "../../enums";
 
 export class ChangePlanFrequencyRequest {
   newPlanType: PlanType;
+
+  constructor(newPlanType?: PlanType) {
+    this.newPlanType = newPlanType!;
+  }
 }
