3a28e9f8407d3182cc2353006b901477ff7dbc73
Nick Krantz <nick@livefront.com>
2025-07-02 14:10:51 -0500

    mark cipher as required and remove ignore statements

--- libs/vault/src/components/totp-countdown/totp-countdown.component.ts
+++ libs/vault/src/components/totp-countdown/totp-countdown.component.ts
@@ -1,5 +1,3 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
 import { CommonModule } from "@angular/common";
 import {
   Component,
@@ -23,7 +21,7 @@ import { TypographyModule } from "@bitwarden/components";
   imports: [CommonModule, TypographyModule],
 })
 export class BitTotpCountdownComponent implements OnInit, OnChanges {
-  @Input() cipher: CipherView;
+  @Input({ required: true }) cipher!: CipherView;
   @Output() sendCopyCode = new EventEmitter();
 
   /**
