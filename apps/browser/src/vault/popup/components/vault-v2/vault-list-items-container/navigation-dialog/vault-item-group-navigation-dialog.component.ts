7971129d227464987dcd499026ae770814364ebe
William Martin <contact@willmartian.com>
2025-07-12 22:56:10 -0400

    add story and i18n

--- apps/browser/src/vault/popup/components/vault-v2/vault-list-items-container/navigation-dialog/vault-item-group-navigation-dialog.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/vault-list-items-container/navigation-dialog/vault-item-group-navigation-dialog.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 
-import { DialogModule } from "@bitwarden/components";
+import { ButtonModule, DialogModule, TypographyModule } from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
 
 @Component({
-  imports: [DialogModule, I18nPipe],
+  imports: [DialogModule, ButtonModule, TypographyModule, I18nPipe],
   templateUrl: "vault-item-group-navigation-dialog.component.html",
 })
 export class VaultItemGroupNavigationDialogComponent {}
