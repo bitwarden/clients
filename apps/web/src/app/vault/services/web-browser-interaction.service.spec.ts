30e266fb1356b67130b05cf15d2a65215bc28f9c
Nick Krantz <nick@livefront.com>
2025-07-16 14:18:56 -0500

    update timeouts of the web browser interaction

--- apps/web/src/app/vault/services/web-browser-interaction.service.spec.ts
+++ apps/web/src/app/vault/services/web-browser-interaction.service.spec.ts
@@ -38,7 +38,7 @@ describe("WebBrowserInteractionService", () => {
         expect(installed).toBe(false);
       });
 
-      tick(1500);
+      tick(150);
     }));
 
     it("returns true when the extension is installed", (done) => {
@@ -58,7 +58,7 @@ describe("WebBrowserInteractionService", () => {
       });
 
       // initial timeout, should emit false
-      tick(1500);
+      tick(150);
       expect(results[0]).toBe(false);
 
       tick(2500);
