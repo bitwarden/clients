8e22d7b35f12be70a3a4370de5f295c0b2432caa
Bryan Cunningham <bcunningham@bitwarden.com>
2025-07-17 15:52:36 -0400

    Update libs/components/src/container/container.component.ts
    Co-authored-by: Vicki League <vleague@bitwarden.com>

--- libs/components/src/container/container.component.ts
+++ libs/components/src/container/container.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 /**
- * bit-container is a minimally styled component that limits the max width of it's content to the tailwind theme variable '4xl'. '4xl' is equal to the value of 56rem
+ * bit-container is a minimally styled component that limits the max width of its content to the tailwind theme variable '4xl'. '4xl' is equal to the value of 56rem
  */
 @Component({
   selector: "bit-container",
