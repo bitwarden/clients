e44794a283a53d9c3684fc08634a6d15d4aeb59c
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-08-07 12:54:16 -0400

    One of each

--- libs/eslint/platform/required-using.spec.mjs
+++ libs/eslint/platform/required-using.spec.mjs
@@ -34,65 +34,65 @@ ruleTester.run("required-using", rule.default, {
         using client = rc.take();
       `,
     },
+    // {
+    //   name: "Function reference with `using`",
+    //   code: `
+    //     ${setup}
+    //     const t = rc.take;
+    //     using client = t();
+    //   `,
+    // },
+  ],
+  invalid: [
     {
-      name: "Function reference with `using`",
+      name: "Direct declaration without `using`",
       code: `
         ${setup}
-        const t = rc.take;
-        using client = t();
+        const client = rc.take();
       `,
+      errors: [
+        {
+          message: errorMessage,
+        },
+      ],
     },
+    // {
+    //   name: "Assignment without `using`",
+    //   code: `
+    //     ${setup}
+    //     let client;
+    //     client = rc.take();
+    //   `,
+    //   errors: [
+    //     {
+    //       message: errorMessage,
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: "Function reference without `using`",
+    //   code: `
+    //     ${setup}
+    //     const t = rc.take;
+    //     const client = t();
+    //   `,
+    //   errors: [
+    //     {
+    //       message: errorMessage,
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: "Destructuring without `using`",
+    //   code: `
+    //     ${setup}
+    //     const { value } = rc.take();
+    //   `,
+    //   errors: [
+    //     {
+    //       message: errorMessage,
+    //     },
+    //   ],
+    // },
   ],
-  // invalid: [
-  //   {
-  //     name: "Direct declaration without `using`",
-  //     code: `
-  //       ${setup}
-  //       const client = rc.take();
-  //     `,
-  //     errors: [
-  //       {
-  //         message: errorMessage,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: "Assignment without `using`",
-  //     code: `
-  //       ${setup}
-  //       let client;
-  //       client = rc.take();
-  //     `,
-  //     errors: [
-  //       {
-  //         message: errorMessage,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: "Function reference without `using`",
-  //     code: `
-  //       ${setup}
-  //       const t = rc.take;
-  //       const client = t();
-  //     `,
-  //     errors: [
-  //       {
-  //         message: errorMessage,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: "Destructuring without `using`",
-  //     code: `
-  //       ${setup}
-  //       const { value } = rc.take();
-  //     `,
-  //     errors: [
-  //       {
-  //         message: errorMessage,
-  //       },
-  //     ],
-  //   },
-  // ],
 });
