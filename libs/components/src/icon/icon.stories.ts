0e054fa18fa482ebcee52f97a23650b5f22f8458
Vicki League <vleague@bitwarden.com>
2025-08-21 15:36:37 -0400

    try these dimensions

--- libs/components/src/icon/icon.stories.ts
+++ libs/components/src/icon/icon.stories.ts
@@ -1,19 +1,12 @@
-import { Meta, moduleMetadata } from "@storybook/angular";
+import { Meta } from "@storybook/angular";
 
 import * as SvgIcons from "@bitwarden/assets/svg";
-
-import { TableDataSource, TableModule } from "../table";
 
 import { BitIconComponent } from "./icon.component";
 
 export default {
   title: "Component Library/Icon",
   component: BitIconComponent,
-  decorators: [
-    moduleMetadata({
-      imports: [TableModule],
-    }),
-  ],
   parameters: {
     design: {
       type: "figma",
@@ -53,32 +46,21 @@ function iconsMapped(): IconWithName[] {
   }, []);
 }
 
-const iconsData = new TableDataSource<IconWithName>();
-iconsData.data = iconsMapped();
+const iconsData = iconsMapped();
 
 export const Default = {
   render: (args: { dataSource: typeof iconsData }) => ({
     props: args,
     template: /*html*/ `
-    <div class="tw-bg-secondary-100">
-      <bit-table [dataSource]="dataSource">
-        <ng-container header>
-          <tr>
-            <th bitCell class="tw-text-main">Icon Name</th>
-            <th bitCell class="tw-text-main">Icon</th>
-          </tr>
-        </ng-container>
-        <ng-template body let-rows$>
-          @for (row of rows$ | async; track row.name) {
-            <tr bitRow alignContent="middle">
-              <td bitCell>{{row.name}}</td>
-              <td bitCell class=" tw-size-72">
-                <bit-icon [icon]="row.icon"></bit-icon>
-              </td>
-            </tr>
-          }
-        </ng-template>
-      </bit-table>
+    <div class="tw-bg-secondary-100 tw-grid tw-grid-cols-[repeat(auto-fit,minmax(224px,1fr))] tw-gap-2">
+      @for (icon of dataSource; track icon.name) {
+        <div class="tw-size-56 tw-border tw-border-text-main">
+          <div class="tw-text-xs">{{icon.name}}</div>
+          <div class="tw-size-52">
+            <bit-icon [icon]="icon.icon"></bit-icon>
+          </div>
+        </div>
+      }
     </div>
     `,
   }),
