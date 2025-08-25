6c75363a56e399df9dce88a25cfce97c5d31d7aa
Bernd Schoolmann <mail@quexten.com>
2025-08-26 00:43:17 +0200

    Prevent memory dumping and debugger on windows and mac main process

--- apps/desktop/desktop_native/core/src/process_isolation/macos.rs
+++ apps/desktop/desktop_native/core/src/process_isolation/macos.rs
@@ -9,5 +9,12 @@ pub fn is_core_dumping_disabled() -> Result<bool> {
 }
 
 pub fn disable_memory_access() -> Result<()> {
-    bail!("Not implemented on Mac")
+    let pid: u32 = std::process::id();
+    println!("Isolating Process {}", pid);
+    if let Err(e) = secmem_proc::harden_process() {
+        println!("ERROR: could not harden process, exiting");
+        println!("ERROR: {}", e);
+        return Ok(());
+    }
+    return Ok(());
 }
--- apps/desktop/src/main/window.main.ts
+++ apps/desktop/src/main/window.main.ts
@@ -151,6 +151,8 @@ export class WindowMain {
         app.on("ready", async () => {
           if (isMac() || isWindows()) {
             this.enableRendererProcessForceCrashReload = true;
+            // Isolate main the process from debuggers and memory dumping.
+            await processisolations.disableMemoryAccess();
           } else if (isLinux() && !isDev()) {
             if (await processisolations.isCoreDumpingDisabled()) {
               this.logService.info("Coredumps are disabled in renderer process");
