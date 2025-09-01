c6e79cb4175cd16e1c0bd38adb5a36a5fe7073f4
Bernd Schoolmann <mail@quexten.com>
2025-09-01 16:46:45 +0200

    Move to desktop native

--- apps/desktop/desktop_native/process_isolation/src/lib.rs
+++ apps/desktop/desktop_native/process_isolation/src/lib.rs
@@ -3,14 +3,13 @@
 //! On Linux, this is PR_SET_DUMPABLE to prevent debuggers from attaching, the env
 //! from being read and the memory from being stolen.
 
-use std::ffi::{c_char, c_void};
+use std::{ffi::c_char, sync::LazyLock};
+use desktop_core::process_isolation;
 
-mod isolate;
-
-#[link(name = "dl")]
-unsafe extern "C" {
-    unsafe fn dlsym(handle: *const c_void, symbol: *const c_char) -> *const c_void;
-}
+static ORIGINAL_UNSETENV: LazyLock<unsafe extern "C" fn(*const c_char) -> i32> =
+    LazyLock::new(|| unsafe {
+        std::mem::transmute(libc::dlsym(libc::RTLD_NEXT, c"unsetenv".as_ptr()))
+    });
 
 /// Hooks unsetenv to fix a bug in zypak-wrapper.
 /// Zypak unsets the env in Flatpak as a side-effect, which means that only the top level
@@ -18,9 +17,9 @@ unsafe extern "C" {
 #[unsafe(no_mangle)]
 unsafe extern "C" fn unsetenv(name: *const c_char) -> i32 {
     unsafe {
-        let name_str = std::ffi::CStr::from_ptr(name).to_str().unwrap();
-        let original_unsetenv: unsafe extern "C" fn(*const c_char) -> i32 =
-            std::mem::transmute(dlsym(libc::RTLD_NEXT, c"unsetenv".as_ptr()));
+        let Ok(name_str) = std::ffi::CStr::from_ptr(name).to_str() else {
+            return ORIGINAL_UNSETENV(name);
+        };
 
         if name_str == "LD_PRELOAD" {
             // This env variable is provided by the flatpak configuration
@@ -29,7 +28,7 @@ unsafe extern "C" fn unsetenv(name: *const c_char) -> i32 {
             return 0;
         }
 
-        original_unsetenv(name)
+        ORIGINAL_UNSETENV(name)
     }
 }
 
@@ -39,7 +38,7 @@ fn preload_init() {
     let pid = unsafe { libc::getpid() };
     unsafe {
         println!("[Process Isolation] Enabling memory security for process {pid}");
-        isolate::isolate_process();
-        isolate::disable_coredumps();
+        process_isolation::disable_memory_access();
+        process_isolation::disable_coredumps();
     }
 }
