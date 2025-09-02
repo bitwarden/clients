904e72e30ddf1dd13985d3ab53b71aeae0ec819a
Alex Morask <amorask@bitwarden.com>
2025-09-02 11:21:58 -0500

    Use OrganizationWarningsService in OrgSwitcherComponent

--- apps/web/src/app/billing/services/trial-flow.service.ts
+++ apps/web/src/app/billing/services/trial-flow.service.ts
@@ -72,6 +72,7 @@ export class TrialFlowService {
     }
   }
 
+  // showInactiveSubscriptionDialog$
   async handleUnpaidSubscriptionDialog(
     org: Organization,
     organizationBillingMetadata: OrganizationBillingMetadataResponse,
