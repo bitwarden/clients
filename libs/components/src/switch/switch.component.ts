17c22c2d2037d1c5780d5cfb38f928d71c1abba4
Bryan Cunningham <bryan.cunningham@me.com>
2025-09-03 12:34:02 -0400

    fix notify function type

--- libs/components/src/switch/switch.component.ts
+++ libs/components/src/switch/switch.component.ts
@@ -64,7 +64,7 @@ export class SwitchComponent implements ControlValueAccessor {
   });
 
   // ControlValueAccessor functions
-  private notifyOnChange: (value: unknown) => void = () => {};
+  private notifyOnChange: (value: boolean) => void = () => {};
   private notifyOnTouch: () => void = () => {};
 
   writeValue(value: boolean): void {
