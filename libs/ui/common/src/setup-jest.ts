85b098dbf06a1bb58670bf94fc379e93ffb8fd22
Bryan Cunningham <bryan.cunningham@me.com>
2025-09-03 15:49:26 -0400

    remove Observer mock

--- libs/ui/common/src/setup-jest.ts
+++ libs/ui/common/src/setup-jest.ts
@@ -1,6 +1,3 @@
-import { mock } from "jest-mock-extended";
 import { setupZoneTestEnv } from "jest-preset-angular/setup-env/zone";
 
 setupZoneTestEnv({ errorOnUnknownElements: true, errorOnUnknownProperties: true });
-
-window.IntersectionObserver = jest.fn(() => mock<IntersectionObserver>());
