c98d28eff343bd8e0111379eb29352c9409d0c13
Kyle Denney <4227399+kdenney@users.noreply.github.com>
2025-09-16 08:59:39 -0500

    [PM-25029] pricing service, getting closer

--- apps/web/src/app/billing/services/subscription-pricing.service.ts
+++ apps/web/src/app/billing/services/subscription-pricing.service.ts
@@ -1,6 +1,6 @@
 import { formatCurrency } from "@angular/common";
 import { Injectable } from "@angular/core";
-import { combineLatest, from, map, Observable, shareReplay } from "rxjs";
+import { combineLatest, from, map, Observable, of, shareReplay } from "rxjs";
 import { catchError } from "rxjs/operators";
 
 import { ApiService } from "@bitwarden/common/abstractions/api.service";
@@ -10,15 +10,16 @@ import { ListResponse } from "@bitwarden/common/models/response/list.response";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { ToastService } from "@bitwarden/components";
 import { LogService } from "@bitwarden/logging";
+import { BillingServicesModule } from "@bitwarden/web-vault/app/billing/services/billing-services.module";
 import {
   BusinessSubscriptionPricingTier,
+  BusinessSubscriptionPricingTierIds,
   PersonalSubscriptionPricingTier,
+  PersonalSubscriptionPricingTierIds,
   SubscriptionCadenceIds,
 } from "@bitwarden/web-vault/app/billing/types/subscription-pricing-tier";
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable({ providedIn: BillingServicesModule })
 export class SubscriptionPricingService {
   constructor(
     private apiService: ApiService,
@@ -27,8 +28,21 @@ export class SubscriptionPricingService {
     private toastService: ToastService,
   ) {}
 
-  getPersonalTiers$ = (): Observable<PersonalSubscriptionPricingTier[]> =>
+  getPersonalSubscriptionPricingTiers$ = (): Observable<PersonalSubscriptionPricingTier[]> =>
     combineLatest([this.premium$, this.families$]).pipe(
+      catchError((error: unknown) => {
+        this.logService.error(error);
+        this.toastService.showToast({
+          variant: "error",
+          title: "",
+          message: this.i18nService.t("unexpectedError"),
+        });
+        return [];
+      }),
+    );
+
+  getBusinessSubscriptionPricingTiers$ = (): Observable<BusinessSubscriptionPricingTier[]> =>
+    combineLatest([this.teams$, this.enterprise$]).pipe(
       catchError((error: unknown) => {
         this.logService.error(error);
         this.toastService.showToast({
@@ -44,28 +58,25 @@ export class SubscriptionPricingService {
     this.apiService.getPlans(),
   ).pipe(shareReplay({ bufferSize: 1, refCount: false }));
 
-  private premium$: Observable<PersonalSubscriptionPricingTier> = this.plansResponse$.pipe(
-    map((plans) => {
-      // todo hardcode the premium plan instead of looking for it.
-      const premiumPlan = plans.data.find((plan) => plan.type === PlanType.Free)!;
-
+  private premium$: Observable<PersonalSubscriptionPricingTier> = of({
+    // premium plan is not configured server-side so for now, hardcode it
+    basePrice: 10,
+    additionalStoragePricePerGb: 4,
+    baseStorageGb: 1,
+  }).pipe(
+    map((details) => {
       return {
-        id: "premium",
-        // todo get the actual keys for name and desc here
+        id: PersonalSubscriptionPricingTierIds.Premium,
         name: this.i18nService.t("planNamePremium"),
-        description: this.i18nService.t("planDescPremium", "1"),
+        description: this.i18nService.t("planDescPremium"),
         availableCadences: [SubscriptionCadenceIds.Annually],
         passwordManager: {
           type: "standalone",
-          monthlyCost: premiumPlan.PasswordManager.basePrice / 12,
-          monthlyCostPerAdditionalStorageGB:
-            premiumPlan.PasswordManager.additionalStoragePricePerGb / 12,
+          monthlyCost: details.basePrice / 12,
+          monthlyCostPerAdditionalStorageGB: details.additionalStoragePricePerGb / 12,
           features: [
-            // todo what are the premium plan features? translations? these are mostly fake
             this.featureTranslations.builtInAuthenticator(),
-            this.featureTranslations.encryptedFileStorage(
-              premiumPlan.PasswordManager.baseStorageGb,
-            ),
+            this.featureTranslations.encryptedFileStorage(details.baseStorageGb),
             this.featureTranslations.emergencyAccess(),
             this.featureTranslations.breachMonitoring(),
             this.featureTranslations.andMoreFeatures(),
@@ -80,7 +91,7 @@ export class SubscriptionPricingService {
       const familiesPlan = plans.data.find((plan) => plan.type === PlanType.FamiliesAnnually)!;
 
       return {
-        id: "families",
+        id: PersonalSubscriptionPricingTierIds.Families,
         name: this.i18nService.t("planNameFamilies"),
         description: this.i18nService.t("planDescFamilies"),
         availableCadences: [SubscriptionCadenceIds.Annually],
@@ -114,7 +125,7 @@ export class SubscriptionPricingService {
       const annualTeamsPlan = plans.data.find((plan) => plan.type === PlanType.TeamsAnnually)!;
 
       return {
-        id: "teams",
+        id: BusinessSubscriptionPricingTierIds.Teams,
         name: this.i18nService.t("planNameTeams"),
         description: this.i18nService.t("planDescTeams"),
         availableCadences: [SubscriptionCadenceIds.Annually, SubscriptionCadenceIds.Monthly],
@@ -147,7 +158,7 @@ export class SubscriptionPricingService {
       )!;
 
       return {
-        id: "enterprise",
+        id: BusinessSubscriptionPricingTierIds.Enterprise,
         name: this.i18nService.t("planNameEnterprise"),
         description: this.i18nService.t("planDesc"),
         availableCadences: ["annually", "monthly"],
