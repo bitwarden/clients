39043519deea0ecd5fca3b4e50ebc86656436c49
voommen-livefront <voommen@livefront.com>
2025-09-25 12:13:21 -0500

    PM-26018 password change metric

--- bitwarden_license/bit-web/src/app/dirt/access-intelligence/activity-cards/password-change-metric.component.ts
+++ bitwarden_license/bit-web/src/app/dirt/access-intelligence/activity-cards/password-change-metric.component.ts
@@ -1,22 +1,49 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subject, switchMap, takeUntil, of } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
-import { ProgressModule, TypographyModule } from "@bitwarden/components";
+import { SecurityTasksApiService } from "@bitwarden/bit-common/dirt/reports/risk-insights";
+import { OrganizationId } from "@bitwarden/common/types/guid";
+import { ButtonModule, ProgressModule, TypographyModule } from "@bitwarden/components";
 
 @Component({
   selector: "dirt-password-change-metric",
-  imports: [CommonModule, TypographyModule, JslibModule, ProgressModule],
+  imports: [CommonModule, TypographyModule, JslibModule, ProgressModule, ButtonModule],
   templateUrl: "./password-change-metric.component.html",
-  host: {
-    class:
-      "tw-box-border tw-bg-background tw-text-main tw-border-solid tw-border tw-border-secondary-300 tw-border [&:not(bit-layout_*)]:tw-rounded-lg tw-rounded-lg tw-p-6",
-  },
 })
-export class PasswordChangeMetricComponent {
-  get completedPercent(): number {
-    return (this.completedTasks / this.totalTasks) * 100;
+export class PasswordChangeMetricComponent implements OnInit {
+  totalTasks: number = 0;
+  completedTasks: number = 0;
+  private organizationId!: OrganizationId;
+  private destroyRef = new Subject<void>();
+
+  async ngOnInit(): Promise<void> {
+    this.activatedRoute.paramMap
+      .pipe(
+        switchMap((paramMap) => {
+          const orgId = paramMap.get("organizationId");
+          if (orgId) {
+            this.organizationId = orgId as OrganizationId;
+            return this.securityTasksApiService.getTaskMetrics(this.organizationId);
+          }
+          return of({ totalTasks: 0, completedTasks: 0 });
+        }),
+        takeUntil(this.destroyRef),
+      )
+      .subscribe((metrics) => {
+        this.totalTasks = metrics.totalTasks;
+        this.completedTasks = metrics.completedTasks;
+      });
   }
-  totalTasks: number = 200;
-  completedTasks: number = 100;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private securityTasksApiService: SecurityTasksApiService,
+  ) {}
+
+  get completedPercent(): number {
+    return Math.round((this.completedTasks / this.totalTasks) * 100);
+  }
 }
