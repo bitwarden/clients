b3fcdff0ce356e30da5a78bcef2270971a1552a6
Kyle Denney <4227399+kdenney@users.noreply.github.com>
2025-10-01 16:43:22 -0500

    removing further properties from the response and replacing them with data already available

--- libs/common/src/billing/models/response/organization-billing-metadata.response.ts
+++ libs/common/src/billing/models/response/organization-billing-metadata.response.ts
@@ -1,15 +1,11 @@
 import { BaseResponse } from "../../../models/response/base.response";
 
 export class OrganizationBillingMetadataResponse extends BaseResponse {
-  isEligibleForSelfHost: boolean;
-  isManaged: boolean;
   isOnSecretsManagerStandalone: boolean;
   organizationOccupiedSeats: number;
 
   constructor(response: any) {
     super(response);
-    this.isEligibleForSelfHost = this.getResponseProperty("IsEligibleForSelfHost");
-    this.isManaged = this.getResponseProperty("IsManaged");
     this.isOnSecretsManagerStandalone = this.getResponseProperty("IsOnSecretsManagerStandalone");
     this.organizationOccupiedSeats = this.getResponseProperty("OrganizationOccupiedSeats");
   }
