bfbcce23069c9724f2dca7a673d74a52d76ef5ab
adudek-bw <adudek@bitwarden.com>
2025-10-01 16:34:38 -0400

    Fix clippy

--- apps/desktop/desktop_native/bitwarden_chromium_importer/src/windows.rs
+++ apps/desktop/desktop_native/bitwarden_chromium_importer/src/windows.rs
@@ -9,7 +9,6 @@ use windows::Win32::Foundation::{LocalFree, HLOCAL};
 
 use crate::chromium::{BrowserConfig, CryptoService, LocalState};
 
-#[allow(dead_code)]
 use crate::util;
 
 //
@@ -18,10 +17,6 @@ use crate::util;
 
 // IMPORTANT adjust array size when enabling / disabling chromium importers here
 pub const SUPPORTED_BROWSERS: [BrowserConfig; 4] = [
-    // BrowserConfig {
-    //     name: "Chrome",
-    //     data_dir: "AppData/Local/Google/Chrome/User Data",
-    // },
     BrowserConfig {
         name: "Chromium",
         data_dir: "AppData/Local/Chromium/User Data",
@@ -30,10 +25,6 @@ pub const SUPPORTED_BROWSERS: [BrowserConfig; 4] = [
         name: "Microsoft Edge",
         data_dir: "AppData/Local/Microsoft/Edge/User Data",
     },
-    // BrowserConfig {
-    //     name: "Brave",
-    //     data_dir: "AppData/Local/BraveSoftware/Brave-Browser/User Data",
-    // },
     BrowserConfig {
         name: "Opera",
         data_dir: "AppData/Roaming/Opera Software/Opera Stable",
