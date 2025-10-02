99c069d567e50862c32b4a5746ad0dc0dc33ccf4
jng <jng@bitwarden.com>
2025-10-02 15:32:06 -0400

    updating banners in at risk password callout component

--- apps/browser/src/vault/popup/components/at-risk-callout/at-risk-password-callout.component.ts
+++ apps/browser/src/vault/popup/components/at-risk-callout/at-risk-password-callout.component.ts
@@ -1,25 +1,32 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
-import { Router } from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { firstValueFrom, switchMap } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { getUserId } from "@bitwarden/common/auth/services/account.service";
-import { CalloutModule, BannerModule } from "@bitwarden/components";
+import { CalloutModule, BannerModule, LinkModule } from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
 import { AtRiskPasswordCalloutData, AtRiskPasswordCalloutService } from "@bitwarden/vault";
 
 @Component({
   selector: "vault-at-risk-password-callout",
-  imports: [CommonModule, CalloutModule, I18nPipe, BannerModule, JslibModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    CalloutModule,
+    I18nPipe,
+    BannerModule,
+    JslibModule,
+    LinkModule,
+  ],
   providers: [AtRiskPasswordCalloutService],
   templateUrl: "./at-risk-password-callout.component.html",
 })
 export class AtRiskPasswordCalloutComponent {
   private activeAccount$ = inject(AccountService).activeAccount$.pipe(getUserId);
   private atRiskPasswordCalloutService = inject(AtRiskPasswordCalloutService);
-  private router = inject(Router);
 
   showTasksBanner$ = this.activeAccount$.pipe(
     switchMap((userId) => this.atRiskPasswordCalloutService.showCompletedTasksBanner$(userId)),
@@ -36,9 +43,5 @@ export class AtRiskPasswordCalloutComponent {
     };
     const userId = await firstValueFrom(this.activeAccount$);
     this.atRiskPasswordCalloutService.updateAtRiskPasswordState(userId, updateObject);
-  }
-
-  async navigateToAtRiskPasswords() {
-    await this.router.navigate(["/at-risk-passwords"]);
   }
 }
