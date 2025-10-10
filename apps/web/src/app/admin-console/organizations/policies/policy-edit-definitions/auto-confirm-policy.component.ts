5e039e897acd1076bc71f06d767d477b35daca21
Brandon <btreston@bitwarden.com>
2025-10-10 13:14:53 -0400

    wip

--- apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/auto-confirm-policy.component.ts
+++ apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/auto-confirm-policy.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Signal, TemplateRef, viewChild } from "@angular/core";
-import { FormControl, UntypedFormBuilder, Validators } from "@angular/forms";
 import { BehaviorSubject, map, Observable } from "rxjs";
 
 import { PolicyType } from "@bitwarden/common/admin-console/enums";
@@ -8,6 +7,7 @@ import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
 import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 
 import { SharedModule } from "../../../../shared";
+import { AutoConfirmPolicyDialogComponent } from "../auto-confirm-edit-policy-dialog.component";
 import { BasePolicyEditDefinition, BasePolicyEditComponent } from "../base-policy-edit.component";
 
 export class AutoConfirmPolicy extends BasePolicyEditDefinition {
@@ -16,7 +16,7 @@ export class AutoConfirmPolicy extends BasePolicyEditDefinition {
   type = PolicyType.AutoConfirm;
   component = AutoConfirmPolicyEditComponent;
   showDescription = false;
-  // @TODO: assign the new AutoConfirmEditPolicyComponent here
+  editDialogComponent = AutoConfirmPolicyDialogComponent;
 
   override display$(organization: Organization, configService: ConfigService): Observable<boolean> {
     return configService.getFeatureFlag$(FeatureFlag.AutoConfirm).pipe(
@@ -39,16 +39,6 @@ export class AutoConfirmPolicyEditComponent extends BasePolicyEditComponent impl
 
   protected singleOrgEnabled$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  data = this.formBuilder.group({
-    securityRisks: new FormControl(false, Validators.requiredTrue),
-    singleOrgAccepted: new FormControl(false, Validators.requiredTrue),
-    removeEmergencyAccess: new FormControl(false, Validators.requiredTrue),
-  });
-
-  constructor(private formBuilder: UntypedFormBuilder) {
-    super();
-  }
-
   setSingleOrgEnabled(enabled: boolean) {
     this.singleOrgEnabled$.next(enabled);
   }
@@ -59,6 +49,5 @@ export class AutoConfirmPolicyEditComponent extends BasePolicyEditComponent impl
 
   ngOnInit(): void {
     super.ngOnInit();
-    this.data.updateValueAndValidity();
   }
 }
