d45c876d034cd83cbda98a66c814327657df2381
Daniel James Smith <djsmith85@users.noreply.github.com>
2025-10-14 18:03:44 +0200

    Remove logic to skip Brave as that now happens via the native code

--- libs/importer/src/services/default-import-metadata.service.ts
+++ libs/importer/src/services/default-import-metadata.service.ts
@@ -1,10 +1,9 @@
 import { map, Observable } from "rxjs";
 
-import { DeviceType } from "@bitwarden/common/enums";
 import { SemanticLogger } from "@bitwarden/common/tools/log";
 import { SystemServiceProvider } from "@bitwarden/common/tools/providers";
 
-import { ImporterMetadata, ImportersMetadata, Loader } from "../metadata";
+import { ImporterMetadata, ImportersMetadata } from "../metadata";
 import { ImportType } from "../models/import-options";
 import { availableLoaders } from "../util";
 
@@ -30,27 +29,7 @@ export class DefaultImportMetadataService implements ImportMetadataServiceAbstra
           return { type, loaders: [] };
         }
 
-        let loaders = availableLoaders(this.importers, type, client);
-
-        let isUnsupported = false;
-
-        // FIXME Likely needs to be extended for Chrome and Edge as well
-        if (type === "bravecsv") {
-          try {
-            const device = this.system.environment.getDevice();
-            const isWindowsDesktop = device === DeviceType.WindowsDesktop;
-            if (isWindowsDesktop) {
-              isUnsupported = true;
-            }
-          } catch {
-            isUnsupported = true;
-          }
-        }
-
-        // If the feature flag is disabled, or if the browser is unsupported, remove the chromium loader
-        if (isUnsupported) {
-          loaders = loaders?.filter((loader) => loader !== Loader.chromium);
-        }
+        const loaders = availableLoaders(this.importers, type, client);
 
         if (!loaders || loaders.length === 0) {
           return { type, loaders: [] };
