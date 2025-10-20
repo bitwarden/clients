979b5592d72fdfddaa0f26b357a31adbbd253679
Kyle Denney <4227399+kdenney@users.noreply.github.com>
2025-10-20 09:40:38 -0500

    [PM-24032] upgrade nav button post-upgrade action

--- apps/web/src/app/billing/individual/upgrade/upgrade-nav-button/upgrade-nav-button/upgrade-nav-button.component.ts
+++ apps/web/src/app/billing/individual/upgrade/upgrade-nav-button/upgrade-nav-button/upgrade-nav-button.component.ts
@@ -1,11 +1,17 @@
 import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
 import { firstValueFrom, lastValueFrom } from "rxjs";
 
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
+import { SyncService } from "@bitwarden/common/vault/abstractions/sync/sync.service.abstraction";
 import { DialogService } from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
 
-import { UnifiedUpgradeDialogComponent } from "../../unified-upgrade-dialog/unified-upgrade-dialog.component";
+import {
+  UnifiedUpgradeDialogComponent,
+  UnifiedUpgradeDialogStatus,
+} from "../../unified-upgrade-dialog/unified-upgrade-dialog.component";
 
 @Component({
   selector: "app-upgrade-nav-button",
@@ -16,6 +22,9 @@ import { UnifiedUpgradeDialogComponent } from "../../unified-upgrade-dialog/unif
 export class UpgradeNavButtonComponent {
   private dialogService = inject(DialogService);
   private accountService = inject(AccountService);
+  private syncService = inject(SyncService);
+  private apiService = inject(ApiService);
+  private router = inject(Router);
 
   openUpgradeDialog = async () => {
     const account = await firstValueFrom(this.accountService.activeAccount$);
@@ -31,6 +40,14 @@ export class UpgradeNavButtonComponent {
       },
     });
 
-    await lastValueFrom(dialogRef.closed);
+    const result = await lastValueFrom(dialogRef.closed);
+
+    if (result?.status === UnifiedUpgradeDialogStatus.UpgradedToPremium) {
+      await this.apiService.refreshIdentityToken();
+      await this.syncService.fullSync(true);
+    } else if (result?.status === UnifiedUpgradeDialogStatus.UpgradedToFamilies) {
+      const redirectUrl = `/organizations/${result.organizationId}/vault`;
+      void this.router.navigate([redirectUrl]);
+    }
   };
 }
