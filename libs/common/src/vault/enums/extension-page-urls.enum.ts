7d51c3c331d195e16de95e2fdeabb4122dd35c8d
Brandon <btreston@bitwarden.com>
2025-10-21 15:22:30 -0400

    remove placeholder URL

--- libs/common/src/vault/enums/extension-page-urls.enum.ts
+++ libs/common/src/vault/enums/extension-page-urls.enum.ts
@@ -7,8 +7,6 @@ import { UnionOfValues } from "../types/union-of-values";
 export const ExtensionPageUrls: Record<string, `popup/index.html#/${string}`> = {
   Index: "popup/index.html#/",
   AtRiskPasswords: "popup/index.html#/at-risk-passwords",
-  //@FIXME: replace with the real page
-  AutoConfirm: "popup/index.html#/",
 } as const;
 
 export type ExtensionPageUrls = UnionOfValues<typeof ExtensionPageUrls>;
