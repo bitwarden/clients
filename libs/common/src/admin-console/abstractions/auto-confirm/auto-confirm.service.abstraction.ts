abac73d36dc19957c996a0eb132ccaf51217c34d
Brandon <btreston@bitwarden.com>
2025-10-23 12:09:29 -0400

    add missing userId param, jsdoc

--- libs/common/src/admin-console/abstractions/auto-confirm/auto-confirm.service.abstraction.ts
+++ libs/common/src/admin-console/abstractions/auto-confirm/auto-confirm.service.abstraction.ts
@@ -9,30 +9,34 @@ import { AutoConfirmState } from "../../services/auto-confirm/auto-confirm.state
 export abstract class AutomaticUserConfirmationService {
   /**
    * @param userId
-   * @returns Observable<Record<string, AutoConfirmState>>
-   * Returns an observable with the Auto Confirm user state for the provided userId
+   * @returns Observable<AutoConfirmState> an observable with the Auto Confirm user state for the provided userId.
    **/
   abstract configuration$(userId: UserId): Observable<AutoConfirmState>;
   /**
+   * Upserts the existing user state with a new configuration.
    * @param userId
-   * @param config the new AutoConfirmState to upsert into the user state for the provided userId
-   * Upserts the existing user state with a new configuration
+   * @param config The new AutoConfirmState to upsert into the user state for the provided userId.
    **/
   abstract upsert(userId: UserId, config: AutoConfirmState): Promise<void>;
   /**
-   * @param userId
-   * Returns an observable with a boolean telling us if the provided user may confgure the auto confirm feature.
    * This will check if the feature is enabled, the organization plan feature UseAutomaticUserConfirmation is enabled
    * and the the provided user has admin/owner/manage custom permission role.
+   * @param userId
+   * @returns Observable<boolean> an observable with a boolean telling us if the provided user may confgure the auto confirm feature.
    **/
   abstract canManageAutoConfirm$(
     userId: UserId,
     organizationId: OrganizationId,
   ): Observable<boolean>;
   /**
-   * @param userId
-   * @param organizationid
-   * Calls the API endpoint to initiate automatic user confirmation
+   * Calls the API endpoint to initiate automatic user confirmation.
+   * @param userId The userId of the logged in admin performing auto confirmation. This is neccesary to perform the key exchange and for permissions checks.
+   * @param confirmingUserId The userId of the user being confirmed.
+   * @param organization the organization the user is being auto confirmed to.
    **/
-  abstract autoConfirmUser(userId: UserId, organization: Organization): Promise<void>;
+  abstract autoConfirmUser(
+    userId: UserId,
+    confirmingUserId: UserId,
+    organization: Organization,
+  ): Promise<void>;
 }
