6d37827ac7ad0f73d087639313101363fb53815b
Daniel James Smith <djsmith85@users.noreply.github.com>
2025-10-24 18:49:46 +0200

    Remove unused getInstalledBrowsers metthod

--- apps/desktop/desktop_native/napi/src/lib.rs
+++ apps/desktop/desktop_native/napi/src/lib.rs
@@ -1061,7 +1061,6 @@ pub mod logging {
 #[napi]
 pub mod chromium_importer {
     use bitwarden_chromium_importer::chromium::DefaultInstalledBrowserRetriever;
-    use bitwarden_chromium_importer::chromium::InstalledBrowserRetriever;
     use bitwarden_chromium_importer::chromium::LoginImportResult as _LoginImportResult;
     use bitwarden_chromium_importer::chromium::ProfileInfo as _ProfileInfo;
     use bitwarden_chromium_importer::metadata::NativeImporterMetadata;
@@ -1133,12 +1132,6 @@ pub mod chromium_importer {
         bitwarden_chromium_importer::metadata::get_supported_importers::<
             DefaultInstalledBrowserRetriever,
         >()
-    }
-
-    #[napi]
-    pub fn get_installed_browsers() -> napi::Result<Vec<String>> {
-        bitwarden_chromium_importer::chromium::DefaultInstalledBrowserRetriever::get_installed_browsers()
-            .map_err(|e| napi::Error::from_reason(e.to_string()))
     }
 
     #[napi]
