fdc08ae789e66875afb83d7d7e5a0bc91ea5e258
Bryan Cunningham <bryan.cunningham@me.com>
2025-10-27 10:03:02 -0400

    use tooltip in a11y directive

--- libs/components/src/a11y/a11y-title.directive.ts
+++ libs/components/src/a11y/a11y-title.directive.ts
@@ -1,23 +1,35 @@
-import { Directive, effect, ElementRef, input } from "@angular/core";
+import { Directive } from "@angular/core";
 
-import { setA11yTitleAndAriaLabel } from "./set-a11y-title-and-aria-label";
+import { TooltipDirective } from "../tooltip/tooltip.directive";
 
+/**
+ * Directive that provides accessible tooltips by internally using TooltipDirective.
+ * This maintains the appA11yTitle API while leveraging the enhanced tooltip functionality.
+ */
 @Directive({
   selector: "[appA11yTitle]",
+  hostDirectives: [
+    {
+      directive: TooltipDirective,
+      inputs: ["bitTooltip: appA11yTitle", "tooltipPosition"],
+    },
+  ],
 })
 export class A11yTitleDirective {
-  readonly title = input.required<string>({ alias: "appA11yTitle" });
-
-  constructor(private el: ElementRef) {
-    const originalTitle = this.el.nativeElement.getAttribute("title");
-    const originalAriaLabel = this.el.nativeElement.getAttribute("aria-label");
-
-    effect(() => {
-      setA11yTitleAndAriaLabel({
-        element: this.el.nativeElement,
-        title: originalTitle ?? this.title(),
-        label: originalAriaLabel ?? this.title(),
-      });
-    });
-  }
+  // readonly appA11yTitle = input.required<string>({ alias: "appA11yTitle" });
+  // private tooltipDirective = inject(TooltipDirective);
+  // private injector = inject(Injector);
+  // constructor() {
+  //   effect(
+  //     () => {
+  //       // Sync the appA11yTitle input to the TooltipDirective's bitTooltip input
+  //       const title = this.appA11yTitle();
+  //       // We need to manually set the tooltip content since hostDirectives
+  //       // don't automatically map inputs
+  //       // This is a workaround - we'll use reflection to set the input
+  //       (this.tooltipDirective as any).bitTooltip = () => title;
+  //     },
+  //     { injector: this.injector },
+  //   );
+  // }
 }
