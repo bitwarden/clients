fc5d953c049bc69072af97c5d9f181706c1fde9d
Graham Walker <gwalker@bitwarden.com>
2025-10-29 15:57:42 -0500

    PM-23810 merged in new KM changes and filter nulls within the collectionName array

--- bitwarden_license/bit-web/src/app/dirt/reports/member-access-report/services/member-access-report.service.spec.ts
+++ bitwarden_license/bit-web/src/app/dirt/reports/member-access-report/services/member-access-report.service.spec.ts
@@ -2,6 +2,7 @@ import { mock } from "jest-mock-extended";
 import { of } from "rxjs";
 
 import { EncryptService } from "@bitwarden/common/key-management/crypto/abstractions/encrypt.service";
+import { EncString } from "@bitwarden/common/key-management/crypto/models/enc-string";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { SymmetricCryptoKey } from "@bitwarden/common/platform/models/domain/symmetric-crypto-key";
 import { mockAccountServiceWith } from "@bitwarden/common/spec";
