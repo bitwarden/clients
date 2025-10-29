98d16eadcd65d3edbb13c39f8b82317c2074d663
Jonathan Prusik <jprusik@classynemesis.com>
2025-10-29 18:13:08 -0400

    additional cleanup

--- apps/browser/src/autofill/models/autofill-field.ts
+++ apps/browser/src/autofill/models/autofill-field.ts
@@ -1,6 +1,6 @@
-import { FieldRect } from "../background/abstractions/overlay.background";
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
+import { FieldRect } from "../background/abstractions/overlay.background";
 import { AutofillFieldQualifierType } from "../enums/autofill-field.enums";
 import {
   InlineMenuAccountCreationFieldTypes,
