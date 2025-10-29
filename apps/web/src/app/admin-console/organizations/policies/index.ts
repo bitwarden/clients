5326ecedca8a2f3f5cedc8d7a64d04191254a7a0
Brandon <btreston@bitwarden.com>
2025-10-29 10:34:32 -0400

    add one time setup dialog for auto confirm

--- apps/web/src/app/admin-console/organizations/policies/index.ts
+++ apps/web/src/app/admin-console/organizations/policies/index.ts
@@ -2,3 +2,5 @@ export { PoliciesComponent } from "./policies.component";
 export { ossPolicyEditRegister } from "./policy-edit-register";
 export { BasePolicyEditDefinition, BasePolicyEditComponent } from "./base-policy-edit.component";
 export { POLICY_EDIT_REGISTER } from "./policy-register-token";
+export { AutoConfirmPolicyDialogComponent } from "./auto-confirm-edit-policy-dialog.component";
+export { AutoConfirmPolicy } from "./policy-edit-definitions";
