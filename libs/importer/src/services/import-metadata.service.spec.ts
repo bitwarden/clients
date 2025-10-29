c1c265cf71e190e01f1c1b72f6c5346f0e9d51f9
Daniel James Smith <djsmith85@users.noreply.github.com>
2025-10-29 15:12:37 +0100

    Remove duplicate test

--- libs/importer/src/services/import-metadata.service.spec.ts
+++ libs/importer/src/services/import-metadata.service.spec.ts
@@ -166,20 +166,6 @@ describe("ImportMetadataService", () => {
       expect(result.loaders).toContain(Loader.file);
     });
 
-    it("should exclude chromium loader when ABE is disabled but on Windows Desktop", async () => {
-      environment.getDevice.mockReturnValue(DeviceType.WindowsDesktop);
-      const testType: ImportType = "bravecsv"; // bravecsv supports both file and chromium loaders
-      featureFlagSubject.next(false);
-
-      const metadataPromise = firstValueFrom(sut.metadata$(typeSubject));
-      typeSubject.next(testType);
-
-      const result = await metadataPromise;
-
-      expect(result.loaders).not.toContain(Loader.chromium);
-      expect(result.loaders).toContain(Loader.file);
-    });
-
     it("should include chromium loader when ABE is disabled and not on Windows Desktop", async () => {
       environment.getDevice.mockReturnValue(DeviceType.MacOsDesktop);
       const testType: ImportType = "bravecsv"; // bravecsv supports both file and chromium loaders
