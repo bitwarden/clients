ca51ea0fc94dab3982953f06c532192e03243f65
voommen-livefront <voommen@livefront.com>
2025-10-29 20:34:29 -0500

    PM-27291 updated the guard to include null check

--- bitwarden_license/bit-common/src/dirt/reports/risk-insights/services/domain/risk-insights-type-guards.ts
+++ bitwarden_license/bit-common/src/dirt/reports/risk-insights/services/domain/risk-insights-type-guards.ts
@@ -48,7 +48,8 @@ export function isMemberDetails(obj: any): obj is MemberDetails {
     typeof obj.userGuid === "string" &&
     obj.userGuid.length > 0 &&
     obj.userGuid.length <= MAX_STRING_LENGTH &&
-    (obj.userName === undefined ||
+    (obj.userName === null ||
+      obj.userName === undefined ||
       (typeof obj.userName === "string" &&
         obj.userName.length > 0 &&
         obj.userName.length <= MAX_STRING_LENGTH)) &&
