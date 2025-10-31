34306bb1722b031495867bb3c64ee248dd4f8e26
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-10-30 18:04:56 -0700

    update service import

--- apps/web/src/app/vault/guards/premium-setup-intent-redirect/premium-setup-intent-redirect.guard.ts
+++ apps/web/src/app/vault/guards/premium-setup-intent-redirect/premium-setup-intent-redirect.guard.ts
@@ -2,18 +2,18 @@ import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { firstValueFrom } from "rxjs";
 
-import { PremiumSetupIntentService } from "@bitwarden/angular/billing/services/premium-setup-intent/premium-setup-intent-state.service.abstraction";
+import { PremiumSetupIntentStateService } from "@bitwarden/angular/billing/services/premium-setup-intent/premium-setup-intent-state.service.abstraction";
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 
 export const premiumSetupIntentRedirectGuard: CanActivateFn = async () => {
   const router = inject(Router);
   const accountService = inject(AccountService);
-  const premiumSetupIntentService = inject(PremiumSetupIntentService);
+  const premiumSetupIntentStateService = inject(PremiumSetupIntentStateService);
 
   const currentAcct = await firstValueFrom(accountService.activeAccount$);
   const userId = currentAcct.id;
 
-  const intendsToSetupPremium = await premiumSetupIntentService.getPremiumSetupIntent(userId);
+  const intendsToSetupPremium = await premiumSetupIntentStateService.getPremiumSetupIntent(userId);
 
   if (intendsToSetupPremium) {
     return router.createUrlTree(["/settings/subscription/premium"]);
