89f412f52ac3bdb25b84ebeeb0edb57fccc0a276
neuronull <9162534+neuronull@users.noreply.github.com>
2025-11-03 11:00:48 -0700

    Revert "ipc handlers in init"
    This reverts commit a23999edcfda396eb0c0910d8a318b084a3c4120.

--- apps/desktop/src/autofill/main/main-desktop-autotype.service.ts
+++ apps/desktop/src/autofill/main/main-desktop-autotype.service.ts
@@ -18,9 +18,7 @@ export class MainDesktopAutotypeService {
     private windowMain: WindowMain,
   ) {
     this.autotypeKeyboardShortcut = new AutotypeKeyboardShortcut();
-  }
 
-  init() {
     ipcMain.handle(AUTOTYPE_IPC_CHANNELS.INIT, () => {
       this.init();
     });
@@ -28,7 +26,9 @@ export class MainDesktopAutotypeService {
     ipcMain.handle(AUTOTYPE_IPC_CHANNELS.INITIALIZED, () => {
       return this.isInitialized;
     });
+  }
 
+  init() {
     ipcMain.on(AUTOTYPE_IPC_CHANNELS.TOGGLE, (_event, enable: boolean) => {
       if (enable) {
         this.enableAutotype();
