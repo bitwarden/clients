6dfc8af46db3a3a57f0ed8b086a83059fd2e4217
Will Martin <contact@willmartian.com>
2025-11-06 13:55:18 -0500

    [PM-26944] fix(browser/phishing-detection): fix various issues (#17197)
    (cherry picked from commit 1be9e19fad1c602f9f05a6becfd78b010ebecf24)

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1469,6 +1469,7 @@ export default class MainBackground {
       this.configService,
       this.logService,
       this.phishingDataService,
+      messageListener,
     );
 
     this.ipcContentScriptManagerService = new IpcContentScriptManagerService(this.configService);
