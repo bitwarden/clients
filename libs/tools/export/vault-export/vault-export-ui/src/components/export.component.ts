e78538a1f3970cdf1ae27b9b189454a39ca54dfc
Alex Dragovich <adragovich@bitwarden.com>
2025-11-06 08:49:34 -0800

    [PM-23258] small text changes from design

--- libs/tools/export/vault-export/vault-export-ui/src/components/export.component.ts
+++ libs/tools/export/vault-export/vault-export-ui/src/components/export.component.ts
@@ -608,7 +608,7 @@ export class ExportComponent implements OnInit, OnDestroy, AfterViewInit {
       title: "confirmVaultExport",
       bodyText: confirmDescription,
       confirmButtonOptions: {
-        text: "export",
+        text: "continue",
         type: "primary",
       },
     });
