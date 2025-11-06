e5faf90d3e956120717043888a537a6360aaca47
Hinton <hinton@users.noreply.github.com>
2025-11-06 16:01:40 +0100

    Update code after merge

--- apps/desktop/desktop_native/chromium_importer/config_constants.rs
+++ apps/desktop/desktop_native/chromium_importer/config_constants.rs
@@ -1,6 +1,5 @@
 // Enable this to log to a file. The way this executable is used, it's not easy to debug and the stdout gets lost.
-// This is intended for development time only. All the logging is wrapped in `dbg_log!` macro that compiles to
-// no-op when logging is disabled. This is needed to avoid any sensitive data being logged in production.
+// This is intended for development time only.
 pub const ENABLE_DEVELOPER_LOGGING: bool = false;
 
 // The absolute path to log file when developer logging is enabled
