05b227df70bc192ffd2c275694146795cfae9f53
Hinton <hinton@users.noreply.github.com>
2025-11-06 15:13:42 +0100

    Detect changes

--- apps/desktop/desktop_native/chromium_importer/build.rs
+++ apps/desktop/desktop_native/chromium_importer/build.rs
@@ -1,6 +1,8 @@
 include!("config_constants.rs");
 
 fn main() {
+    println!("cargo:rerun-if-changed=config_constants.rs");
+
     if cfg!(not(debug_assertions)) {
         if ENABLE_DEVELOPER_LOGGING {
             panic!("ENABLE_DEVELOPER_LOGGING must be false in release builds");
