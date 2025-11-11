60377ffb3742201e88732405d6d05860ce5b8ffc
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-11-11 14:37:16 +0100

    typo

--- libs/shared/polyfill-node-globals.ts
+++ libs/shared/polyfill-node-globals.ts
@@ -1,7 +1,7 @@
 import { TextEncoder, TextDecoder } from "util";
 
 // SDK/WASM code relies on TextEncoder/TextDecoder being available globally
-// We can't use `test.environment.ts` because the breaks other tests that rely on
+// We can't use `test.environment.ts` because that breaks other tests that rely on
 // the default jest jsdom environment
 if (!(globalThis as any).TextEncoder) {
   (globalThis as any).TextEncoder = TextEncoder;
