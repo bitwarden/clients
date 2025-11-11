5fbcec814cf2356d05884f0c7cbc380ca2b37d74
voommen-livefront <voommen@livefront.com>
2025-11-11 14:34:32 -0600

    PM-20132 removed unwanted code

--- bitwarden_license/bit-common/src/dirt/reports/risk-insights/services/domain/risk-insights-report.service.ts
+++ bitwarden_license/bit-common/src/dirt/reports/risk-insights/services/domain/risk-insights-report.service.ts
@@ -85,7 +85,6 @@ export class RiskInsightsReportService {
     applicationData: OrganizationReportApplication[],
     totalMemberCount: number = 0,
   ): OrganizationReportSummary {
-    const totalUniqueMembers = getUniqueMembers(reports.flatMap((x) => x.memberDetails));
     const atRiskUniqueMembers = getUniqueMembers(reports.flatMap((x) => x.atRiskMemberDetails));
 
     const criticalReports = this.filterApplicationsByCritical(reports, applicationData);
@@ -95,7 +94,7 @@ export class RiskInsightsReportService {
     );
 
     return {
-      totalMemberCount: totalMemberCount > 0 ? totalMemberCount : totalUniqueMembers.length,
+      totalMemberCount: totalMemberCount,
       totalAtRiskMemberCount: atRiskUniqueMembers.length,
       totalApplicationCount: reports.length,
       totalAtRiskApplicationCount: reports.filter((app) => app.atRiskPasswordCount > 0).length,
