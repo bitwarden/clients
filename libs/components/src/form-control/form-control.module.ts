ef8ace46acab7ec51bf294be2ccfae7127c97611
Hinton <hinton@users.noreply.github.com>
2025-11-14 11:42:36 +0100

    Enable directive-class-suffix

--- libs/components/src/form-control/form-control.module.ts
+++ libs/components/src/form-control/form-control.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 
 import { FormControlComponent } from "./form-control.component";
-import { BitHintComponent } from "./hint.component";
+import { BitHintDirective } from "./hint.directive";
 import { BitLabel } from "./label.component";
 
 @NgModule({
-  imports: [BitLabel, FormControlComponent, BitHintComponent],
-  exports: [FormControlComponent, BitLabel, BitHintComponent],
+  imports: [BitLabel, FormControlComponent, BitHintDirective],
+  exports: [FormControlComponent, BitLabel, BitHintDirective],
 })
 export class FormControlModule {}
