7a8ec65c54b9bcb8a514f7a95d59baacbe214d83
jaasen-livefront <jaasen@livefront.com>
2025-11-20 10:29:08 -0800

    no need to check for presence

--- libs/common/src/models/export/folder.export.ts
+++ libs/common/src/models/export/folder.export.ts
@@ -19,7 +19,7 @@ export class FolderExport {
   }
 
   static toDomain(req: FolderExport, domain = new FolderDomain()) {
-    domain.name = req.name ? new EncString(req.name) : undefined;
+    domain.name = new EncString(req.name);
     return domain;
   }
 
