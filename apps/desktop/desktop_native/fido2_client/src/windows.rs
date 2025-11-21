d718623cea03767cbc6c8036ba79c149498b3cee
Bernd Schoolmann <mail@quexten.com>
2025-11-21 01:55:50 +0100

    tmp

--- apps/desktop/desktop_native/fido2_client/src/windows.rs
+++ apps/desktop/desktop_native/fido2_client/src/windows.rs
@@ -1,46 +1,122 @@
-use webauthn_authenticator_rs::{AuthenticatorBackend, prelude::Url, win10::Win10};
-use webauthn_rs_proto::{HmacGetSecretInput, PublicKeyCredentialRequestOptions, RequestAuthenticationExtensions};
+use base64::{Engine, prelude::BASE64_URL_SAFE_NO_PAD};
+use windows::{Win32::{Foundation::BOOL, Networking::WindowsWebServices::{WEBAUTHN_AUTHENTICATOR_GET_ASSERTION_OPTIONS, WEBAUTHN_AUTHENTICATOR_GET_ASSERTION_OPTIONS_CURRENT_VERSION, WEBAUTHN_CLIENT_DATA, WEBAUTHN_CLIENT_DATA_CURRENT_VERSION, WEBAUTHN_CREDENTIAL_LIST, WEBAUTHN_CREDENTIALS, WEBAUTHN_EXTENSION, WEBAUTHN_EXTENSIONS, WEBAUTHN_HMAC_SECRET_SALT, WEBAUTHN_HMAC_SECRET_SALT_VALUES, WEBAUTHN_USER_VERIFICATION_REQUIREMENT_DISCOURAGED, WEBAUTHN_USER_VERIFICATION_REQUIREMENT_PREFERRED, WEBAUTHN_USER_VERIFICATION_REQUIREMENT_REQUIRED}, UI::WindowsAndMessaging::GetForegroundWindow}, core::{HSTRING, PCWSTR, w}};
 
 use crate::{AssertionOptions, Fido2ClientError, PublicKeyCredential};
 
 pub fn get(options: AssertionOptions) -> Result<PublicKeyCredential, Fido2ClientError> {
     let uv = match options.user_verification {
-        crate::UserVerification::Required => webauthn_rs_proto::UserVerificationPolicy::Required,
-        crate::UserVerification::Preferred => webauthn_rs_proto::UserVerificationPolicy::Preferred,
-        crate::UserVerification::Discouraged => webauthn_rs_proto::UserVerificationPolicy::Discouraged_DO_NOT_USE,
+        crate::UserVerification::Required => WEBAUTHN_USER_VERIFICATION_REQUIREMENT_REQUIRED,
+        crate::UserVerification::Preferred => WEBAUTHN_USER_VERIFICATION_REQUIREMENT_PREFERRED,
+        crate::UserVerification::Discouraged => WEBAUTHN_USER_VERIFICATION_REQUIREMENT_DISCOURAGED,
     };
-    println!("User verification policy: {:?}", uv);
+    let rp_id: HSTRING = options.rpid.clone().into();
+    
+    let client_data_json = format!(
+        r#"{{"type":"webauthn.get","challenge":"{}","origin":"https://{}","crossOrigin": true}}"#,
+        BASE64_URL_SAFE_NO_PAD.encode(&options.challenge),
+        options.rpid
+    );
+   
+    let salt_first = vec![0u8; options.prf_eval_first.len()];
+    let mut hmac_secret_salt = WEBAUTHN_HMAC_SECRET_SALT {
+        cbFirst: salt_first.len() as u32,
+        pbFirst: salt_first.as_ptr() as *mut _,
+        cbSecond: 0,
+        pbSecond: std::ptr::null_mut(),
+    };
+    let mut hmac_secret_salt_values = WEBAUTHN_HMAC_SECRET_SALT_VALUES {
+        pGlobalHmacSalt: std::ptr::addr_of_mut!(hmac_secret_salt) as *mut _,
+        pCredWithHmacSecretSaltList: [].as_mut_ptr(),
+        cCredWithHmacSecretSaltList: 0,
+    };
+    let mut app_id_used: BOOL = false.into();
 
-    let opts = PublicKeyCredentialRequestOptions {
-        challenge: base64urlsafedata::Base64UrlSafeData::from(options.challenge),
-        timeout: Some(options.timeout as u32),
-        rp_id: options.rpid,
-        allow_credentials: vec![],
-        user_verification: uv,
-        hints: None,
-        extensions: Some(RequestAuthenticationExtensions {
-            hmac_get_secret: Some(HmacGetSecretInput {
-                output1: base64urlsafedata::Base64UrlSafeData::from(&options.prf_eval_first),
-                output2: None,
-            }),
-            appid: None,
-            uvm: None,
-        })
+    let used: BOOL = true.into();
+    let extension: WEBAUTHN_EXTENSION = WEBAUTHN_EXTENSION {
+        pwszExtensionIdentifier: w!("hmacGetSecret"),
+        cbExtension: 1,
+        pvExtension: &used as *const _ as *mut _,
     };
-    println!("Prepared PublicKeyCredentialRequestOptions: {:?}", opts);
-    let public_key_credential = Win10::default().perform_auth(Url::parse(
-        "https://vault.usdev.bitwarden.pw",
-    ).unwrap(), opts, 60000)
-        .map_err(|_e| Fido2ClientError::AssertionError)?;
-    println!("PublicKeyCredential: {:?}", public_key_credential);
+    let mut extensions: WEBAUTHN_EXTENSIONS = WEBAUTHN_EXTENSIONS {
+        cExtensions: 1,
+        pExtensions: &extension as *const _ as *mut _,
+    };
+    let client_data = WEBAUTHN_CLIENT_DATA {
+        dwVersion: WEBAUTHN_CLIENT_DATA_CURRENT_VERSION,
+        cbClientDataJSON: client_data_json.len() as u32,
+        pbClientDataJSON: client_data_json.as_ptr() as *mut _,
+        pwszHashAlgId: w!("SHA-256"),
+    };
+    let mut list = WEBAUTHN_CREDENTIAL_LIST {
+        cCredentials: 0,
+        ppCredentials: std::ptr::null_mut(), 
+    };
+
+    let getassertopts = WEBAUTHN_AUTHENTICATOR_GET_ASSERTION_OPTIONS {
+            dwVersion: WEBAUTHN_AUTHENTICATOR_GET_ASSERTION_OPTIONS_CURRENT_VERSION,
+            dwTimeoutMilliseconds: options.timeout as u32,
+            CredentialList: WEBAUTHN_CREDENTIALS {
+                cCredentials: 0,
+                pCredentials: [].as_mut_ptr(),
+            },
+            Extensions: extensions,
+            dwAuthenticatorAttachment: 0,
+            dwUserVerificationRequirement: uv,
+            dwFlags: 0,
+            pwszU2fAppId: PCWSTR::null(),
+            pbU2fAppId: std::ptr::addr_of_mut!(app_id_used),
+            pCancellationId: std::ptr::null_mut(),
+            pAllowCredentialList: std::ptr::addr_of_mut!(list),
+            dwCredLargeBlobOperation: 0,
+            cbCredLargeBlob: 0,
+            pbCredLargeBlob: std::ptr::null_mut(),
+            bBrowserInPrivateMode: false.into(),
+            pHmacSecretSaltValues: if true {
+                std::ptr::addr_of_mut!(hmac_secret_salt_values) as *mut _
+            } else {
+                std::ptr::null_mut()
+            },
+        };
+
+    let assertion = unsafe {
+        let a = windows::Win32::Networking::WindowsWebServices::WebAuthNAuthenticatorGetAssertion(
+            unsafe { GetForegroundWindow() },
+            &rp_id,
+            std::ptr::addr_of!(client_data) as *const _,
+            Some(&getassertopts),
+        )
+        .unwrap();
+        *a
+    };
+
+    let id = unsafe {
+        std::slice::from_raw_parts(
+            assertion.Credential.pbId as *const u8,
+            assertion.Credential.cbId as usize,
+        )
+    };
+
+    let signature = unsafe {
+        std::slice::from_raw_parts(
+            assertion.pbSignature as *const u8,
+            assertion.cbSignature as usize,
+        )
+    };
+
+    let authenticator_data = unsafe {
+        std::slice::from_raw_parts(
+            assertion.pbAuthenticatorData as *const u8,
+            assertion.cbAuthenticatorData as usize,
+        )
+    };
 
     Ok(PublicKeyCredential {
-        id: public_key_credential.id,
-        raw_id: public_key_credential.raw_id.to_vec(),
+        id: BASE64_URL_SAFE_NO_PAD.encode(id),
+        raw_id: id.to_vec(),
         response: crate::AuthenticatorAssertionResponse {
             authenticator_data: public_key_credential.response.authenticator_data.to_vec(),
             client_data_json: public_key_credential.response.client_data_json.to_vec(),
-            signature: public_key_credential.response.signature.to_vec(),
+            signature: signature.to_vec(),
             user_handle: public_key_credential.response.user_handle.map(|h| h.to_vec()).unwrap_or_default(),
         },
         prf: public_key_credential.extensions.hmac_get_secret.map(|hmac| {
