a38e431e3c86939ce36f253c80e1b7a263e32815
neuronull <9162534+neuronull@users.noreply.github.com>
2025-11-24 14:01:40 -0700

    clippy

--- apps/desktop/desktop_native/chromium_importer/src/metadata.rs
+++ apps/desktop/desktop_native/chromium_importer/src/metadata.rs
@@ -147,7 +147,7 @@ mod tests {
         for (key, meta) in map.iter() {
             assert_eq!(&meta.id, key);
             assert_eq!(meta.instructions, "chromium");
-            assert!(meta.loaders.iter().any(|l| *l == "file"));
+            assert!(meta.loaders.contains(&"file"));
         }
     }
 
