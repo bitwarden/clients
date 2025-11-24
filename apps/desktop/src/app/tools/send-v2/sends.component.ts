b7d0bc7796fb69d3f33e42167ccdbdfe61b69e17
Isaac Ivins <iivins@livefront.com>
2025-11-24 16:48:53 -0500

    wip

--- apps/desktop/src/app/tools/send-v2/sends.component.ts
+++ apps/desktop/src/app/tools/send-v2/sends.component.ts
@@ -1,10 +1,11 @@
+import { DatePipe } from "@angular/common";
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
   selector: "app-sends-v2",
   standalone: true,
-  imports: [],
-  template: "<p>Sends V2 Component</p>",
+  imports: [DatePipe],
+  templateUrl: "sends.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SendsComponent {}
