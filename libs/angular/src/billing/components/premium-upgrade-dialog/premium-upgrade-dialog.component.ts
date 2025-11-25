e21b7345ba88cc891c33c4d9efe8eff2bd1590ce
jaasen-livefront <jaasen@livefront.com>
2025-11-25 15:54:34 -0800

    adjust copy for month/annual price

--- libs/angular/src/billing/components/premium-upgrade-dialog/premium-upgrade-dialog.component.ts
+++ libs/angular/src/billing/components/premium-upgrade-dialog/premium-upgrade-dialog.component.ts
@@ -97,7 +97,7 @@ export class PremiumUpgradeDialogComponent {
       tagline: tier.description,
       price: {
         amount: tier.passwordManager.annualPrice / 12,
-        cadence: SubscriptionCadenceIds.Monthly,
+        cadence: this.i18nService.t(SubscriptionCadenceIds.Monthly),
       },
       button: {
         text: this.i18nService.t("upgradeNow"),
