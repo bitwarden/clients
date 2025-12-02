26523b6bfb885c219e5079450f9e5d2a22796ec1
Isaac Ivins <iivins@livefront.com>
2025-12-02 11:55:14 -0500

    wip

--- apps/desktop/src/app/tools/send-v2/send-v2.component.ts
+++ apps/desktop/src/app/tools/send-v2/send-v2.component.ts
@@ -3,7 +3,7 @@ import { Component, ChangeDetectionStrategy } from "@angular/core";
 @Component({
   selector: "app-send-v2",
   imports: [],
-  template: "<p>Sends V2 Component</p>",
+  templateUrl: "./send-v2.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SendV2Component {}
