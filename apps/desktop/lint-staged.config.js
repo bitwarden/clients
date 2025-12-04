e77f57029311658e6180663fd18e3ac48d3d8e03
neuronull <9162534+neuronull@users.noreply.github.com>
2025-12-04 15:43:57 -0700

    prettier

--- apps/desktop/lint-staged.config.js
+++ apps/desktop/lint-staged.config.js
@@ -1,7 +1,7 @@
 export default {
   "*.rs": (stagedFiles) => [
     "cargo +nightly -Z unstable-options -C ./desktop_native fmt",
-    "cargo +nightly -Z unstable-options -C ./desktop_native clippy --all-features --all-targets --tests -- -D warnings"
+    "cargo +nightly -Z unstable-options -C ./desktop_native clippy --all-features --all-targets --tests -- -D warnings",
   ],
   "*Cargo.toml": (stagedFiles) => [
     "cargo +nightly -Z unstable-options -C ./desktop_native udeps --workspace --all-features --all-targets",
