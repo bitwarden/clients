0b55462f59cdfedade48c4017af9a3bb1f5ae5e1
Bernd Schoolmann <mail@quexten.com>
2025-12-09 15:56:12 +0100

    Undo encstring test string change

--- libs/common/src/platform/services/key-state/user-key.state.spec.ts
+++ libs/common/src/platform/services/key-state/user-key.state.spec.ts
@@ -1,4 +1,13 @@
-import { USER_EVER_HAD_USER_KEY } from "./user-key.state";
+import { EncString } from "../../../key-management/crypto/models/enc-string";
+import { EncryptionType } from "../../enums";
+import { Utils } from "../../misc/utils";
+
+import { USER_ENCRYPTED_PRIVATE_KEY, USER_EVER_HAD_USER_KEY } from "./user-key.state";
+
+function makeEncString(data?: string) {
+  data ??= Utils.newGuid();
+  return new EncString(EncryptionType.AesCbc256_HmacSha256_B64, data, "test", "test");
+}
 
 describe("Ever had user key", () => {
   const sut = USER_EVER_HAD_USER_KEY;
@@ -9,5 +18,17 @@ describe("Ever had user key", () => {
     const result = sut.deserializer(JSON.parse(JSON.stringify(everHadUserKey)));
 
     expect(result).toEqual(everHadUserKey);
+  });
+});
+
+describe("Encrypted private key", () => {
+  const sut = USER_ENCRYPTED_PRIVATE_KEY;
+
+  it("should deserialize encrypted private key", () => {
+    const encryptedPrivateKey = makeEncString().encryptedString;
+
+    const result = sut.deserializer(JSON.parse(JSON.stringify(encryptedPrivateKey)));
+
+    expect(result).toEqual(encryptedPrivateKey);
   });
 });
