c4bc068fbf12cd1c00047bc48422875a10bcefb7
Jared Scarito <me@jaredscarito.com>
2025-12-08 21:36:06 -0500

    Fix sdk.service.ts

--- libs/common/src/platform/abstractions/sdk/sdk.service.ts
+++ libs/common/src/platform/abstractions/sdk/sdk.service.ts
@@ -1,10 +1,6 @@
 import { Observable } from "rxjs";
 
-import { BitwardenClient, Uuid, DeviceType as SdkDeviceType } from "@bitwarden/sdk-internal";
-
-// PasswordManagerClient is an alias for BitwardenClient in newer SDK versions
-// Using BitwardenClient for compatibility
-type PasswordManagerClient = BitwardenClient;
+import { PasswordManagerClient, Uuid, DeviceType as SdkDeviceType } from "@bitwarden/sdk-internal";
 
 import { DeviceType } from "../../../enums";
 import { UserId } from "../../../types/guid";
