0728f78a49b3a36a31112e59ecd7f880d5f43f9c
adudek-bw <adudek@bitwarden.com>
2025-12-09 16:48:32 -0500

    cargo fmt

--- apps/desktop/desktop_native/chromium_importer/src/chromium/platform/linux.rs
+++ apps/desktop/desktop_native/chromium_importer/src/chromium/platform/linux.rs
@@ -18,10 +18,7 @@ use crate::{
 pub(crate) const SUPPORTED_BROWSERS: &[BrowserConfig] = &[
     BrowserConfig {
         name: "Chrome",
-        data_dir: &[
-            ".config/google-chrome",
-            "snap/chromium/common/chromium",
-        ],
+        data_dir: &[".config/google-chrome", "snap/chromium/common/chromium"],
     },
     BrowserConfig {
         name: "Chromium",
