7ca158ed87015af5b6aa912624dafe6a482346c9
John Harrington <84741727+harr1424@users.noreply.github.com>
2025-12-10 10:04:01 -0700

    remove hostname from url fallback

--- libs/importer/src/importers/arc-csv-importer.ts
+++ libs/importer/src/importers/arc-csv-importer.ts
@@ -14,7 +14,7 @@ export class ArcCsvImporter extends BaseImporter implements Importer {
 
     results.forEach((value) => {
       const cipher = this.initLoginCipher();
-      const url = this.getValueOrDefault(value.url, this.getValueOrDefault(value.hostname));
+      const url = this.getValueOrDefault(value.url);
       cipher.name = this.getValueOrDefault(this.nameFromUrl(url), "--");
       cipher.login.username = this.getValueOrDefault(value.username);
       cipher.login.password = this.getValueOrDefault(value.password);
