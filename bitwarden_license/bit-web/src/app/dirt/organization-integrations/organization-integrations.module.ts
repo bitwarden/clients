5c8d7664c67f96bebf694dc0f92381ad2c7410ab
voommen-livefront <voommen@livefront.com>
2025-12-10 14:27:42 -0600

    PM-28450 removed datadog and Hec specific files

--- bitwarden_license/bit-web/src/app/dirt/organization-integrations/organization-integrations.module.ts
+++ bitwarden_license/bit-web/src/app/dirt/organization-integrations/organization-integrations.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from "@angular/core";
 
-import { DatadogOrganizationIntegrationService } from "@bitwarden/bit-common/dirt/organization-integrations/services/datadog-organization-integration-service";
-import { HecOrganizationIntegrationService } from "@bitwarden/bit-common/dirt/organization-integrations/services/hec-organization-integration-service";
 import { OrganizationIntegrationApiService } from "@bitwarden/bit-common/dirt/organization-integrations/services/organization-integration-api.service";
 import { OrganizationIntegrationConfigurationApiService } from "@bitwarden/bit-common/dirt/organization-integrations/services/organization-integration-configuration-api.service";
 import { OrganizationIntegrationService } from "@bitwarden/bit-common/dirt/organization-integrations/services/organization-integration-service";
@@ -14,16 +12,6 @@ import { OrganizationIntegrationsRoutingModule } from "./organization-integratio
 @NgModule({
   imports: [AdminConsoleIntegrationsComponent, OrganizationIntegrationsRoutingModule],
   providers: [
-    safeProvider({
-      provide: DatadogOrganizationIntegrationService,
-      useClass: DatadogOrganizationIntegrationService,
-      deps: [OrganizationIntegrationApiService, OrganizationIntegrationConfigurationApiService],
-    }),
-    safeProvider({
-      provide: HecOrganizationIntegrationService,
-      useClass: HecOrganizationIntegrationService,
-      deps: [OrganizationIntegrationApiService, OrganizationIntegrationConfigurationApiService],
-    }),
     safeProvider({
       provide: OrganizationIntegrationService,
       useClass: OrganizationIntegrationService,
