b07d0ae39bf570f06dfa1bbc7d4b63719d609d65
Derek Nance <dnance@bitwarden.com>
2025-12-12 17:06:02 -0600

    WIP bit-desktop entrypoints
    Need to get preload and renderer set up in commercial webpack config,
    otherwise it fails when running webpack.

---
+++ bitwarden_license/bit-desktop/src/entry.ts
@@ -0,0 +1,8 @@
+// implicitly execute the OSS entrypoint
+import "@bitwarden/desktop/entry";
+
+// TODO coalesce
+// import {Main as OssMain} from "@bitwarden/desktop/main";
+
+// export class Main extends OssMain
+// { }
---
+++ bitwarden_license/bit-desktop/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const { buildConfig } = require("../../apps/desktop/webpack.base");
+
+module.exports = (webpackConfig, context) => {
+  // Detect if called by Nx (context parameter exists)
+  const isNxBuild = context && context.options;
+
+  if (isNxBuild) {
+    return buildConfig({
+      configName: "Commercial",
+      main: {
+        entry: path.resolve(__dirname, "src/entry.ts"),
+        tsConfig: path.resolve(context.context.root, "bitwarden_license/bit-desktop/tsconfig.json"),
+      },
+      outputPath: path.resolve(context.context.root, context.options.outputPath),
+    });
+  } else {
+    return buildConfig({
+      configName: "Commercial",
+      main: {
+        entry: path.resolve(__dirname, "src/entry.ts"),
+        tsConfig: path.resolve(__dirname, "tsconfig.json"),
+      },
+    });
+  }
+};
