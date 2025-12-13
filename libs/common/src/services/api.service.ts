3f8cd35f160c51251ea0a36c6dd582aea5b88279
Jared Scarito <me@jaredscarito.com>
2025-12-13 13:05:07 -0500

    Add back comment

--- libs/common/src/services/api.service.ts
+++ libs/common/src/services/api.service.ts
@@ -1602,6 +1602,7 @@ export class ApiService implements ApiServiceAbstraction {
       throw new Error("The request URL contains dangerous patterns.");
     }
 
+    // Prevent directory traversal from malicious paths
     const requestUrl =
       apiUrl + Utils.normalizePath(pathParts[0]) + (pathParts.length > 1 ? `?${pathParts[1]}` : "");
 
