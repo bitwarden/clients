1d46284a0192a8690bd777c73ccb2f2a53dd6332
Anders Åberg <anders@andersaberg.com>
2025-12-16 10:36:25 +0100

    Added open and all its transitive dependencies to the webpack allowlist
     so they get bundled instead of being marked as external (which caused pnpm symlink issues with pkg)

--- apps/cli/webpack.base.js
+++ apps/cli/webpack.base.js
@@ -108,7 +108,22 @@ module.exports.buildConfig = function buildConfig(params) {
     externals: [
       nodeExternals({
         modulesDir: params.externalsModulesDir,
-        allowlist: [/@bitwarden/],
+        allowlist: [
+          /@bitwarden/,
+          // Bundle 'open' and its dependencies to avoid pnpm symlink issues with pkg
+          /^open$/,
+          /^wsl-utils$/,
+          /^powershell-utils$/,
+          /^define-lazy-prop$/,
+          /^default-browser$/,
+          /^default-browser-id$/,
+          /^bundle-name$/,
+          /^run-applescript$/,
+          /^is-inside-container$/,
+          /^is-in-ssh$/,
+          /^is-docker$/,
+          /^is-wsl$/,
+        ],
       }),
     ],
     experiments: {
