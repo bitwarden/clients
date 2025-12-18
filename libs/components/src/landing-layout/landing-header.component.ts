22f76778c1863ae5f51abc6d65ffc8680796f629
Bryan Cunningham <bryan.cunningham@me.com>
2025-12-18 16:37:01 -0500

    add missing imports to header

--- libs/components/src/landing-layout/landing-header.component.ts
+++ libs/components/src/landing-layout/landing-header.component.ts
@@ -1,10 +1,18 @@
 import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { BitwardenLogo } from "@bitwarden/assets/svg";
+
+import { IconModule } from "../icon";
+import { SharedModule } from "../shared";
 
 @Component({
   selector: "bit-landing-header",
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: "./landing-header.component.html",
+  imports: [RouterModule, IconModule, SharedModule],
 })
 export class LandingHeaderComponent {
   readonly hideLogo = input<boolean>(false);
+  protected logo = BitwardenLogo;
 }
