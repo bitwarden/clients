44e125da3d1898f590e597239fcd137e22f7ca0a
Derek Nance <dnance@bitwarden.com>
2025-12-18 17:11:41 -0600

    renderer is finally up and running

--- bitwarden_license/bit-desktop/src/app/app.component.ts
+++ bitwarden_license/bit-desktop/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from "@angular/core";
 
-import { BaseAppComponent } from "./base-app.component";
+import { BaseAppComponent } from "@bitwarden/desktop/app/base-app.component";
 
 // FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
 // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
   selector: "app-root",
   styles: [],
-  templateUrl: "app.component.html",
+  templateUrl: "../../../../apps/desktop/src/app/app.component.html",
   standalone: false,
 })
 export class AppComponent extends BaseAppComponent {}
--- bitwarden_license/bit-desktop/src/entry.ts
+++ bitwarden_license/bit-desktop/src/entry.ts
@@ -1,8 +1,2 @@
 // implicitly execute the OSS entrypoint
 import "@bitwarden/desktop/entry";
-
-// TODO coalesce
-// import {Main as OssMain} from "@bitwarden/desktop/main";
-
-// export class Main extends OssMain
-// { }
