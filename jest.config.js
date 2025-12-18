37d61673b56cd56b611f9fd7df5726821e9402e0
Anders Åberg <anders@andersaberg.com>
2025-12-18 19:16:58 +0100

    Trigger CI workflows for pnpm timing analysis

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+// TODO: Remove this comment - added to trigger CI workflows for pnpm timing analysis
 const { pathsToModuleNameMapper } = require("ts-jest");
 
 const { compilerOptions } = require("./tsconfig.base");
