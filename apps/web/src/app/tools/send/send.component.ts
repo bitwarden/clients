16cac33d92c9ffa547d4b3a264f564f4154b6a56
Ben McFerren <bmcferren@bitwarden.com>
2025-12-22 07:41:32 -0500

    PM-28183 resolved pr comment re defining filteredSends member variable

--- apps/web/src/app/tools/send/send.component.ts
+++ apps/web/src/app/tools/send/send.component.ts
@@ -16,11 +16,11 @@ import { EnvironmentService } from "@bitwarden/common/platform/abstractions/envi
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
-import { SendType } from "@bitwarden/common/tools/send/types/send-type";
 import { SendView } from "@bitwarden/common/tools/send/models/view/send.view";
 import { SendApiService } from "@bitwarden/common/tools/send/services/send-api.service.abstraction";
 import { SendService } from "@bitwarden/common/tools/send/services/send.service.abstraction";
 import { SendFilterType } from "@bitwarden/common/tools/send/types/send-filter-type";
+import { SendType } from "@bitwarden/common/tools/send/types/send-type";
 import { SendId } from "@bitwarden/common/types/guid";
 import { SearchService } from "@bitwarden/common/vault/abstractions/search.service";
 import {
@@ -140,7 +140,6 @@ export class SendComponent extends BaseSendComponent implements OnInit, OnDestro
   async ngOnInit() {
     await super.ngOnInit();
 
-    this.filteredSends = [];
     await this.load();
 
     if (this.selectedToggleValue) {
