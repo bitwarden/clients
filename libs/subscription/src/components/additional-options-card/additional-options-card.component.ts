d89fa00618bbc1f72d0db4bc934159d692855265
Alex Morask <amorask@bitwarden.com>
2025-12-29 13:30:06 -0600

    feat(subscription): additional-options-card.component
    - Add additional-options-card.component
    - Add spec file, stories and MDX file

---
+++ libs/subscription/src/components/additional-options-card/additional-options-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, ChangeDetectionStrategy, output } from "@angular/core";
+
+import { ButtonModule, CardComponent, TypographyModule } from "@bitwarden/components";
+import { I18nPipe } from "@bitwarden/ui-common";
+
+export type AdditionalOptionsCardAction = "download-license" | "cancel-subscription";
+
+@Component({
+  selector: "billing-additional-options-card",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./additional-options-card.component.html",
+  imports: [ButtonModule, CardComponent, TypographyModule, I18nPipe],
+})
+export class AdditionalOptionsCardComponent {
+  readonly callToActionClicked = output<AdditionalOptionsCardAction>();
+}
