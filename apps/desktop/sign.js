14da87a236456c4f904a4267a26504c497a3e572
Isaiah Inuwa <iinuwa@bitwarden.com>
2025-12-31 11:05:41 -0600

    Use execFileSync instead of execSync

--- apps/desktop/sign.js
+++ apps/desktop/sign.js
@@ -5,17 +5,22 @@ exports.default = async function (configuration) {
   const ext = configuration.path.split(".").at(-1);
   if (parseInt(process.env.ELECTRON_BUILDER_SIGN) === 1 && ext == "exe") {
     console.log(`[*] Signing file: ${configuration.path}`);
-    child_process.execSync(
-      `azuresigntool sign -v ` +
-        `-kvu ${process.env.SIGNING_VAULT_URL} ` +
-        `-kvi ${process.env.SIGNING_CLIENT_ID} ` +
-        `-kvt ${process.env.SIGNING_TENANT_ID} ` +
-        `-kvs ${process.env.SIGNING_CLIENT_SECRET} ` +
-        `-kvc ${process.env.SIGNING_CERT_NAME} ` +
-        `-fd ${configuration.hash} ` +
-        `-du ${configuration.site} ` +
-        `-tr http://timestamp.digicert.com ` +
-        `"${configuration.path}"`,
+    child_process.execFileSync(
+      "azuresigntool",
+      // prettier-ignore
+      [
+        "sign",
+        "-v",
+        "-kvu", process.env.SIGNING_VAULT_URL,
+        "-kvi", process.env.SIGNING_CLIENT_ID,
+        "-kvt", process.env.SIGNING_TENANT_ID,
+        "-kvs", process.env.SIGNING_CLIENT_SECRET,
+        "-kvc", process.env.SIGNING_CERT_NAME,
+        "-fd", configuration.hash,
+        "-du", configuration.site,
+        "-tr", "http://timestamp.digicert.com",
+        configuration.path,
+      ],
       {
         stdio: "inherit",
       },
