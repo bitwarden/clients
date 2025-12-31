b63c89fbe00c61b872f433a62a2b72c7fb82ea3c
Isaiah Inuwa <iinuwa@bitwarden.com>
2025-12-31 07:09:15 -0600

    Add documentation

--- apps/desktop/cross-build.ps1
+++ apps/desktop/cross-build.ps1
@@ -1,11 +1,76 @@
 #!/usr/bin/env pwsh
+
+<#
+.SYNOPSIS
+Script to build, package and sign the Bitwarden desktop client as a Windows Appx
+package.
+
+.DESCRIPTION
+This script provides cross-platform support for packaging and signing the
+Bitwarden desktop client as a Windows Appx package.
+
+Currently, only macOS -> Windows Appx is supported, but Linux -> Windows Appx
+could be added in the future by providing Linux binaries for the msix-packaging
+project.
+
+.NOTES
+The reason this script exists is because electron-builder does not currently
+support cross-platform Appx packaging without proprietary tools (Parallels
+Windows VM). This script uses third-party tools (makemsix from msix-packaging
+and osslsigncode) to package and sign the Appx.
+
+The signing certificate must have the same subject as the publisher name. This
+can be generated on the Windows target using PowerShell 5.1 and copied to the
+host, or directly on the host with OpenSSL.
+
+Using Windows PowerShell 5.1:
+```powershell
+$publisher = "CN=Bitwarden Inc., O=Bitwarden Inc., L=Santa Barbara, S=California, C=US, SERIALNUMBER=7654941, OID.2.5.4.15=Private Organization, OID.1.3.6.1.4.1.311.60.2.1.2=Delaware, OID.1.3.6.1.4.1.311.60.2.1.3=US"
+$certificate = New-SelfSignedCertificate -Type Custom -KeyUsage DigitalSignature -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") -Subject $publisher -FriendlyName "Bitwarden Developer Signing Certificate"
+$password = Read-Host -AsSecureString
+Export-PfxCertificate -cert "Cert:\CurrentUser\My\${$certificate.Thumbprint}" -FilePath "C:\path/to/pfx" -Password $password
+```
+
+Using OpenSSL:
+```sh
+subject="jurisdictionCountryName=US/jurisdictionStateOrProvinceName=Delaware/businessCategory=Private Organization/serialNumber=7654941, C=US, ST=California, L=Santa Barbara, O=Bitwarden Inc., CN=Bitwarden Inc."
+keyfile="/tmp/mysigning.rsa.pem"
+certfile="/tmp/mysigning.cert.pem"
+p12file="/tmp/mysigning.p12"
+openssl req -x509 -keyout "$keyfile" -out "$certfile" -subj "$subject" \
+    -newkey rsa:2048 -days 3650 -nodes \
+    -addext 'keyUsage=critical,digitalSignature' \
+    -addext 'extendedKeyUsage=critical,codeSigning' \
+    -addext 'basicConstraints=critical,CA:FALSE'
+openssl pkcs12 -inkey "$keyfile" -in "$certfile" -export -out "$p12file"
+rm $keyfile
+```
+
+.EXAMPLE
+./cross-build.ps1 -Architecture arm64 -CertificatePath ~/Development/code-signing.pfx -CertificatePassword (Read-Host -AsSecureString) -Release -ElectronConfigFile ./electron-builder.beta.json
+
+Reads the signing certificate password from user input, then builds, packages
+and signs the Appx.
+
+Alternatively, you can specify the CERTIFICATE_PASSWORD environment variable.
+#>
 param(
     [Parameter(Mandatory=$true)]
     [ValidateSet("X64", "ARM64")]$Architecture,
+    [string]
+    # Path to PKCS12 certificate file. If not specified, the Appx will not be signed.
     $CertificatePath,
-    [SecureString]$CertificatePassword,
+    [SecureString]
+    # Password for PKCS12 certificate. Alternatively, may be specified in
+    # CERTIFICATE_PASSWORD environment variable. If not specified, the Appx will
+    # not be signed.
+    $CertificatePassword,
+    [string]
+    # Path to electron-builder JSON configuration file, relative to the script directory.
     $ElectronConfigFile="electron-builder.json",
-    [Switch]$Release=$false
+    [Switch]
+    # Whether to build in release mode.
+    $Release=$false
 )
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
