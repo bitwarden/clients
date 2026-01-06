993b57187911df36e798d20299e7b5f050ee95b3
Isaac Ivins <iivins@livefront.com>
2026-01-06 12:47:38 -0500

    removing web related code

--- apps/web/src/app/layouts/header/web-header.component.ts
+++ apps/web/src/app/layouts/header/web-header.component.ts
@@ -1,44 +1,75 @@
-import { ChangeDetectionStrategy, Component, inject, input } from "@angular/core";
+// FIXME: Update this file to be type safe and remove this and next line
+// @ts-strict-ignore
+import { Component, Input } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { map, Observable } from "rxjs";
 
-import { BannerModule, HeaderComponent } from "@bitwarden/components";
+import { User } from "@bitwarden/angular/pipes/user-name.pipe";
+import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
+import {
+  VaultTimeoutAction,
+  VaultTimeoutSettingsService,
+} from "@bitwarden/common/key-management/vault-timeout";
+import { MessagingService } from "@bitwarden/common/platform/abstractions/messaging.service";
+import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
+import { UserId } from "@bitwarden/common/types/guid";
 
-import { SharedModule } from "../../shared";
-import { ProductSwitcherModule } from "../product-switcher/product-switcher.module";
-
-import { AccountMenuComponent } from "./account-menu.component";
-
+// FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
+// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
   selector: "app-header",
   templateUrl: "./web-header.component.html",
-  imports: [
-    SharedModule,
-    ProductSwitcherModule,
-    BannerModule,
-    HeaderComponent,
-    AccountMenuComponent,
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
 export class WebHeaderComponent {
-  private route = inject(ActivatedRoute);
-
   /**
    * Custom title that overrides the route data `titleId`
    */
-  readonly title = input<string>();
+  // FIXME(https://bitwarden.atlassian.net/browse/CL-903): Migrate to Signals
+  // eslint-disable-next-line @angular-eslint/prefer-signals
+  @Input() title: string;
 
   /**
    * Icon to show before the title
    */
-  readonly icon = input<string>();
+  // FIXME(https://bitwarden.atlassian.net/browse/CL-903): Migrate to Signals
+  // eslint-disable-next-line @angular-eslint/prefer-signals
+  @Input() icon: string;
 
-  protected routeData$: Observable<{ titleId: string }> = this.route.data.pipe(
-    map((params) => {
-      return {
-        titleId: params.titleId,
-      };
-    }),
-  );
+  protected routeData$: Observable<{ titleId: string }>;
+  protected account$: Observable<User & { id: UserId }>;
+  protected canLock$: Observable<boolean>;
+  protected selfHosted: boolean;
+  protected hostname = location.hostname;
+
+  constructor(
+    private route: ActivatedRoute,
+    private platformUtilsService: PlatformUtilsService,
+    private vaultTimeoutSettingsService: VaultTimeoutSettingsService,
+    private messagingService: MessagingService,
+    private accountService: AccountService,
+  ) {
+    this.routeData$ = this.route.data.pipe(
+      map((params) => {
+        return {
+          titleId: params.titleId,
+        };
+      }),
+    );
+
+    this.selfHosted = this.platformUtilsService.isSelfHost();
+
+    this.account$ = this.accountService.activeAccount$;
+    this.canLock$ = this.vaultTimeoutSettingsService
+      .availableVaultTimeoutActions$()
+      .pipe(map((actions) => actions.includes(VaultTimeoutAction.Lock)));
+  }
+
+  protected lock() {
+    this.messagingService.send("lockVault");
+  }
+
+  protected logout() {
+    this.messagingService.send("logout");
+  }
 }
