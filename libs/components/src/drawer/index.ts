813969a6321398f570b3387e9a0aa428c294946c
Ben McFerren <bmcferren@bitwarden.com>
2026-01-06 14:10:44 -0500

    PM-28182 removed unneeded export

--- libs/components/src/drawer/index.ts
+++ libs/components/src/drawer/index.ts
@@ -3,4 +3,3 @@ export * from "./drawer.component";
 export * from "./drawer-body.component";
 export * from "./drawer-close.directive";
 export * from "./drawer-header.component";
-export * from "./drawer.service";
