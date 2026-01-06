1c07990f1d6f096b8bad5c5cc04ab08493e4dc1f
Isaac Ivins <iivins@livefront.com>
2026-01-06 14:12:09 -0500

    resetting FF

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -176,9 +176,6 @@ export function getFeatureFlagValue<Flag extends FeatureFlag>(
   serverConfig: ServerConfig | null,
   flag: Flag,
 ) {
-  if (flag === FeatureFlag.DesktopUiMigrationMilestone1) {
-    return true;
-  }
   if (serverConfig?.featureStates == null || serverConfig.featureStates[flag] == null) {
     return DefaultFeatureFlagValue[flag];
   }
