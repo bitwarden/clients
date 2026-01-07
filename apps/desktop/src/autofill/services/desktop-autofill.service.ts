bcaefeac473fb9e9629f00e9223671cf3a2b9ee8
Isaiah Inuwa <iinuwa@bitwarden.com>
2026-01-07 10:23:49 -0600

    Move macOS-specific window positioning to macOS provider

--- apps/desktop/src/autofill/services/desktop-autofill.service.ts
+++ apps/desktop/src/autofill/services/desktop-autofill.service.ts
@@ -227,7 +227,7 @@ export class DesktopAutofillService implements OnDestroy {
       try {
         const response = await this.fido2AuthenticatorService.makeCredential(
           this.convertRegistrationRequest(request),
-          { windowXy: normalizePosition(request.windowXy) },
+          { windowXy: request.windowXy },
           controller,
         );
 
@@ -293,7 +293,7 @@ export class DesktopAutofillService implements OnDestroy {
 
           const response = await this.fido2AuthenticatorService.getAssertion(
             this.convertAssertionRequest(request, true),
-            { windowXy: normalizePosition(request.windowXy) },
+            { windowXy: request.windowXy },
             controller,
           );
 
@@ -321,7 +321,7 @@ export class DesktopAutofillService implements OnDestroy {
       try {
         const response = await this.fido2AuthenticatorService.getAssertion(
           this.convertAssertionRequest(request),
-          { windowXy: normalizePosition(request.windowXy) },
+          { windowXy: request.windowXy },
           controller,
         );
 
@@ -454,14 +454,4 @@ export class DesktopAutofillService implements OnDestroy {
     this.destroy$.next();
     this.destroy$.complete();
   }
-}
-
-function normalizePosition(position: { x: number; y: number }): { x: number; y: number } {
-  // Add 100 pixels to the x-coordinate to offset the native OS dialog positioning.
-  const xPositionOffset = 100;
-
-  return {
-    x: Math.round(position.x + xPositionOffset),
-    y: Math.round(position.y),
-  };
 }
