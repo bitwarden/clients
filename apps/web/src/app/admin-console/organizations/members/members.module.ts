5b93ff02e86c739a299b15bfad59f598c71f5f44
Brandon <btreston@bitwarden.com>
2026-01-08 12:48:10 -0500

    wip

--- apps/web/src/app/admin-console/organizations/members/members.module.ts
+++ apps/web/src/app/admin-console/organizations/members/members.module.ts
@@ -17,8 +17,9 @@ import { BulkRemoveDialogComponent } from "./components/bulk/bulk-remove-dialog.
 import { BulkRestoreRevokeComponent } from "./components/bulk/bulk-restore-revoke.component";
 import { BulkStatusComponent } from "./components/bulk/bulk-status.component";
 import { UserDialogModule } from "./components/member-dialog";
+import { MembersComponent } from "./deprecated_members.component";
 import { MembersRoutingModule } from "./members-routing.module";
-import { MembersComponent } from "./members.component";
+import { vNextMembersComponent } from "./members.component";
 import {
   OrganizationMembersService,
   MemberActionsService,
@@ -44,6 +45,7 @@ import {
     BulkRestoreRevokeComponent,
     BulkStatusComponent,
     MembersComponent,
+    vNextMembersComponent,
     BulkDeleteDialogComponent,
   ],
   providers: [
