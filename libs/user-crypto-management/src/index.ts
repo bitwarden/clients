0fc53d6277a62bfb9c74338133c699045917b2be
Bernd Schoolmann <mail@quexten.com>
2026-01-09 13:02:07 +0100

    Implement client-side user-key-rotation-service

---
+++ libs/user-crypto-management/src/index.ts
@@ -0,0 +1,2 @@
+export { DefaultUserKeyRotationService as UserKeyRotationService } from "./user-key-rotation.service";
+export { UserKeyRotationService as UserKeyRotationServiceAbstraction } from "./user-key-rotation.service.abstraction";
