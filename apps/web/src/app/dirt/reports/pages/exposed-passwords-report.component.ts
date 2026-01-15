81a4c32ed12be5943b406c3c32fcc2943ffc4829
voommen-livefront <voommen@livefront.com>
2026-01-15 09:12:45 -0600

    PM-25901 cannot change changedetectionstrategy as it causes the page to fail

--- apps/web/src/app/dirt/reports/pages/exposed-passwords-report.component.ts
+++ apps/web/src/app/dirt/reports/pages/exposed-passwords-report.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { AuditService } from "@bitwarden/common/abstractions/audit.service";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
@@ -18,8 +18,9 @@ import { CipherReportComponent } from "./cipher-report.component";
 
 type ReportResult = CipherView & { exposedXTimes: number };
 
+// FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
+// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "app-exposed-passwords-report",
   templateUrl: "exposed-passwords-report.component.html",
   standalone: false,
