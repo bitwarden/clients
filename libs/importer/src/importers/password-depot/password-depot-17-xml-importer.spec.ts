acad7545409d7bb1c2bf8f8d3ee999a43af475ff
Leslie Xiong <lxiong@livefront.com>
2026-01-15 10:17:00 -0500

    Desktop/pm 18769/migrate vault filters (#17919)
    Migrated vault filters to new v3 vault's navigation
    
    * Decoupled existing vault filtering from vault component by using routed params with routed-vault-filter-bridge
    * Converted vault filters to standalone components
    * Removed extending filter Base Components from deprecated /libs/angular library and handled logic directly
    * Moved shared 'models' and 'services' directories from web-vault into /libs/vault

--- libs/importer/src/importers/password-depot/password-depot-17-xml-importer.spec.ts
+++ libs/importer/src/importers/password-depot/password-depot-17-xml-importer.spec.ts
@@ -1,10 +1,8 @@
-// This import has been flagged as unallowed for this class. It may be involved in a circular dependency loop.
-// eslint-disable-next-line no-restricted-imports
-import { CollectionView } from "@bitwarden/admin-console/common";
-import { CollectionId, OrganizationId } from "@bitwarden/common/types/guid";
+import { CollectionView } from "@bitwarden/common/admin-console/models/collections";
+import { OrganizationId } from "@bitwarden/common/types/guid";
 import { FieldType, SecureNoteType } from "@bitwarden/common/vault/enums";
 import { FolderView } from "@bitwarden/common/vault/models/view/folder.view";
-import { CipherType } from "@bitwarden/sdk-internal";
+import { CipherType, CollectionId } from "@bitwarden/sdk-internal";
 
 import {
   EncryptedFileData,
