102f78ad853127e51742f3c7b9d9baad72f6a5c1
Bryan Cunningham <bryan.cunningham@me.com>
2026-01-15 13:41:14 -0500

    create link component to handle anchors and buttons

--- libs/components/src/link/link.module.ts
+++ libs/components/src/link/link.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from "@angular/core";
 
 import { LinkComponent } from "./link.component";
-import { ButtonLinkDirective } from "./link.directive";
 
 @NgModule({
-  imports: [LinkComponent, ButtonLinkDirective],
-  exports: [LinkComponent, ButtonLinkDirective],
+  imports: [LinkComponent],
+  exports: [LinkComponent],
 })
 export class LinkModule {}
