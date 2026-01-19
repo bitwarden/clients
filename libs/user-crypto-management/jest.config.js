a508e5d11d7826e4e2ca481865228ed1d5c11273
Bernd Schoolmann <mail@quexten.com>
2026-01-19 12:47:16 +0100

    Add tests

--- libs/user-crypto-management/jest.config.js
+++ libs/user-crypto-management/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   displayName: "user-crypto-management",
   preset: "../../jest.preset.js",
   testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/test.setup.ts"],
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
