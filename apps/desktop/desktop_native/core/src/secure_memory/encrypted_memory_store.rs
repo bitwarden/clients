48adaaea8678ea11a8870148d8af8d110725e9c0
neuronull <9162534+neuronull@users.noreply.github.com>
2026-01-21 15:36:29 -0700

    Generalize EncryptedMemoryStore

--- apps/desktop/desktop_native/core/src/secure_memory/encrypted_memory_store.rs
+++ apps/desktop/desktop_native/core/src/secure_memory/encrypted_memory_store.rs
@@ -1,7 +1,9 @@
+use std::collections::BTreeMap;
+
 use tracing::error;
 
 use crate::secure_memory::{
-    secure_key::{EncryptedMemory, SecureMemoryEncryptionKey},
+    secure_key::{DecryptionError, EncryptedMemory, SecureMemoryEncryptionKey},
     SecureMemoryStore,
 };
 
@@ -12,50 +14,86 @@ use crate::secure_memory::{
 ///
 /// The key is briefly in process memory during encryption and decryption, in memory that is
 /// protected from swapping to disk via mlock, and then zeroed out immediately after use.
-#[allow(unused)]
-pub(crate) struct EncryptedMemoryStore {
-    map: std::collections::HashMap<String, EncryptedMemory>,
+/// # Type Parameters
+///
+/// * `K` - The type of the key.
+pub struct EncryptedMemoryStore<K>
+where
+    K: std::cmp::Ord + std::fmt::Display + std::clone::Clone,
+{
+    map: BTreeMap<K, EncryptedMemory>,
     memory_encryption_key: SecureMemoryEncryptionKey,
 }
 
-impl EncryptedMemoryStore {
-    #[allow(unused)]
-    pub(crate) fn new() -> Self {
+impl<K> EncryptedMemoryStore<K>
+where
+    K: std::cmp::Ord + std::fmt::Display + std::clone::Clone,
+{
+    #[must_use]
+    pub fn new() -> Self {
         EncryptedMemoryStore {
-            map: std::collections::HashMap::new(),
+            map: BTreeMap::new(),
             memory_encryption_key: SecureMemoryEncryptionKey::new(),
         }
     }
+
+    /// # Returns
+    ///
+    /// An array of all decrypted values.
+    ///
+    /// # Errors
+    ///
+    /// `DecryptionError` if an error occured during decryption
+    pub fn to_vec(&mut self) -> Result<Vec<Vec<u8>>, DecryptionError> {
+        let mut result = vec![];
+        let keys: Vec<_> = self.map.keys().cloned().collect();
+
+        for key in &keys {
+            let bytes = self.get(key)?.expect("All keys to still be in map.");
+            result.push(bytes);
+        }
+        Ok(result)
+    }
 }
 
-impl SecureMemoryStore for EncryptedMemoryStore {
-    fn put(&mut self, key: String, value: &[u8]) {
+impl<K> Default for EncryptedMemoryStore<K>
+where
+    K: std::cmp::Ord + std::fmt::Display + std::clone::Clone,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<K> SecureMemoryStore for EncryptedMemoryStore<K>
+where
+    K: std::cmp::Ord + std::fmt::Display + std::clone::Clone,
+{
+    type KeyType = K;
+
+    fn put(&mut self, key: Self::KeyType, value: &[u8]) {
         let encrypted_value = self.memory_encryption_key.encrypt(value);
         self.map.insert(key, encrypted_value);
     }
 
-    fn get(&mut self, key: &str) -> Option<Vec<u8>> {
-        let encrypted_memory = self.map.get(key);
-        if let Some(encrypted_memory) = encrypted_memory {
-            match self.memory_encryption_key.decrypt(encrypted_memory) {
-                Ok(plaintext) => Some(plaintext),
-                Err(_) => {
-                    error!("In memory store, decryption failed for key {}. The memory may have been tampered with. re-keying.", key);
-                    self.memory_encryption_key = SecureMemoryEncryptionKey::new();
-                    self.clear();
-                    None
-                }
-            }
+    fn get(&mut self, key: &Self::KeyType) -> Result<Option<Vec<u8>>, DecryptionError> {
+        if let Some(encrypted) = self.map.get(key) {
+            self.memory_encryption_key.decrypt(encrypted).map_err(|error| {
+                error!(?error, %key, "In memory store, decryption failed. The memory may have been tampered with. Re-keying.");
+                self.memory_encryption_key = SecureMemoryEncryptionKey::new();
+                self.clear();
+                error
+            }).map(Some)
         } else {
-            None
+            Ok(None)
         }
     }
 
-    fn has(&self, key: &str) -> bool {
+    fn has(&self, key: &Self::KeyType) -> bool {
         self.map.contains_key(key)
     }
 
-    fn remove(&mut self, key: &str) {
+    fn remove(&mut self, key: &Self::KeyType) {
         self.map.remove(key);
     }
 
@@ -64,7 +102,10 @@ impl SecureMemoryStore for EncryptedMemoryStore {
     }
 }
 
-impl Drop for EncryptedMemoryStore {
+impl<K> Drop for EncryptedMemoryStore<K>
+where
+    K: std::cmp::Ord + std::fmt::Display + std::clone::Clone,
+{
     fn drop(&mut self) {
         self.clear();
     }
@@ -76,29 +117,28 @@ mod tests {
 
     #[test]
     fn test_secret_kv_store_various_sizes() {
-        let mut store = EncryptedMemoryStore::new();
+        let mut store = EncryptedMemoryStore::default();
         for size in 0..=2048 {
-            let key = format!("test_key_{}", size);
+            let key = format!("test_key_{size}");
             let value: Vec<u8> = (0..size).map(|i| (i % 256) as u8).collect();
             store.put(key.clone(), &value);
-            assert!(store.has(&key), "Store should have key for size {}", size);
+            assert!(store.has(&key), "Store should have key for size {size}");
             assert_eq!(
-                store.get(&key),
+                store.get(&key).expect("entry in map for key"),
                 Some(value),
-                "Value mismatch for size {}",
-                size
+                "Value mismatch for size {size}",
             );
         }
     }
 
     #[test]
     fn test_crud() {
-        let mut store = EncryptedMemoryStore::new();
+        let mut store = EncryptedMemoryStore::default();
         let key = "test_key".to_string();
         let value = vec![1, 2, 3, 4, 5];
         store.put(key.clone(), &value);
         assert!(store.has(&key));
-        assert_eq!(store.get(&key), Some(value));
+        assert_eq!(store.get(&key).expect("entry in map for key"), Some(value));
         store.remove(&key);
         assert!(!store.has(&key));
     }
