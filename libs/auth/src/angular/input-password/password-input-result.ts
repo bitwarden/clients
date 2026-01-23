0446d55cb3c9ae77a20d0b350f0381c4fea86cdd
rr-bw <102181210+rr-bw@users.noreply.github.com>
2026-01-22 16:01:16 -0800

    [PM-27086] Update comment for clarity

--- libs/auth/src/angular/input-password/password-input-result.ts
+++ libs/auth/src/angular/input-password/password-input-result.ts
@@ -12,7 +12,11 @@ export interface PasswordInputResult {
 
   /**
    * Temporary property that persists the flag state through the entire set/change password process.
-   * Consumers will use this value instead of re-checking the flag state via ConfigService themselves.
+   * This allows flows to consume this value instead of re-checking the flag state via ConfigService themselves.
+   *
+   * The ChangePasswordDelegation flows (Emergency Access Takeover and Account Recovery), however, only ever
+   * require a raw newPassword from the InputPasswordComponent regardless of whether the flag is on or off.
+   * Flagging for those 2 flows will be done via the ConfigService in their respective services.
    *
    * To be removed in PM-28143
    */
