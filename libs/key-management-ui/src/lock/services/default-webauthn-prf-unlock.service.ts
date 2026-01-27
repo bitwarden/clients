bb1310ca0dd1c5444d458784464c87882c4e79ed
Anders Åberg <anders@andersaberg.com>
2026-01-27 18:05:30 +0100

    Added missing Utils import

--- libs/key-management-ui/src/lock/services/default-webauthn-prf-unlock.service.ts
+++ libs/key-management-ui/src/lock/services/default-webauthn-prf-unlock.service.ts
@@ -14,6 +14,7 @@ import { ConfigService } from "@bitwarden/common/platform/abstractions/config/co
 import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
+import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { Fido2Utils } from "@bitwarden/common/platform/services/fido2/fido2-utils";
 import { UserId } from "@bitwarden/common/types/guid";
 import { PrfKey, UserKey } from "@bitwarden/common/types/key";
