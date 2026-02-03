a6b6ddb93e082e4bade7bffa041d51caac88a6fc
rr-bw <102181210+rr-bw@users.noreply.github.com>
2026-02-03 10:38:55 -0800

    [PM-27086 TDE & Permission User] update general error handling

--- libs/angular/src/auth/password-management/set-initial-password/default-set-initial-password.service.implementation.ts
+++ libs/angular/src/auth/password-management/set-initial-password/default-set-initial-password.service.implementation.ts
@@ -15,6 +15,7 @@ import { MasterPasswordApiService } from "@bitwarden/common/auth/abstractions/ma
 import { ForceSetPasswordReason } from "@bitwarden/common/auth/models/domain/force-set-password-reason";
 import { SetPasswordRequest } from "@bitwarden/common/auth/models/request/set-password.request";
 import { UpdateTdeOffboardingPasswordRequest } from "@bitwarden/common/auth/models/request/update-tde-offboarding-password.request";
+import { assertNonNullish, assertTruthy } from "@bitwarden/common/auth/utils";
 import { AccountCryptographicStateService } from "@bitwarden/common/key-management/account-cryptography/account-cryptographic-state.service";
 import { EncryptService } from "@bitwarden/common/key-management/crypto/abstractions/encrypt.service";
 import { EncString } from "@bitwarden/common/key-management/crypto/models/enc-string";
@@ -343,15 +344,17 @@ export class DefaultSetInitialPasswordService implements SetInitialPasswordServi
     credentials: SetInitialPasswordTdeUserWithPermissionCredentials,
     userId: UserId,
   ): Promise<void> {
-    if (userId == null) {
-      throw new Error("User ID is required.");
-    }
+    const ctx =
+      "Could not set initial password for TDE user with Manage Account Recovery permission.";
 
-    for (const [key, value] of Object.entries(credentials)) {
-      if (value == null) {
-        throw new Error(`${key} not found.`);
-      }
-    }
+    assertTruthy(credentials.newPassword, "newPassword", ctx);
+    assertTruthy(credentials.salt, "salt", ctx);
+    assertNonNullish(credentials.kdfConfig, "kdfConfig", ctx);
+    assertNonNullish(credentials.newPasswordHint, "newPasswordHint", ctx); // can have an empty string as a valid value, so check non-nullish
+    assertTruthy(credentials.orgSsoIdentifier, "orgSsoIdentifier", ctx);
+    assertTruthy(credentials.orgId, "orgId", ctx);
+    assertNonNullish(credentials.resetPasswordAutoEnroll, "resetPasswordAutoEnroll", ctx); // can have `false` as a valid value, so check non-nullish
+    assertTruthy(userId, "userId", ctx);
 
     const {
       newPassword,
