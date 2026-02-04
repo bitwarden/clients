67256c5919e08503116c081d70143985151b0b6f
Brandon <btreston@bitwarden.com>
2026-02-04 14:20:29 -0500

    add feature flag key

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -15,7 +15,7 @@ export enum FeatureFlag {
   BlockClaimedDomainAccountCreation = "pm-28297-block-uninvited-claimed-domain-registration",
   DefaultUserCollectionRestore = "pm-30883-my-items-restored-users",
   MembersComponentRefactor = "pm-29503-refactor-members-inheritance",
-  BulkReinviteUI = "tbd",
+  BulkReinviteUI = "pm-28416-bulk-reinvite-ux-improvements",
 
   /* Auth */
   PM23801_PrefetchPasswordPrelogin = "pm-23801-prefetch-password-prelogin",
