2a2f29c95a24344958182d5015a3d5a9cf966d83
enmande <3836813+enmande@users.noreply.github.com>
2026-02-05 10:21:00 -0500

    feat(change-email) [PM-30811]: Add new constructor to EmailTokenRequest.

--- libs/common/src/auth/models/request/email-token.request.ts
+++ libs/common/src/auth/models/request/email-token.request.ts
@@ -1,8 +1,24 @@
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
+import { MasterPasswordAuthenticationData } from "@bitwarden/sdk-internal";
+
 import { SecretVerificationRequest } from "./secret-verification.request";
 
 export class EmailTokenRequest extends SecretVerificationRequest {
   newEmail: string;
   masterPasswordHash: string;
+  /**
+   * Creates an EmailTokenRequest using new KM types.
+   * This will eventually become the primary constructor once all callers are updated.
+   *
+   * https://bitwarden.atlassian.net/browse/PM-30811
+   */
+  static newConstructor(authenticationData: MasterPasswordAuthenticationData, newEmail: string) {
+    const emailTokenRequest = new EmailTokenRequest();
+
+    emailTokenRequest.newEmail = newEmail;
+    emailTokenRequest.masterPasswordHash = authenticationData.masterPasswordAuthenticationHash;
+
+    return emailTokenRequest;
+  }
 }
