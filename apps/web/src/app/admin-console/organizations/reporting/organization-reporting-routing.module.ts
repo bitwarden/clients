74de8942411812948f747780b2075306190eae3b
Leslie Tilton <23057410+Banrion@users.noreply.github.com>
2026-02-05 01:40:47 -0600

    Remove access-intelligence-prototype route

--- apps/web/src/app/admin-console/organizations/reporting/organization-reporting-routing.module.ts
+++ apps/web/src/app/admin-console/organizations/reporting/organization-reporting-routing.module.ts
@@ -21,7 +21,6 @@ import { organizationPermissionsGuard } from "../guards/org-permissions.guard";
 import { organizationRedirectGuard } from "../guards/org-redirect.guard";
 import { EventsComponent } from "../manage/events.component";
 
-import { AccessIntelligenceComponent } from "./access-intelligence/access-intelligence.component";
 import { ReportsHomeComponent } from "./reports-home.component";
 
 const routes: Routes = [
@@ -80,14 +79,6 @@ const routes: Routes = [
             component: WeakPasswordsReportComponent,
             data: {
               titleId: "weakPasswordsReport",
-            },
-            canActivate: [isPaidOrgGuard()],
-          },
-          {
-            path: "access-intelligence-prototype",
-            component: AccessIntelligenceComponent,
-            data: {
-              titleId: "accessIntelligencePrototype",
             },
             canActivate: [isPaidOrgGuard()],
           },
