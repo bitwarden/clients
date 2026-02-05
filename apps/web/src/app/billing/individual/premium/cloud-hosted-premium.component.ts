980a16ce35ef0326fc7cf5c79084b5fdf123df5a
Cy Okeke <cokeke@bitwarden.com>
2026-02-05 19:06:44 +0100

    revert the canViewSubscription change

--- apps/web/src/app/billing/individual/premium/cloud-hosted-premium.component.ts
+++ apps/web/src/app/billing/individual/premium/cloud-hosted-premium.component.ts
@@ -104,7 +104,7 @@ export class CloudHostedPremiumComponent {
     this.hasPremiumPersonally$ = this.accountService.activeAccount$.pipe(
       switchMap((account) =>
         account
-          ? this.billingAccountProfileStateService.canViewSubscription$(account.id)
+          ? this.billingAccountProfileStateService.hasPremiumPersonally$(account.id)
           : of(false),
       ),
     );
