b44364a6a587e282eccbc2a0c5f02481165f7e2a
Leslie Tilton <23057410+Banrion@users.noreply.github.com>
2026-02-05 03:17:49 -0600

    Update barrel file

--- bitwarden_license/bit-common/src/dirt/reports/risk-insights/models/index.ts
+++ bitwarden_license/bit-common/src/dirt/reports/risk-insights/models/index.ts
@@ -4,3 +4,4 @@ export * from "./report-data-service.types";
 export * from "./report-encryption.types";
 export * from "./report-models";
 export * from "./drawer-models.types";
+export * from "../constants";
