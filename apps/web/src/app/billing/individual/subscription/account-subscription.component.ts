207763fc36cc98dcbed2c6a3817b297160464d69
Cy Okeke <cokeke@bitwarden.com>
2026-02-05 09:09:48 +0100

    remove extra space

--- apps/web/src/app/billing/individual/subscription/account-subscription.component.ts
+++ apps/web/src/app/billing/individual/subscription/account-subscription.component.ts
@@ -86,7 +86,6 @@ export class AccountSubscriptionComponent {
       if (!hasPremiumPersonally) {
         return await redirectToPremiumPage();
       }
-
       return await this.accountBillingClient.getSubscription();
     },
   });
