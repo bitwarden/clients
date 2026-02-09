fadbd0d9a5e0d0d846b38809a0e5039e6ee7cf44
enmande <3836813+enmande@users.noreply.github.com>
2026-02-09 17:00:29 -0500

    refactor(change-email) [PM-30811]: EmailTokenRequest strict-ignore until legacy support can be unwound.

--- libs/common/src/auth/models/request/email-token.request.ts
+++ libs/common/src/auth/models/request/email-token.request.ts
@@ -1,3 +1,5 @@
+// FIXME: Update this file to be type safe and remove this and next line
+// @ts-strict-ignore
 import { MasterPasswordAuthenticationData } from "../../../key-management/master-password/types/master-password.types";
 
 import { SecretVerificationRequest } from "./secret-verification.request";
