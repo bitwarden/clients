4ea662f2970de1ee699c2265ce2b3d9f2c813ea6
enmande <3836813+enmande@users.noreply.github.com>
2026-02-09 16:11:27 -0500

    feat(change-email) [PM-30811]: Update EmailTokenRequest for new APIs.

--- libs/common/src/auth/models/request/email-token.request.ts
+++ libs/common/src/auth/models/request/email-token.request.ts
@@ -1,24 +1,22 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
 import { MasterPasswordAuthenticationData } from "../../../key-management/master-password/types/master-password.types";
 
 import { SecretVerificationRequest } from "./secret-verification.request";
 
 export class EmailTokenRequest extends SecretVerificationRequest {
   newEmail: string;
-  masterPasswordHash: string;
+
   /**
-   * Creates an EmailTokenRequest using new KM types.
+   * Creates an EmailTokenRequest using new KM data types.
    * This will eventually become the primary constructor once all callers are updated.
-   *
-   * https://bitwarden.atlassian.net/browse/PM-30811
+   * @see https://bitwarden.atlassian.net/browse/PM-30811
    */
-  static newConstructor(authenticationData: MasterPasswordAuthenticationData, newEmail: string) {
-    const emailTokenRequest = new EmailTokenRequest();
-
-    emailTokenRequest.newEmail = newEmail;
-    emailTokenRequest.masterPasswordHash = authenticationData.masterPasswordAuthenticationHash;
-
-    return emailTokenRequest;
+  static forNewEmail(
+    authenticationData: MasterPasswordAuthenticationData,
+    newEmail: string,
+  ): EmailTokenRequest {
+    const request = new EmailTokenRequest();
+    request.newEmail = newEmail;
+    request.authenticateWith(authenticationData);
+    return request;
   }
 }
