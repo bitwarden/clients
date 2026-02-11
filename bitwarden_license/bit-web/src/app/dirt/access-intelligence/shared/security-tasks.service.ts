021e37e32524deaa7270c7e3822933251b34951c
jaasen-livefront <jaasen@livefront.com>
2026-02-11 11:09:17 -0800

    revert change to security-tasks

--- bitwarden_license/bit-web/src/app/dirt/access-intelligence/shared/security-tasks.service.ts
+++ bitwarden_license/bit-web/src/app/dirt/access-intelligence/shared/security-tasks.service.ts
@@ -1,7 +1,8 @@
 import { BehaviorSubject, combineLatest, Observable } from "rxjs";
 import { map, shareReplay } from "rxjs/operators";
 
-import { RiskInsightsDataService , SecurityTasksApiService } from "@bitwarden/bit-common/dirt/reports/risk-insights";
+import { RiskInsightsDataService } from "@bitwarden/bit-common/dirt/reports/risk-insights";
+import { SecurityTasksApiService } from "@bitwarden/bit-common/dirt/reports/risk-insights";
 import { CipherId, OrganizationId } from "@bitwarden/common/types/guid";
 import { SecurityTask, SecurityTaskStatus, SecurityTaskType } from "@bitwarden/common/vault/tasks";
 
