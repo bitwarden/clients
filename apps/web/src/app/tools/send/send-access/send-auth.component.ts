d931bbfb35122a4ecf0d72891c485d4ca483fdc0
John Harrington <84741727+harr1424@users.noreply.github.com>
2026-02-11 09:23:14 -0700

    share i18n key for both invalid email and invalid otp submission

--- apps/web/src/app/tools/send/send-access/send-auth.component.ts
+++ apps/web/src/app/tools/send/send-access/send-auth.component.ts
@@ -52,6 +52,7 @@ export class SendAuthComponent implements OnInit {
   authType = AuthType;
 
   private expiredAuthAttempts = 0;
+  private otpSubmitted = false;
 
   readonly loading = signal<boolean>(false);
   readonly error = signal<boolean>(false);
@@ -162,11 +163,19 @@ export class SendAuthComponent implements OnInit {
         this.sendAuthType.set(AuthType.Email);
       } else if (emailAndOtpRequired(response.error)) {
         this.enterOtp.set(true);
+        if (this.otpSubmitted) {
+          this.toastService.showToast({
+            variant: "error",
+            title: this.i18nService.t("errorOccurred"),
+            message: this.i18nService.t("invalidEmailOrVerificationCode"),
+          });
+        }
+        this.otpSubmitted = true;
       } else if (otpInvalid(response.error)) {
         this.toastService.showToast({
           variant: "error",
           title: this.i18nService.t("errorOccurred"),
-          message: this.i18nService.t("invalidVerificationCode"),
+          message: this.i18nService.t("invalidEmailOrVerificationCode"),
         });
       } else if (passwordHashB64Required(response.error)) {
         this.sendAuthType.set(AuthType.Password);
