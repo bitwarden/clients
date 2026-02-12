e055f0dca678476d5189d9bf84473bd759267315
Dmitry Yakimenko <detunized@gmail.com>
2026-02-12 20:33:12 +0100

    Major refactor

--- libs/importer/src/importers/keeper/access/index.ts
+++ libs/importer/src/importers/keeper/access/index.ts
@@ -1,5 +1,4 @@
 export { Vault } from "./vault";
-export type { VaultRecord, VaultField, VaultFolder, VaultSharedFolder } from "./vault";
 export { Client } from "./client";
 export type { ClientOptions, LoginResult } from "./client";
 export { base64UrlEncode, base64UrlDecode } from "./crypto";
