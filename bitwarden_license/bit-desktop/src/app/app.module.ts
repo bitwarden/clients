52ace2de8cd2d9bb2c12f0e6b53c883b064fed88
Derek Nance <dnance@bitwarden.com>
2026-02-13 16:07:46 -0600

    shared OSS app module

--- bitwarden_license/bit-desktop/src/app/app.module.ts
+++ bitwarden_license/bit-desktop/src/app/app.module.ts
@@ -3,17 +3,16 @@ import "zone.js";
 // Register the locales for the application
 import "@bitwarden/desktop/platform/app/locales";
 
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule as OssRoutingModule } from "@bitwarden/desktop/app/app-routing.module";
-import { AppModule as OssModule } from "@bitwarden/desktop/app/app.module";
+import { OssModule } from "@bitwarden/desktop/app/oss.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [CommonModule, OssModule, OssRoutingModule, AppRoutingModule],
+  imports: [OssModule, OssRoutingModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
