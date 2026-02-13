c63f89e8b4c1296591ec957c7e1a457f9ec3ece2
Thomas Avery <tavery@bitwarden.com>
2026-02-13 16:48:00 -0600

    Fix native messaging state management

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1365,12 +1365,10 @@ export default class MainBackground {
       this.keyService,
       this.encryptService,
       this.cryptoFunctionService,
-      this.runtimeBackground,
       this.messagingService,
       this.appIdService,
       this.platformUtilsService,
       this.logService,
-      this.authService,
       this.biometricStateService,
       this.accountService,
     );
