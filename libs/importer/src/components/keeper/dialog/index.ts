8d72dd0582a1fac3dd8482a9a6704e879b73b1e7
Dmitry Yakimenko <detunized@gmail.com>
2026-02-17 00:01:31 +0100

    2FA selection

--- libs/importer/src/components/keeper/dialog/index.ts
+++ libs/importer/src/components/keeper/dialog/index.ts
@@ -1,4 +1,6 @@
 export { KeeperApprovalMethodSelectComponent } from "./keeper-approval-method-select.component";
 export { KeeperDeviceApprovalPromptComponent } from "./keeper-device-approval-prompt.component";
+export { KeeperDuoMethodSelectComponent } from "./keeper-duo-method-select.component";
 export { KeeperMultifactorPromptComponent } from "./keeper-multifactor-prompt.component";
 export { KeeperPasswordPromptComponent } from "./keeper-password-prompt.component";
+export { KeeperTwoFactorMethodSelectComponent } from "./keeper-two-factor-method-select.component";
