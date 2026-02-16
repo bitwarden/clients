2bc7b526ffbfab0993897f2fc8742fb8468f856c
Nick Krantz <nick@livefront.com>
2026-02-16 12:49:48 -0600

    add ping animation with filled info icon

--- apps/browser/src/vault/popup/components/vault/simplified-autofill-info/simplified-autofill-info.component.ts
+++ apps/browser/src/vault/popup/components/vault/simplified-autofill-info/simplified-autofill-info.component.ts
@@ -1,13 +1,16 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
-import { PopoverModule, IconModule, ButtonModule } from "@bitwarden/components";
+import { InfoFilledIcon } from "@bitwarden/assets/svg";
+import { PopoverModule, IconModule, ButtonModule, SvgModule } from "@bitwarden/components";
 
 @Component({
   selector: "app-simplified-autofill-info",
   templateUrl: "./simplified-autofill-info.component.html",
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [JslibModule, PopoverModule, IconModule, ButtonModule],
+  imports: [JslibModule, PopoverModule, IconModule, ButtonModule, SvgModule],
 })
-export class SimplifiedAutofillInfoComponent {}
+export class SimplifiedAutofillInfoComponent {
+  protected readonly InfoFilledIcon = InfoFilledIcon;
+}
