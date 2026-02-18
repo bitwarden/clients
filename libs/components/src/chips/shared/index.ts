1ac37349d394ea8b1a7c1b34aa4a480bed9d1a9b
Bryan Cunningham <bryan.cunningham@me.com>
2026-02-18 09:08:11 -0500

    remove unnecessary comment

--- libs/components/src/chips/shared/index.ts
+++ libs/components/src/chips/shared/index.ts
@@ -1,4 +1,3 @@
 export * from "./base-chip.directive";
 export * from "./chip-content.component";
 export * from "./chip-dismiss-button.component";
-// Note: shared-story-arg-types.ts not exported (internal to stories)
