04400cca52e6eb11638a95e8e7ec1a62c31a4f9c
Vicki League <vleague@bitwarden.com>
2026-02-27 15:25:31 -0500

    doc updates

--- libs/components/src/chips/chip-action/chip-action.component.ts
+++ libs/components/src/chips/chip-action/chip-action.component.ts
@@ -4,6 +4,11 @@ import { BitwardenIcon } from "../../shared/icon";
 import { BaseChipDirective } from "../shared/base-chip.directive";
 import { ChipContentComponent } from "../shared/chip-content.component";
 
+/**
+ * Action chips trigger a single contextual action related to the current view or task. They do not
+ * have selection state and do not manage input. Use action chips for quick, one-off actions like
+ * filtering, sorting, or navigating to related content.
+ */
 @Component({
   selector: "a[bit-chip-action], button[bit-chip-action]",
   imports: [ChipContentComponent],
