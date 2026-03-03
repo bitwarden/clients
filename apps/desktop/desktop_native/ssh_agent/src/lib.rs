b495e836c2f059413a0a670e25cb54d8adadf03c
neuronull <9162534+neuronull@users.noreply.github.com>
2026-03-03 11:07:44 -0700

    add doc comment for crate

--- apps/desktop/desktop_native/ssh_agent/src/lib.rs
+++ apps/desktop/desktop_native/ssh_agent/src/lib.rs
@@ -1,8 +1,25 @@
-//! Bitwarden SSH Agent implementation
+//! Bitwarden SSH Agent implementation.
 //!
-//! TODO: add details
+//! Adheres to the protocol defined in the IETF spec:
+//! <https://datatracker.ietf.org/doc/draft-ietf-sshm-ssh-agent/>
 //!
-//! <https://www.ietf.org/archive/id/draft-miller-ssh-agent-11.html#RFC4253>
+//! # Architecture:
+//! - `crypto` provides the raw cryptographic primitive objects and operations for supported SSH
+//!   keys.
+//! - `storage` provides data structures the bridge the primitive key types to Bitwarden's business
+//!   logic, namely the concept of vault items. As well as  the implementations of the objects to
+//!   manage and store the keys used in the agent's runtime.
+//! - `server` provides the low-level implementation of the Server protocol. It uses an
+//!   implementation of an `AuthPolicy` to get authorization for the various agent operations that
+//!   are received by connections.
+//! - `authorization` provides Bitwarden's business logic for the requested authorization from the
+//!   server, for the various agent operations.
+//! - `approval` provides an interface for the agent itself to request approval from an external
+//!   entity (currently, Electron via napi) to approve requests.
+//! - `agent` contains the store of keys, and the server, and uses the authorization and approval
+//!   impelementations to orchestrate operations between the server and the external (Electron)
+//!   entity.
+//! - ``
 
 #![allow(dead_code)] // TODO remove when all code is used in follow-up PR
 
