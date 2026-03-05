4d3ace1a1026d59960b4e33f68aee345b927e0b0
Hinton <hinton@users.noreply.github.com>
2026-03-05 12:01:11 +0100

    PoC to remove SASS

--- apps/browser/src/popup/scss/index.ts
+++ apps/browser/src/popup/scss/index.ts
@@ -1,4 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-require("./popup.scss");
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 require("./tailwind.css");
