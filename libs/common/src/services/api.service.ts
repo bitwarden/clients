c2620c34c2fcdc5ccf7ae6fa5090bd1a0eb197c4
enmande <3836813+enmande@users.noreply.github.com>
2026-03-05 12:14:22 -0500

    feat(utils) [PM-29796]: Remove invalidUrlParams.

--- libs/common/src/services/api.service.ts
+++ libs/common/src/services/api.service.ts
@@ -1649,15 +1649,6 @@ export class ApiService implements ApiServiceAbstraction {
 
   private buildSafeApiRequestUrl(apiUrl: string, path: string): string {
     const pathParts = path.split("?");
-
-    // Check for path traversal patterns from any URL.
-    const fullUrlPath = apiUrl + pathParts[0] + (pathParts.length > 1 ? `?${pathParts[1]}` : "");
-
-    const isInvalidUrl = Utils.invalidUrlPatterns(fullUrlPath);
-    if (isInvalidUrl) {
-      throw new Error("The request URL contains dangerous patterns.");
-    }
-
     const requestUrl =
       apiUrl + Utils.normalizePath(pathParts[0]) + (pathParts.length > 1 ? `?${pathParts[1]}` : "");
 
