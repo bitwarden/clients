1dd8e2c590c74303a54b3965bbca4209cba5d77d
Bryan Cunningham <bryan.cunningham@me.com>
2026-03-06 16:17:28 -0500

    create radio card component

--- libs/components/src/radio-button/index.ts
+++ libs/components/src/radio-button/index.ts
@@ -1,3 +1,5 @@
 export * from "./radio-button.module";
+export * from "./radio-button-base.directive";
 export * from "./radio-button.component";
+export * from "./radio-button-card.component";
 export * from "./radio-group.component";
--- libs/components/src/radio-button/radio-button.module.ts
+++ libs/components/src/radio-button/radio-button.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from "@angular/core";
 
 import { FormControlModule } from "../form-control";
+import { FormControlCardComponent } from "../form-control/form-control-card.component";
 
+import { RadioButtonCardComponent } from "./radio-button-card.component";
 import { RadioButtonComponent } from "./radio-button.component";
 import { RadioGroupComponent } from "./radio-group.component";
 import { RadioInputComponent } from "./radio-input.component";
 
 @NgModule({
-  imports: [FormControlModule, RadioInputComponent, RadioButtonComponent, RadioGroupComponent],
-  exports: [FormControlModule, RadioInputComponent, RadioButtonComponent, RadioGroupComponent],
+  imports: [
+    FormControlModule,
+    FormControlCardComponent,
+    RadioInputComponent,
+    RadioButtonComponent,
+    RadioButtonCardComponent,
+    RadioGroupComponent,
+  ],
+  exports: [
+    FormControlModule,
+    FormControlCardComponent,
+    RadioInputComponent,
+    RadioButtonComponent,
+    RadioButtonCardComponent,
+    RadioGroupComponent,
+  ],
 })
 export class RadioButtonModule {}
