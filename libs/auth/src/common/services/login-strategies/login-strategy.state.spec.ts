11c02db8c7dcc601f06e8e1fc01d0de0df0a981f
Jared Snider <jsnider@bitwarden.com>
2026-03-11 22:07:06 -0400

    PM-33353  - login-strategy.state.spec.ts - Remove incorrect todo and fix ts strict issues

--- libs/auth/src/common/services/login-strategies/login-strategy.state.spec.ts
+++ libs/auth/src/common/services/login-strategies/login-strategy.state.spec.ts
@@ -23,7 +23,6 @@ import { AuthRequestLoginCredentials, WebAuthnLoginCredentials } from "../../mod
 
 import { CACHE_KEY } from "./login-strategy.state";
 
-// TODO: update tests
 describe("LOGIN_STRATEGY_CACHE_KEY", () => {
   const sut = CACHE_KEY;
 
@@ -56,8 +55,9 @@ describe("LOGIN_STRATEGY_CACHE_KEY", () => {
 
     const result = sut.deserializer(JSON.parse(JSON.stringify(actual)));
 
-    expect(result.password).toBeInstanceOf(PasswordLoginStrategyData);
-    verifyPropertyPrototypes(result, actual);
+    expect(result).not.toBeNull();
+    expect(result!.password).toBeInstanceOf(PasswordLoginStrategyData);
+    verifyPropertyPrototypes(result!, actual);
   });
 
   it("should correctly deserialize SsoLoginStrategyData", () => {
@@ -72,23 +72,34 @@ describe("LOGIN_STRATEGY_CACHE_KEY", () => {
 
     const result = sut.deserializer(JSON.parse(JSON.stringify(actual)));
 
-    expect(result.sso).toBeInstanceOf(SsoLoginStrategyData);
-    verifyPropertyPrototypes(result, actual);
+    expect(result).not.toBeNull();
+    expect(result!.sso).toBeInstanceOf(SsoLoginStrategyData);
+    verifyPropertyPrototypes(result!, actual);
   });
 
   it("should correctly deserialize UserApiLoginStrategyData", () => {
     const actual = { userApiKey: new UserApiLoginStrategyData() };
-    actual.userApiKey.tokenRequest = new UserApiTokenRequest("CLIENT_ID", "CLIENT_SECRET", null);
+    actual.userApiKey.tokenRequest = new UserApiTokenRequest(
+      "CLIENT_ID",
+      "CLIENT_SECRET",
+      twoFactorRequest,
+    );
 
     const result = sut.deserializer(JSON.parse(JSON.stringify(actual)));
 
-    expect(result.userApiKey).toBeInstanceOf(UserApiLoginStrategyData);
-    verifyPropertyPrototypes(result, actual);
+    expect(result).not.toBeNull();
+    expect(result!.userApiKey).toBeInstanceOf(UserApiLoginStrategyData);
+    verifyPropertyPrototypes(result!, actual);
   });
 
   it("should correctly deserialize AuthRequestLoginStrategyData", () => {
     const actual = { authRequest: new AuthRequestLoginStrategyData() };
-    actual.authRequest.tokenRequest = new PasswordTokenRequest("EMAIL", "ACCESS_CODE", null, null);
+    actual.authRequest.tokenRequest = new PasswordTokenRequest(
+      "EMAIL",
+      "ACCESS_CODE",
+      twoFactorRequest,
+      deviceRequest,
+    );
     actual.authRequest.authRequestCredentials = new AuthRequestLoginCredentials(
       "EMAIL",
       "ACCESS_CODE",
@@ -98,8 +109,9 @@ describe("LOGIN_STRATEGY_CACHE_KEY", () => {
 
     const result = sut.deserializer(JSON.parse(JSON.stringify(actual)));
 
-    expect(result.authRequest).toBeInstanceOf(AuthRequestLoginStrategyData);
-    verifyPropertyPrototypes(result, actual);
+    expect(result).not.toBeNull();
+    expect(result!.authRequest).toBeInstanceOf(AuthRequestLoginStrategyData);
+    verifyPropertyPrototypes(result!, actual);
   });
 
   it("should correctly deserialize WebAuthnLoginStrategyData", () => {
@@ -121,8 +133,9 @@ describe("LOGIN_STRATEGY_CACHE_KEY", () => {
 
     const result = sut.deserializer(JSON.parse(JSON.stringify(actual)));
 
-    expect(result.webAuthn).toBeInstanceOf(WebAuthnLoginStrategyData);
-    verifyPropertyPrototypes(result, actual);
+    expect(result).not.toBeNull();
+    expect(result!.webAuthn).toBeInstanceOf(WebAuthnLoginStrategyData);
+    verifyPropertyPrototypes(result!, actual);
   });
 });
 
