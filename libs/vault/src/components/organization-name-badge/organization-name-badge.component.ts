940575f7cf92e4a70097c75398cfeb155b313f74
Bryan Cunningham <bryan.cunningham@me.com>
2026-03-12 10:46:28 -0400

    remove unused custom color code

--- libs/vault/src/components/organization-name-badge/organization-name-badge.component.ts
+++ libs/vault/src/components/organization-name-badge/organization-name-badge.component.ts
@@ -2,14 +2,12 @@
 // @ts-strict-ignore
 import { Component, Input, OnChanges } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { firstValueFrom } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { Unassigned } from "@bitwarden/common/admin-console/models/collections";
 import { AvatarService } from "@bitwarden/common/auth/abstractions/avatar.service";
 import { TokenService } from "@bitwarden/common/auth/abstractions/token.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
-import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { ChipActionComponent } from "@bitwarden/components";
 import { OrganizationId } from "@bitwarden/sdk-internal";
 
@@ -45,27 +43,12 @@ export class OrganizationNameBadgeComponent implements OnChanges {
 
   // ngOnChanges is required since this component might be reused as part of
   // cdk virtual scrolling
-  async ngOnChanges() {
-    this.isMe = this.organizationName == null || this.organizationName === "";
-
-    if (this.isMe) {
+  ngOnChanges() {
+    if (this.organizationName == null || this.organizationName === "") {
       this.name = this.i18nService.t("me");
-      this.color = await firstValueFrom(this.avatarService.avatarColor$);
-      if (this.color == null) {
-        const userId = await this.tokenService.getUserId();
-        if (userId != null) {
-          this.color = Utils.stringToColor(userId);
-        } else {
-          const userName =
-            (await this.tokenService.getName()) ?? (await this.tokenService.getEmail());
-          this.color = Utils.stringToColor(userName.toUpperCase());
-        }
-      }
     } else {
       this.name = this.organizationName;
-      this.color = Utils.stringToColor(this.organizationName.toUpperCase());
     }
-    this.textColor = Utils.pickTextColorBasedOnBgColor(this.color, 135, true) + "!important";
   }
 
   get organizationIdLink() {
