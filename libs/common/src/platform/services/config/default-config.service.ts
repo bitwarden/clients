3e1c6e8f034e23c2098fff91c14d8a61b60920e2
Daniel James Smith <djsmith85@users.noreply.github.com>
2026-03-19 08:59:01 +0100

    Fix vaultUrl being set twice

--- libs/common/src/platform/services/config/default-config.service.ts
+++ libs/common/src/platform/services/config/default-config.service.ts
@@ -259,7 +259,6 @@ export class DefaultConfigService implements ConfigService {
         ? {
             bootstrap: {
               type: "ssoCookieVendor",
-              vaultUrl: response.environment.vault,
               idpLoginUrl: bootstrap.idpLoginUrl,
               cookieName: bootstrap.cookieName,
               cookieDomain: bootstrap.cookieDomain,
