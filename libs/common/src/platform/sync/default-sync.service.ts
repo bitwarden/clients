819ad1ff84d5ea54f3b3b4a59ce1b41fdb02dc21
Shane <smelton@bitwarden.com>
2026-03-26 15:18:27 -0700

    Integrate preference sync with sync infrastructure
    - Add userPreferences field to SyncResponse
    - Call PreferenceSyncService.pull() in DefaultSyncService.fullSync()
    - Add SyncUserPreferences notification type (27)
    - Handle SyncUserPreferences in server notifications service

--- libs/common/src/platform/sync/default-sync.service.ts
+++ libs/common/src/platform/sync/default-sync.service.ts
@@ -65,6 +65,7 @@ import { MessageSender } from "../messaging";
 import { StateProvider } from "../state";
 
 import { CoreSyncService } from "./core-sync.service";
+import { PreferenceSyncService } from "./preferences/preference-sync.service";
 import { SyncResponse } from "./sync.response";
 import { SyncOptions } from "./sync.service";
 
@@ -108,6 +109,7 @@ export class DefaultSyncService extends CoreSyncService {
     private securityStateService: SecurityStateService,
     private kdfConfigService: KdfConfigService,
     private accountCryptographicStateService: AccountCryptographicStateService,
+    private preferenceSyncService?: PreferenceSyncService,
   ) {
     super(
       tokenService,
@@ -193,6 +195,7 @@ export class DefaultSyncService extends CoreSyncService {
       await this.syncSends(response.sends, response.profile.id);
       await this.syncSettings(response.domains, response.profile.id);
       await this.syncPolicies(response.policies, response.profile.id);
+      await this.preferenceSyncService?.pull(response.userPreferences, response.profile.id);
 
       await this.setLastSync(now, userId);
       return this.syncCompleted(true, userId);
