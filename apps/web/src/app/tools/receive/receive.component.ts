e733a607e3b750f4c32def43f8eb86de81547ae2
adudek-bw <adudek@bitwarden.com>
2026-03-30 12:25:36 -0400

    Create View Receive component

--- apps/web/src/app/tools/receive/receive.component.ts
+++ apps/web/src/app/tools/receive/receive.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from "@angular/forms";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { NoResults, NoSendsIcon } from "@bitwarden/assets/svg";
+import { AuthType } from "@bitwarden/common/tools/send/types/auth-type";
 import {
   ButtonModule,
   DialogService,
@@ -19,6 +20,19 @@ import { HeaderModule } from "../../layouts/header/header.module";
 import { ReceiveAddEditComponent } from "./receive-add-edit.component";
 import { ReceiveTableComponent } from "./receive-table.component";
 import { ReceiveListState, ReceiveView } from "./receive-view";
+import { ReceiveViewComponent } from "./receive-view.component";
+
+const DUMMY_RECEIVE: ReceiveView = {
+  id: "dummy-1",
+  name: "My first receive",
+  deletionDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  disabled: false,
+  authType: AuthType.None,
+  maxAccessCountReached: false,
+  expired: false,
+  pendingDelete: false,
+  password: null,
+};
 
 // FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
 // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
@@ -39,10 +53,19 @@ import { ReceiveListState, ReceiveView } from "./receive-view";
   ],
 })
 export class ReceiveComponent {
-  constructor(private readonly dialogService: DialogService) {}
+  constructor(private readonly dialogService: DialogService) {
+    this.dataSource.data = [DUMMY_RECEIVE];
+  }
 
   openNewReceiveDrawer(): void {
     this.dialogService.openDrawer(ReceiveAddEditComponent, { closeOnNavigation: true });
+  }
+
+  openViewReceiveDrawer(receive: ReceiveView): void {
+    this.dialogService.openDrawer(ReceiveViewComponent, {
+      data: receive,
+      closeOnNavigation: true,
+    });
   }
 
   protected readonly noItemIcon = NoSendsIcon;
@@ -50,7 +73,7 @@ export class ReceiveComponent {
   protected readonly ReceiveListState = ReceiveListState;
 
   protected readonly loading = signal(false);
-  protected readonly listState = signal<ReceiveListState | null>(ReceiveListState.Empty);
+  protected readonly listState = signal<ReceiveListState | null>(null);
   protected readonly currentSearchText = signal("");
   protected readonly dataSource = new TableDataSource<ReceiveView>();
 
