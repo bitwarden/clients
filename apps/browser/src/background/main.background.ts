3e26e34026749dd711ad945dfffffe23b4290a9c
Shane <smelton@bitwarden.com>
2026-03-31 14:21:42 -0700

    Wire up browser background for preference sync

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -191,6 +191,10 @@ import { WindowStorageService } from "@bitwarden/common/platform/storage/window-
 import { SyncService } from "@bitwarden/common/platform/sync";
 // eslint-disable-next-line no-restricted-imports -- Needed for service creation
 import { DefaultSyncService } from "@bitwarden/common/platform/sync/internal";
+import {
+  PreferenceSyncApiService,
+  PreferenceSyncService,
+} from "@bitwarden/common/platform/sync/preferences";
 import { SystemNotificationsService } from "@bitwarden/common/platform/system-notifications/";
 import { SystemNotificationEvent } from "@bitwarden/common/platform/system-notifications/system-notifications.service";
 import { UnsupportedSystemNotificationsService } from "@bitwarden/common/platform/system-notifications/unsupported-system-notifications.service";
@@ -1070,6 +1074,16 @@ export default class MainBackground {
 
     this.providerService = new ProviderService(this.stateProvider);
 
+    const preferenceSyncApiService = new PreferenceSyncApiService(this.apiService);
+    const preferenceSyncService = new PreferenceSyncService(
+      this.stateProvider,
+      this.encryptService,
+      (userId) => firstValueFrom(this.keyService.userKey$(userId)),
+      preferenceSyncApiService,
+      this.logService,
+      this.platformUtilsService,
+    );
+
     this.syncService = new DefaultSyncService(
       this.masterPasswordService,
       this.accountService,
@@ -1098,6 +1112,7 @@ export default class MainBackground {
       this.securityStateService,
       this.kdfConfigService,
       this.accountCryptographicStateService,
+      preferenceSyncService,
     );
 
     this.syncServiceListener = new SyncServiceListener(
