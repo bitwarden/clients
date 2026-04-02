6039974659b10981541c1230204e2bced2d7866f
Jonathan Prusik <jprusik@classynemesis.com>
2026-04-02 08:52:09 -0400

    add state handling for environment switching

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1008,6 +1008,7 @@ export default class MainBackground {
       this.apiService,
       this.domainSettingsService,
       this.configService,
+      this.environmentService,
       this.taskSchedulerService,
       this.globalStateProvider,
       this.logService,
