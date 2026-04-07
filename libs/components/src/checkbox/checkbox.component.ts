998604d8f5a20fbbd6a2f3c5e9f3910722ab6374
Bryan Cunningham <bryan.cunningham@me.com>
2026-04-07 11:16:45 -0400

    cleaning up eslint ignore

--- libs/components/src/checkbox/checkbox.component.ts
+++ libs/components/src/checkbox/checkbox.component.ts
@@ -1,32 +1,31 @@
 import {
   booleanAttribute,
+  ChangeDetectionStrategy,
   Component,
   ElementRef,
-  HostBinding,
   inject,
   input,
-  Optional,
-  Self,
 } from "@angular/core";
 import { NgControl, Validators } from "@angular/forms";
 
 import { BitFormControlAbstraction } from "../form-control";
 
-// FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
-// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
   selector: "input[type=checkbox][bitCheckbox]",
   template: "",
   providers: [{ provide: BitFormControlAbstraction, useExisting: CheckboxComponent }],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     "[disabled]": "disabled",
+    "[class]": "inputClasses",
+    "[style.--check-mask]": "checkMask",
+    "[style.--indeterminate-mask-image]": "indeterminateImage",
   },
 })
 export class CheckboxComponent implements BitFormControlAbstraction {
   readonly inputEl = inject<ElementRef<HTMLInputElement>>(ElementRef);
 
-  @HostBinding("class")
-  protected inputClasses = [
+  protected readonly inputClasses = [
     "tw-appearance-none",
     "tw-outline-none",
     "tw-box-border",
@@ -114,15 +113,11 @@ export class CheckboxComponent implements BitFormControlAbstraction {
     "indeterminate:disabled:after:tw-bg-fg-inactive",
   ];
 
-  constructor(@Optional() @Self() private ngControl?: NgControl) {}
+  private readonly ngControl = inject(NgControl, { optional: true, self: true });
 
-  @HostBinding("style.--check-mask")
-  protected checkMask =
-    `url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M9.54972 15.15L18.0247 6.675C18.2247 6.475 18.4581 6.375 18.7247 6.375C18.9914 6.375 19.2247 6.475 19.4247 6.675C19.6247 6.875 19.7247 7.1125 19.7247 7.3875C19.7247 7.6625 19.6247 7.9 19.4247 8.1L10.2497 17.3C10.0497 17.5 9.81639 17.6 9.54972 17.6C9.28305 17.6 9.04972 17.5 8.84972 17.3L4.54972 13C4.34972 12.8 4.25389 12.5625 4.26222 12.2875C4.27055 12.0125 4.37472 11.775 4.57472 11.575C4.77472 11.375 5.01222 11.275 5.28722 11.275C5.56222 11.275 5.79972 11.375 5.99972 11.575L9.54972 15.15Z' fill='currentColor'/%3E%3C/svg%3E")`;
+  protected readonly checkMask = `url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M9.54972 15.15L18.0247 6.675C18.2247 6.475 18.4581 6.375 18.7247 6.375C18.9914 6.375 19.2247 6.475 19.4247 6.675C19.6247 6.875 19.7247 7.1125 19.7247 7.3875C19.7247 7.6625 19.6247 7.9 19.4247 8.1L10.2497 17.3C10.0497 17.5 9.81639 17.6 9.54972 17.6C9.28305 17.6 9.04972 17.5 8.84972 17.3L4.54972 13C4.34972 12.8 4.25389 12.5625 4.26222 12.2875C4.27055 12.0125 4.37472 11.775 4.57472 11.575C4.77472 11.375 5.01222 11.275 5.28722 11.275C5.56222 11.275 5.79972 11.375 5.99972 11.575L9.54972 15.15Z' fill='currentColor'/%3E%3C/svg%3E")`;
 
-  @HostBinding("style.--indeterminate-mask-image")
-  protected indeterminateImage =
-    `url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M6 13C5.71667 13 5.47917 12.9042 5.2875 12.7125C5.09583 12.5208 5 12.2833 5 12C5 11.7167 5.09583 11.4792 5.2875 11.2875C5.47917 11.0958 5.71667 11 6 11H18C18.2833 11 18.5208 11.0958 18.7125 11.2875C18.9042 11.4792 19 11.7167 19 12C19 12.2833 18.9042 12.5208 18.7125 12.7125C18.5208 12.9042 18.2833 13 18 13H6Z' fill='currentColor'/%3E%3C/svg%3E")`;
+  protected readonly indeterminateImage = `url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M6 13C5.71667 13 5.47917 12.9042 5.2875 12.7125C5.09583 12.5208 5 12.2833 5 12C5 11.7167 5.09583 11.4792 5.2875 11.2875C5.47917 11.0958 5.71667 11 6 11H18C18.2833 11 18.5208 11.0958 18.7125 11.2875C18.9042 11.4792 19 11.7167 19 12C19 12.2833 18.9042 12.5208 18.7125 12.7125C18.5208 12.9042 18.2833 13 18 13H6Z' fill='currentColor'/%3E%3C/svg%3E")`;
 
   readonly disabledInput = input(false, { transform: booleanAttribute, alias: "disabled" });
 
