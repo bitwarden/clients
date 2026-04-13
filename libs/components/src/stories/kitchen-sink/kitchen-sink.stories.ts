3ea0b29c02b1bb031ae73e6db2306de77e41a411
Leslie Xiong <lxiong@livefront.com>
2026-04-13 12:04:58 -0400

    PR followup: added `icon(); as BitwardenIcon`, added translation to kitchen sink stories

--- libs/components/src/stories/kitchen-sink/kitchen-sink.stories.ts
+++ libs/components/src/stories/kitchen-sink/kitchen-sink.stories.ts
@@ -78,6 +78,7 @@ export default {
               no: "No",
               loading: "Loading",
               resizeSideNavigation: "Resize side navigation",
+              breadcrumbs: "Breadcrumbs",
             });
           },
         },
