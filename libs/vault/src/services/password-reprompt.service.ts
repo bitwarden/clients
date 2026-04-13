c8e357e4468127cc86dc28eee8257efb07b130b3
Nick Krantz <nick@livefront.com>
2026-04-13 09:37:59 -0500

    add PIN and Account Number to protected fields for reprompt service

--- libs/vault/src/services/password-reprompt.service.ts
+++ libs/vault/src/services/password-reprompt.service.ts
@@ -23,7 +23,7 @@ export class PasswordRepromptService {
   enabled$ = Utils.asyncToObservable(() => this.userVerificationService.hasMasterPassword());
 
   protectedFields() {
-    return ["TOTP", "Password", "H_Field", "Card Number", "Security Code"];
+    return ["TOTP", "Password", "H_Field", "Card Number", "Security Code", "PIN", "Account Number"];
   }
 
   async passwordRepromptCheck(cipher: CipherViewLike) {
