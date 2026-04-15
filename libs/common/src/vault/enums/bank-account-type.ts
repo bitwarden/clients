56e778a1c92d6ac19aae151b4d28c0bdc945d5e6
Nick Krantz <nick@livefront.com>
2026-04-15 13:48:29 -0500

    address the fixme

--- libs/common/src/vault/enums/bank-account-type.ts
+++ libs/common/src/vault/enums/bank-account-type.ts
@@ -12,5 +12,4 @@ type _BankAccountType = typeof _BankAccountType;
 
 export type BankAccountType = _BankAccountType[keyof _BankAccountType];
 
-// FIXME: Update typing of `BankAccountType` to be `Record<keyof _BankAccountType, BankAccountType>` which is ADR-0025 compliant when the TypeScript version is at least 5.8.
-export const BankAccountType: typeof _BankAccountType = _BankAccountType;
+export const BankAccountType: Record<keyof _BankAccountType, BankAccountType> = _BankAccountType;
