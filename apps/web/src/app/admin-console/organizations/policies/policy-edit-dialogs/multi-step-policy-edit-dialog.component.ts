035caa48e14e0a5cc15d36a252c29caf737e4bb6
JaredScar <thewolfbadger@gmail.com>
2026-04-16 16:46:57 -0400

    Refactor policy edit dialog to dynamically set title and subtitle based on current step content

--- apps/web/src/app/admin-console/organizations/policies/policy-edit-dialogs/multi-step-policy-edit-dialog.component.ts
+++ apps/web/src/app/admin-console/organizations/policies/policy-edit-dialogs/multi-step-policy-edit-dialog.component.ts
@@ -56,10 +56,6 @@ export class MultiStepPolicyEditDialogComponent
 
   private readonly currentStepConfig = computed(() => this.policySteps()[this.currentStep()]);
 
-  protected readonly titleTemplate = computed(() =>
-    this.policySteps()[this.currentStep()]?.titleContent?.(),
-  );
-
   protected readonly saveDisabled = toSignal(
     toObservable(this.currentStepConfig).pipe(
       switchMap((stepConfig) => {
