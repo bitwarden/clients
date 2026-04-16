04585e1a8d0786ec7667d1e4e70f1f4d5109c0b7
Leslie Xiong <lxiong@livefront.com>
2026-04-16 16:33:16 -0400

    added "more" translation to kitchen-sink stories

--- libs/components/src/stories/kitchen-sink/kitchen-sink.stories.ts
+++ libs/components/src/stories/kitchen-sink/kitchen-sink.stories.ts
@@ -78,6 +78,7 @@ export default {
               no: "No",
               loading: "Loading",
               resizeSideNavigation: "Resize side navigation",
+              more: "More",
             });
           },
         },
