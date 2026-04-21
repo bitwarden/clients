25590a7b8b625e13db5ba39850ea278cebb79584
Brandon <btreston@bitwarden.com>
2026-04-21 16:56:04 -0400

    add missing properties

--- libs/organization-invite-link/src/models/requests/organization-invite-link-create.request.ts
+++ libs/organization-invite-link/src/models/requests/organization-invite-link-create.request.ts
@@ -3,8 +3,13 @@ import { EncString } from "@bitwarden/common/key-management/crypto/models/enc-st
 export class OrganizationInviteLinkCreateRequest {
   allowedDomains: string[];
   encryptedInviteKey: string;
+  encryptedOrgKey: string | null;
 
-  constructor(c: { allowedDomains: string[]; encryptedInviteKey: EncString }) {
+  constructor(c: {
+    allowedDomains: string[];
+    encryptedInviteKey: EncString;
+    encryptedOrgKey?: EncString | null;
+  }) {
     if (!c.allowedDomains || c.allowedDomains.length === 0) {
       throw new Error("At least one allowed domain is required.");
     }
@@ -14,5 +19,6 @@ export class OrganizationInviteLinkCreateRequest {
 
     this.allowedDomains = c.allowedDomains;
     this.encryptedInviteKey = c.encryptedInviteKey.encryptedString;
+    this.encryptedOrgKey = c.encryptedOrgKey?.encryptedString ?? null;
   }
 }
--- libs/organization-invite-link/src/models/responses/organization-invite-link.response.ts
+++ libs/organization-invite-link/src/models/responses/organization-invite-link.response.ts
@@ -1,7 +1,9 @@
 import { BaseResponse } from "@bitwarden/common/models/response/base.response";
 
 export class OrganizationInviteLinkResponseModel extends BaseResponse {
+  id: string;
   code: string;
+  organizationId: string;
   allowedDomains: string[];
   encryptedInviteKey: string;
   encryptedOrgKey: string | null;
@@ -9,7 +11,9 @@ export class OrganizationInviteLinkResponseModel extends BaseResponse {
 
   constructor(response: any) {
     super(response);
+    this.id = this.getResponseProperty("Id");
     this.code = this.getResponseProperty("Code");
+    this.organizationId = this.getResponseProperty("OrganizationId");
     this.allowedDomains = this.getResponseProperty("AllowedDomains");
     this.encryptedInviteKey = this.getResponseProperty("EncryptedInviteKey");
     this.encryptedOrgKey = this.getResponseProperty("EncryptedOrgKey");
