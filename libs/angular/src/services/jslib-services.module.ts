fc87daa6ff7d8172f208359f1f41d3ffa7092e47
Brandon <btreston@bitwarden.com>
2026-04-24 10:31:41 -0400

    implement invite link state and service

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -379,7 +379,9 @@ import {
 } from "@bitwarden/key-management";
 import {
   DefaultOrganizationInviteLinkApiService,
+  DefaultOrganizationInviteLinkService,
   OrganizationInviteLinkApiService,
+  OrganizationInviteLinkService,
 } from "@bitwarden/organization-invite-link";
 import { DefaultTaskSchedulerService, TaskSchedulerService } from "@bitwarden/scheduling";
 import {
@@ -1283,6 +1285,17 @@ const safeProviders: SafeProvider[] = [
     provide: OrganizationInviteLinkApiService,
     useClass: DefaultOrganizationInviteLinkApiService,
     deps: [ApiServiceAbstraction],
+  }),
+  safeProvider({
+    provide: OrganizationInviteLinkService,
+    useClass: DefaultOrganizationInviteLinkService,
+    deps: [
+      KeyService,
+      EncryptService,
+      KeyGenerationService,
+      OrganizationInviteLinkApiService,
+      StateProvider,
+    ],
   }),
   safeProvider({
     provide: PasswordResetEnrollmentServiceAbstraction,
