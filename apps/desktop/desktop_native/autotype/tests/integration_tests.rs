793d0bbd744b7ff36823429d312330deb8c4df09
neuronull <9162534+neuronull@users.noreply.github.com>
2026-04-28 15:09:00 -0600

    import dep

--- apps/desktop/desktop_native/autotype/tests/integration_tests.rs
+++ apps/desktop/desktop_native/autotype/tests/integration_tests.rs
@@ -128,10 +128,7 @@ extern "system" fn show_window_proc(
 impl TestWindow {
     fn set_foreground(&self) -> Result<()> {
         unsafe {
-            use windows::Win32::{
-                System::Threading::GetCurrentThreadId,
-                UI::Input::KeyboardAndMouse::AttachThreadInput,
-            };
+            use windows::Win32::System::Threading::{AttachThreadInput, GetCurrentThreadId};
 
             // AttachThreadInput allows us to synchronize our input queue with the foreground
             // thread's so Windows grants permission to call SetForegroundWindow. Without
