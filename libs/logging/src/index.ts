27bf2b7dd75cb1fc71a6e6a9f0fc6b90b2c671a4
Will Martin <contact@willmartian.com>
2026-04-30 09:11:36 -0400

    Fix type-only imports/exports caught by Rolldown in Storybook v10/Vite v8

--- libs/logging/src/index.ts
+++ libs/logging/src/index.ts
@@ -2,4 +2,4 @@ export { LogService } from "./log.service";
 export { LogLevel } from "./log-level";
 export { ConsoleLogService } from "./console-log.service";
 export { FlightRecorder } from "./flight-recorder";
-export { FlightRecorderEvent } from "@bitwarden/sdk-internal";
+export type { FlightRecorderEvent } from "@bitwarden/sdk-internal";
