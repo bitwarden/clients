0e18bd1e929125695a449975b873f919b216e182
Will Martin <contact@willmartian.com>
2026-04-30 08:56:20 -0400

    Revert accidental change to access-selector spec

--- apps/web/src/app/admin-console/organizations/shared/components/access-selector/access-selector.component.spec.ts
+++ apps/web/src/app/admin-console/organizations/shared/components/access-selector/access-selector.component.spec.ts
@@ -45,7 +45,7 @@ describe("AccessSelectorComponent", () => {
 
   beforeEach(() => {
     // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-     
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     TestBed.configureTestingModule({
       imports: [PreloadedEnglishI18nModule, TestableAccessSelectorComponent],
       providers: [],
