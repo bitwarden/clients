9182aac2e5fd423216becf7a5937cbe7a46a77a2
Jackson Engstrom <jengstrom@bitwarden.com>
2026-05-04 13:42:20 -0700

    Adds vault organization user notification banner component and service

---
+++ libs/vault/src/components/vault-organization-user-notifications/vault-organization-user-notifications.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, computed, inject } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+
+import { BannerComponent, ButtonComponent, IconComponent } from "@bitwarden/components";
+
+import { VaultOrganizationUserNotificationsService } from "../../services/vault-organization-user-notifications.service";
+
+@Component({
+  selector: "vault-organization-user-notifications",
+  templateUrl: "./vault-organization-user-notifications.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [BannerComponent, ButtonComponent, IconComponent],
+  providers: [VaultOrganizationUserNotificationsService],
+})
+export class VaultOrganizationUserNotificationsComponent {
+  private readonly vaultOrganizationUserNotificationsService = inject(
+    VaultOrganizationUserNotificationsService,
+  );
+
+  protected readonly notificationData = toSignal(
+    this.vaultOrganizationUserNotificationsService.notificationData$,
+  );
+
+  protected readonly showBanner = toSignal(
+    this.vaultOrganizationUserNotificationsService.showNotificationBanner$,
+    { initialValue: false },
+  );
+
+  protected readonly showClose = computed(() => !this.notificationData()?.buttonText);
+
+  async close() {
+    await this.vaultOrganizationUserNotificationsService.saveDismissalToState();
+  }
+}
