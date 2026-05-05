9ca2cb753cc76c220c9d6218b87e7dc3e3b6906f
John Harrington <84741727+harr1424@users.noreply.github.com>
2026-05-04 19:04:35 -0700

    update web/broswer/desktop to match design specs

--- libs/logging-angular/src/troubleshooting-dialog/troubleshooting-dialog.component.ts
+++ libs/logging-angular/src/troubleshooting-dialog/troubleshooting-dialog.component.ts
@@ -1,23 +1,39 @@
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 
 import { FileDownloadService } from "@bitwarden/common/platform/abstractions/file-download/file-download.service";
-import { ButtonModule, LinkModule, TypographyModule } from "@bitwarden/components";
+import {
+  ButtonModule,
+  CenterPositionStrategy,
+  DialogModule,
+  DialogRef,
+  DialogService,
+  IconModule,
+  LinkModule,
+} from "@bitwarden/components";
 import { buildFlightRecorderCsvExport } from "@bitwarden/logging";
-import { FlightRecorderService } from "@bitwarden/logging-angular";
 import { I18nPipe } from "@bitwarden/ui-common";
 
+import { FlightRecorderService } from "../flight-recorder.service";
+
 @Component({
-  selector: "app-download-event-logs",
-  templateUrl: "download-event-logs.component.html",
-  imports: [ButtonModule, LinkModule, TypographyModule, I18nPipe],
+  templateUrl: "troubleshooting-dialog.component.html",
+  imports: [DialogModule, ButtonModule, IconModule, LinkModule, I18nPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DownloadEventLogsComponent {
+export class TroubleshootingDialogComponent {
+  private readonly dialogRef = inject(DialogRef);
   private readonly flightRecorder = inject(FlightRecorderService);
   private readonly fileDownloadService = inject(FileDownloadService);
 
   protected readonly download = async () => {
     const events = await this.flightRecorder.read();
     this.fileDownloadService.download(buildFlightRecorderCsvExport(events));
+    await this.dialogRef.close();
   };
+
+  static open(dialogService: DialogService) {
+    return dialogService.open(TroubleshootingDialogComponent, {
+      positionStrategy: new CenterPositionStrategy(),
+    });
+  }
 }
