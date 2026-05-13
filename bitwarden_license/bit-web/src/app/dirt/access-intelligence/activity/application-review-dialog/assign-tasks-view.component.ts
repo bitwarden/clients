0ec800a9084c5a4091e9e5b3351f725ecfccfbc5
voommen-livefront <voommen@livefront.com>
2026-05-13 14:08:18 -0500

    PM-35134 remove lowercase and use a string instead

--- bitwarden_license/bit-web/src/app/dirt/access-intelligence/activity/application-review-dialog/assign-tasks-view.component.ts
+++ bitwarden_license/bit-web/src/app/dirt/access-intelligence/activity/application-review-dialog/assign-tasks-view.component.ts
@@ -1,4 +1,3 @@
-import { LowerCasePipe } from "@angular/common";
 import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 
 import {
@@ -25,7 +24,6 @@ import { DarkImageSourceDirective } from "@bitwarden/vault";
   selector: "dirt-assign-tasks-view",
   templateUrl: "./assign-tasks-view.component.html",
   imports: [
-    LowerCasePipe,
     ButtonModule,
     TypographyModule,
     I18nPipe,
