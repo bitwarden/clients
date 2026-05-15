e445c80466487c9b5dc22828157b8fe50cedcca0
rr-bw <102181210+rr-bw@users.noreply.github.com>
2026-05-15 01:39:21 -0700

    update KdfRequest

--- libs/common/src/models/request/kdf.request.ts
+++ libs/common/src/models/request/kdf.request.ts
@@ -1,19 +1,14 @@
-import { PasswordRequest } from "../../auth/models/request/password.request";
+import { SecretVerificationRequest } from "@bitwarden/common/auth/models/request/secret-verification.request";
 import {
   MasterPasswordAuthenticationData,
   MasterPasswordUnlockData,
-} from "../../key-management/master-password/types/master-password.types";
+} from "@bitwarden/common/key-management/master-password/types/master-password.types";
 
-export class KdfRequest extends PasswordRequest {
+export class KdfRequest extends SecretVerificationRequest {
   constructor(
-    authenticationData: MasterPasswordAuthenticationData,
-    unlockData: MasterPasswordUnlockData,
+    readonly authenticationData: MasterPasswordAuthenticationData,
+    readonly unlockData: MasterPasswordUnlockData,
   ) {
     super();
-    // Note, this init code should be in the super constructor, once PasswordRequest's constructor is updated.
-    this.newMasterPasswordHash = authenticationData.masterPasswordAuthenticationHash;
-    this.key = unlockData.masterKeyWrappedUserKey;
-    this.authenticationData = authenticationData;
-    this.unlockData = unlockData;
   }
 }
