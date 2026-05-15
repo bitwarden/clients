a9987d6711b804a4a16c1a35ac14371a6577002c
rr-bw <102181210+rr-bw@users.noreply.github.com>
2026-05-15 02:01:44 -0700

    update test setup

--- libs/angular/src/auth/password-management/change-password/default-change-password.service.spec.ts
+++ libs/angular/src/auth/password-management/change-password/default-change-password.service.spec.ts
@@ -150,12 +150,12 @@ describe("DefaultChangePasswordService", () => {
             "currentMasterPasswordAuthenticationHash" as MasterPasswordAuthenticationHash,
         };
 
-        request = PasswordRequest.newConstructor(
-          currentAuthenticationData.masterPasswordAuthenticationHash,
+        request = new PasswordRequest(
           newAuthenticationData,
           newUnlockData,
           passwordInputResult.newPasswordHint!,
         );
+        request.authenticateWith(currentAuthenticationData);
 
         masterPasswordService.makeMasterPasswordAuthenticationData
           .mockResolvedValueOnce(currentAuthenticationData) // first call: current auth data
