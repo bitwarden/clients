60c11ea0bc3ecb9cecfc49695bfea6936416f32b
Rui Tome <rtome@bitwarden.com>
2026-05-18 14:27:45 +0100

    Add getStatus API method for public invite link status endpoint

---
+++ libs/organization-invite-link/src/models/responses/organization-invite-link-status.response.ts
@@ -0,0 +1,28 @@
+import { BaseResponse } from "@bitwarden/common/models/response/base.response";
+
+export class OrganizationInviteLinkSsoResponseModel extends BaseResponse {
+  orgSsoId: string;
+  required: boolean;
+
+  constructor(response: any) {
+    super(response);
+    this.orgSsoId = this.getResponseProperty("OrgSsoId");
+    this.required = this.getResponseProperty("Required");
+  }
+}
+
+export class OrganizationInviteLinkStatusResponseModel extends BaseResponse {
+  organizationId: string;
+  organizationName: string;
+  seatsAvailable: boolean;
+  sso: OrganizationInviteLinkSsoResponseModel | null;
+
+  constructor(response: any) {
+    super(response);
+    this.organizationId = this.getResponseProperty("OrganizationId");
+    this.organizationName = this.getResponseProperty("OrganizationName");
+    this.seatsAvailable = this.getResponseProperty("SeatsAvailable");
+    const sso = this.getResponseProperty("Sso");
+    this.sso = sso == null ? null : new OrganizationInviteLinkSsoResponseModel(sso);
+  }
+}
