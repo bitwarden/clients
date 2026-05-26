009065f73917779f05a0c5c9c32f968511e0d836
Bryan Cunningham <bryan.cunningham@me.com>
2026-05-26 13:07:11 -0400

    update imports

--- apps/browser/src/autofill/notification/bootstrap-bar.ts
+++ apps/browser/src/autofill/notification/bootstrap-bar.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require("./bar.scss");
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require("./bar");
+
+import "./bar";
