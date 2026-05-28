1ff698ef8d7909f6ddc8e8ea7f864a12c592a9ad
Bernd Schoolmann <bernd@schoolmann.dev>
2026-05-28 16:57:35 +0900

    Add error log

--- libs/common/src/platform/ipc/noop-ipc.service.ts
+++ libs/common/src/platform/ipc/noop-ipc.service.ts
@@ -26,6 +26,7 @@ export class NoopIpcService extends IpcService {
 
       const communicationBackend = new IpcCommunicationBackend({
         send: async (_message: OutgoingMessage): Promise<void> => {
+          this.logService.info("[IPC] Noop transport dropping message", _message);
           // Drop the message. No transport is available in this environment.
         },
       });
