a32b2277cd5a168ae4556cc753d23a34fa3e3c0b
Nick Krantz <nick@livefront.com>
2026-06-01 11:32:14 -0500

    add initial batch bar for password manager vault

---
+++ apps/web/src/app/vault/individual-vault/bulk-action-dialogs/bulk-delete-dialog-web.adapter.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from "@angular/core";
+import { lastValueFrom } from "rxjs";
+
+import { DialogService } from "@bitwarden/components";
+import {
+  BulkDeleteDialogParams,
+  BulkDeleteDialogRef,
+  BulkDeleteDialogResult,
+} from "@bitwarden/vault";
+
+import { openBulkDeleteDialog } from "./bulk-delete-dialog/bulk-delete-dialog.component";
+
+@Injectable()
+export class BulkDeleteDialogWebAdapter implements BulkDeleteDialogRef {
+  private readonly dialogService = inject(DialogService);
+
+  async open(params: BulkDeleteDialogParams): Promise<BulkDeleteDialogResult> {
+    const dialog = openBulkDeleteDialog(this.dialogService, { data: params });
+    return lastValueFrom(dialog.closed);
+  }
+}
