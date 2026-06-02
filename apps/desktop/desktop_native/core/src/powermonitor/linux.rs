b9d2d5ce9d9239d9379ebb3c819e28c9c8bb3a03
Zhiwei Liang <zhiwei.liang@zliang.me>
2026-06-02 11:30:05 -0400

    Change timeout back to 2 seconds

--- apps/desktop/desktop_native/core/src/powermonitor/linux.rs
+++ apps/desktop/desktop_native/core/src/powermonitor/linux.rs
@@ -45,7 +45,7 @@ pub async fn on_lock(tx: tokio::sync::mpsc::Sender<()>) -> Result<(), Box<dyn st
 
 async fn lock_monitor_connection() -> zbus::Result<Connection> {
     Builder::session()?
-        .method_timeout(Duration::from_secs(25))
+        .method_timeout(Duration::from_secs(2))
         .build()
         .await
 }
