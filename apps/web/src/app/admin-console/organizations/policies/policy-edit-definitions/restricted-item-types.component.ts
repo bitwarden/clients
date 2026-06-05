9527871e240cfaa6f55c922dccd65bb5dd294f28
JaredScar <thewolfbadger@gmail.com>
2026-06-05 17:26:42 -0400

    Enhance RestrictedItemTypesPolicy with new description and feature flag support.
    - Added descriptionV2 for improved clarity on policy implications.
    - Introduced feature flag handling for the SimpleTogglePolicyComponent.
    - Updated imports to include ReactiveFormsModule, FormFieldModule, and I18nPipe for enhanced functionality.

--- apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/restricted-item-types.component.ts
+++ apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/restricted-item-types.component.ts
@@ -1,24 +1,34 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { PolicyType } from "@bitwarden/common/admin-console/enums";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { FormFieldModule } from "@bitwarden/components";
+import { I18nPipe } from "@bitwarden/ui-common";
 
-import { SharedModule } from "../../../../shared";
 import { BasePolicyEditDefinition, BasePolicyEditComponent } from "../base-policy-edit.component";
 import { PolicyCategory } from "../pipes/policy-category";
+
+import { SimpleTogglePolicyComponent } from "./simple-toggle-policy.component";
 
 export class RestrictedItemTypesPolicy extends BasePolicyEditDefinition {
   name = "restrictedItemTypePolicy";
   description = "restrictedItemTypePolicyDesc";
+  descriptionV2 = "restrictedItemTypePolicyDescV2";
   type = PolicyType.RestrictedItemTypes;
   category = PolicyCategory.VaultManagement;
   priority = 50;
   component = RestrictedItemTypesPolicyComponent;
+  flaggedComponent = {
+    flag: FeatureFlag.PolicyDrawers,
+    component: SimpleTogglePolicyComponent,
+  };
 }
 
 @Component({
   selector: "restricted-item-types-policy-edit",
   templateUrl: "restricted-item-types.component.html",
-  imports: [SharedModule],
+  imports: [ReactiveFormsModule, FormFieldModule, I18nPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RestrictedItemTypesPolicyComponent extends BasePolicyEditComponent {
