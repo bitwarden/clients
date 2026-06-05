a56c29124abe33060d732da4b5914df37739bdba
JaredScar <thewolfbadger@gmail.com>
2026-06-05 14:20:39 -0400

    Enhance RemoveUnlockWithPinPolicy with new description and feature flag support
    - Added `descriptionV2` to provide a clearer explanation of the policy's functionality.
    - Introduced `flaggedComponent` to allow for alternative rendering based on feature flags, enhancing flexibility in policy management.

--- apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/remove-unlock-with-pin.component.ts
+++ apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/remove-unlock-with-pin.component.ts
@@ -1,18 +1,26 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 import { PolicyType } from "@bitwarden/common/admin-console/enums";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
 
 import { SharedModule } from "../../../../shared";
 import { BasePolicyEditDefinition, BasePolicyEditComponent } from "../base-policy-edit.component";
 import { PolicyCategory } from "../pipes/policy-category";
 
+import { SimpleTogglePolicyComponent } from "./simple-toggle-policy.component";
+
 export class RemoveUnlockWithPinPolicy extends BasePolicyEditDefinition {
   name = "removeUnlockWithPinPolicyTitle";
   description = "removeUnlockWithPinPolicyDesc";
+  descriptionV2 = "removeUnlockWithPinPolicyDescV2";
   type = PolicyType.RemoveUnlockWithPin;
   category = PolicyCategory.Authentication;
   priority = 80;
   component = RemoveUnlockWithPinPolicyComponent;
+  flaggedComponent = {
+    flag: FeatureFlag.PolicyDrawers,
+    component: SimpleTogglePolicyComponent,
+  };
 }
 
 @Component({
