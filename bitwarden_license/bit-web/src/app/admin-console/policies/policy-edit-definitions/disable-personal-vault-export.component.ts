e8f83dbbdb44caae4bf0278f92dfda2969213cb5
JaredScar <thewolfbadger@gmail.com>
2026-06-05 14:38:16 -0400

    Refactor DisablePersonalVaultExportPolicyComponent to utilize reactive forms and enhance modularity
    - Replaced `SharedModule` with specific imports for `ReactiveFormsModule`, `CheckboxModule`, `FormFieldModule`, and `I18nPipe` to improve component modularity and maintainability.
    - Updated the component's imports to align with best practices for Angular reactive forms.

--- bitwarden_license/bit-web/src/app/admin-console/policies/policy-edit-definitions/disable-personal-vault-export.component.ts
+++ bitwarden_license/bit-web/src/app/admin-console/policies/policy-edit-definitions/disable-personal-vault-export.component.ts
@@ -1,14 +1,16 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { PolicyType } from "@bitwarden/common/admin-console/enums";
 import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { CheckboxModule, FormFieldModule } from "@bitwarden/components";
+import { I18nPipe } from "@bitwarden/ui-common";
 import {
   BasePolicyEditDefinition,
   BasePolicyEditComponent,
   PolicyCategory,
 } from "@bitwarden/web-vault/app/admin-console/organizations/policies";
 import { SimpleTogglePolicyComponent } from "@bitwarden/web-vault/app/admin-console/organizations/policies/policy-edit-definitions";
-import { SharedModule } from "@bitwarden/web-vault/app/shared";
 
 export class DisablePersonalVaultExportPolicy extends BasePolicyEditDefinition {
   name = "disableExport";
@@ -27,7 +29,7 @@ export class DisablePersonalVaultExportPolicy extends BasePolicyEditDefinition {
 @Component({
   selector: "disable-personal-vault-export-policy-edit",
   templateUrl: "disable-personal-vault-export.component.html",
-  imports: [SharedModule],
+  imports: [ReactiveFormsModule, CheckboxModule, FormFieldModule, I18nPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DisablePersonalVaultExportPolicyComponent extends BasePolicyEditComponent {}
