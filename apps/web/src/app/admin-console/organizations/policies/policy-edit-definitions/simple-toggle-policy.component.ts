894385afac7afc86f13f5ba75fd09878ae1e3b63
JaredScar <thewolfbadger@gmail.com>
2026-06-09 12:38:36 -0400

    Enhance policy editing components with warning messages
    - Added a warning message in the policy edit drawer for improved user feedback when a warning key is present.
    - Removed deprecated callout from the SimpleTogglePolicyComponent template to streamline the code and align with the new v2 structure.

--- apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/simple-toggle-policy.component.ts
+++ apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/simple-toggle-policy.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 
-import { CalloutComponent, FormFieldModule, SwitchComponent } from "@bitwarden/components";
+import { FormFieldModule, SwitchComponent } from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
 
 import { BasePolicyEditComponent } from "../base-policy-edit.component";
@@ -9,14 +9,11 @@ import { BasePolicyEditComponent } from "../base-policy-edit.component";
 @Component({
   selector: "app-simple-toggle-policy-edit",
   template: `
-    @if (policy()?.warningKey) {
-      <bit-callout type="warning">{{ policy()!.warningKey! | i18n }}</bit-callout>
-    }
     <bit-switch [formControl]="enabled" [reversed]="true">
       <bit-label>{{ "enablePolicy" | i18n }}</bit-label>
     </bit-switch>
   `,
-  imports: [ReactiveFormsModule, CalloutComponent, FormFieldModule, SwitchComponent, I18nPipe],
+  imports: [ReactiveFormsModule, FormFieldModule, SwitchComponent, I18nPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SimpleTogglePolicyComponent extends BasePolicyEditComponent {}
