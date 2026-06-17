ea971c4829f668884ed4612f189b27a799e8029a
Brandon <btreston@bitwarden.com>
2026-06-17 14:25:18 -0400

    clean up

--- apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/reset-password.component.ts
+++ apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/reset-password.component.ts
@@ -7,9 +7,6 @@ import { firstValueFrom, Observable, of } from "rxjs";
 
 import { PolicyType } from "@bitwarden/common/admin-console/enums";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
-import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
-import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
-import { getUserId } from "@bitwarden/common/auth/services/account.service";
 
 import { SharedModule } from "../../../../shared";
 import { BasePolicyEditDefinition, BasePolicyEditComponent } from "../base-policy-edit.component";
