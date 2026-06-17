466a860ac2523050c1117161fbe02cb11b93c283
Brandon <btreston@bitwarden.com>
2026-06-17 15:44:56 -0400

    clean up

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -135,7 +135,7 @@ export const DefaultFeatureFlagValue = {
   [FeatureFlag.BulkAutoConfirmOnLogin]: FALSE,
   [FeatureFlag.GenerateInviteLink]: FALSE,
   [FeatureFlag.PM35153CollectionSdkDecryption]: FALSE,
-  [FeatureFlag.PolicyDrawers]: true,
+  [FeatureFlag.PolicyDrawers]: FALSE,
   [FeatureFlag.PM36859RefactorOrgCollectionsVaultComponent]: FALSE,
   [FeatureFlag.PoliciesInAcceptedState]: FALSE,
 
