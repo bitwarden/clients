e0d4dc512a1e54d3eff22b65ced8b3c972bab384
Jackson Engstrom <jengstrom@bitwarden.com>
2026-06-17 16:34:33 -0700

    fix tests

--- libs/tools/send/send-ui/src/new-send-dropdown-v2/new-send-dropdown-v2.component.spec.ts
+++ libs/tools/send/send-ui/src/new-send-dropdown-v2/new-send-dropdown-v2.component.spec.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, of } from "rxjs";
 
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { BillingAccountProfileStateService } from "@bitwarden/common/billing/abstractions";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { SendType } from "@bitwarden/common/tools/send/types/send-type";
 import { PremiumUpgradePromptService } from "@bitwarden/common/vault/abstractions/premium-upgrade-prompt.service";
@@ -36,6 +37,7 @@ describe("NewSendDropdownV2Component", () => {
         { provide: AccountService, useValue: accountService },
         { provide: PremiumUpgradePromptService, useValue: premiumUpgradeService },
         { provide: I18nService, useValue: i18nService },
+        { provide: ConfigService, useValue: { getFeatureFlag$: () => of(false) } },
       ],
     }).compileComponents();
 
