990e11c8fdbb4df6ca55c14674cfb69802bb133c
Mike Amirault <mamirault@bitwarden.com>
2026-06-18 10:02:10 -0400

    Fix browser builds

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1313,6 +1313,7 @@ export default class MainBackground {
       this.keyGenerationService,
       this.accountService,
       this.restrictedItemTypesService,
+      this.configService,
     );
 
     this.individualVaultExportService = new IndividualVaultExportService(
