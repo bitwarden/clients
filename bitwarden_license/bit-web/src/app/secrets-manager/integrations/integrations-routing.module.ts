d7ad0f9bb8a53937248395d17925c5b0234d7451
maxkpower <mpower@bitwarden.com>
2026-06-23 15:50:22 +0200

    Convert Secrets Manager integrations page to tabbed layout

--- bitwarden_license/bit-web/src/app/secrets-manager/integrations/integrations-routing.module.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/integrations/integrations-routing.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { IntegrationType } from "@bitwarden/common/enums";
+
 import { IntegrationsComponent } from "./integrations.component";
+import {
+  SmIntegrationsTabComponent,
+  SmIntegrationsTabData,
+} from "./sm-integrations-tab/sm-integrations-tab.component";
+
+const integrationsTabData: SmIntegrationsTabData = {
+  integrationType: IntegrationType.Integration,
+  descriptionKey: "integrationsDesc",
+  tooltipKey: "smIntegrationTooltip",
+  ariaKey: "smIntegrationCardAriaLabel",
+};
+
+const sdksTabData: SmIntegrationsTabData = {
+  integrationType: IntegrationType.SDK,
+  descriptionKey: "sdksDesc",
+  tooltipKey: "smSdkTooltip",
+  ariaKey: "smSdkAriaLabel",
+};
 
 const routes: Routes = [
   {
     path: "",
     component: IntegrationsComponent,
+    children: [
+      { path: "", pathMatch: "full", redirectTo: "integrations" },
+      { path: "integrations", component: SmIntegrationsTabComponent, data: integrationsTabData },
+      { path: "sdks", component: SmIntegrationsTabComponent, data: sdksTabData },
+    ],
   },
 ];
 
