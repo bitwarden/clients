0523f8127aacd8519500a2144dba76f6efd4382b
Nick Krantz <nick@livefront.com>
2026-06-24 10:35:00 -0500

    update deletion dialog for desktop

--- apps/desktop/src/vault/app/vault-v3/bulk-action-dialogs/bulk-delete-dialog-desktop.component.ts
+++ apps/desktop/src/vault/app/vault-v3/bulk-action-dialogs/bulk-delete-dialog-desktop.component.ts
@@ -2,15 +2,10 @@ import { DIALOG_DATA, DialogRef } from "@angular/cdk/dialog";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { firstValueFrom } from "rxjs";
 
-import { CollectionService } from "@bitwarden/admin-console/common";
-import { ApiService } from "@bitwarden/common/abstractions/api.service";
-import { CollectionView } from "@bitwarden/common/admin-console/models/collections";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { getUserId } from "@bitwarden/common/auth/services/account.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
-import { SyncService } from "@bitwarden/common/platform/sync";
-import { CollectionId } from "@bitwarden/common/types/guid";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 import {
   AsyncActionsModule,
@@ -19,6 +14,7 @@ import {
   DialogConfig,
   DialogModule,
   DialogService,
+  IconModule,
   ToastService,
 } from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
@@ -28,24 +24,20 @@ import { BulkDeleteDialogParams, BulkDeleteDialogResult } from "@bitwarden/vault
   standalone: true,
   templateUrl: "bulk-delete-dialog-desktop.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [DialogModule, ButtonModule, I18nPipe, AsyncActionsModule],
+  imports: [DialogModule, ButtonModule, I18nPipe, AsyncActionsModule, IconModule],
 })
 export class BulkDeleteDialogDesktopComponent {
   private readonly dialogData = inject<BulkDeleteDialogParams>(DIALOG_DATA);
   private readonly dialogRef = inject<DialogRef<BulkDeleteDialogResult>>(DialogRef);
   private readonly cipherService = inject(CipherService);
   private readonly i18nService = inject(I18nService);
-  private readonly apiService = inject(ApiService);
-  private readonly collectionService = inject(CollectionService);
   private readonly toastService = inject(ToastService);
   private readonly accountService = inject(AccountService);
-  private readonly syncService = inject(SyncService);
 
   protected readonly cipherIds: string[];
   protected readonly permanent: boolean;
   protected readonly organization: Organization | undefined;
   protected readonly organizations: Organization[] | undefined;
-  protected readonly collections: CollectionView[];
   protected readonly unassignedCiphers: string[];
 
   constructor() {
@@ -53,8 +45,11 @@ export class BulkDeleteDialogDesktopComponent {
     this.permanent = this.dialogData.permanent ?? false;
     this.organization = this.dialogData.organization;
     this.organizations = this.dialogData.organizations;
-    this.collections = this.dialogData.collections ?? [];
     this.unassignedCiphers = this.dialogData.unassignedCiphers ?? [];
+  }
+
+  protected get totalCiphersCount(): number {
+    return this.cipherIds.length + this.unassignedCiphers.length;
   }
 
   protected async cancel() {
@@ -74,27 +69,13 @@ export class BulkDeleteDialogDesktopComponent {
         deletePromises.push(this.deleteCiphersAdmin(this.cipherIds));
       }
     }
-    if (this.collections.length) {
-      deletePromises.push(this.deleteCollections());
-    }
 
     await Promise.all(deletePromises);
 
-    if (this.cipherIds.length || this.unassignedCiphers.length) {
+    if (this.totalCiphersCount) {
       this.toastService.showToast({
         variant: "success",
         message: this.i18nService.t(this.permanent ? "permanentlyDeletedItems" : "deletedItems"),
-      });
-    }
-    if (this.collections.length) {
-      const userId = await firstValueFrom(this.accountService.activeAccount$.pipe(getUserId));
-      await this.collectionService.delete(
-        this.collections.map((c) => c.id as CollectionId),
-        userId,
-      );
-      this.toastService.showToast({
-        variant: "success",
-        message: this.i18nService.t("deletedCollections"),
       });
     }
     this.close(BulkDeleteDialogResult.Deleted);
@@ -122,51 +103,6 @@ export class BulkDeleteDialogDesktopComponent {
 
         this.organization!.id,
       );
-    }
-  }
-
-  private async deleteCollections(): Promise<void> {
-    if (!this.collections.length) {
-      return;
-    }
-
-    const fullSync = async () => {
-      await this.syncService.fullSync(true);
-    };
-
-    if (this.organization) {
-      if (this.collections.some((c) => !c.canDelete(this.organization))) {
-        this.toastService.showToast({
-          variant: "error",
-          title: this.i18nService.t("errorOccurred"),
-          message: this.i18nService.t("missingPermissions"),
-        });
-        return;
-      }
-      await this.apiService.deleteManyCollections(
-        this.organization.id,
-        this.collections.map((c) => c.id),
-      );
-      await fullSync();
-    } else if (this.organizations) {
-      const deletePromises: Promise<unknown>[] = [];
-      for (const organization of this.organizations) {
-        const orgCollections = this.collections.filter((c) => c.organizationId === organization.id);
-        if (orgCollections.some((c) => !c.canDelete(organization))) {
-          this.toastService.showToast({
-            variant: "error",
-            title: this.i18nService.t("errorOccurred"),
-            message: this.i18nService.t("missingPermissions"),
-          });
-          return;
-        }
-        const orgCollectionIds = orgCollections.map((c) => c.id);
-        deletePromises.push(
-          this.apiService.deleteManyCollections(organization.id, orgCollectionIds),
-        );
-      }
-      await Promise.all(deletePromises);
-      await fullSync();
     }
   }
 
