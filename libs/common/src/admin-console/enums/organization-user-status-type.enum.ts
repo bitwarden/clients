4c933e572cf0dc10ba6bdd842542faed87fcc5b2
Sven <svernyi@bitwarden.com>
2026-06-25 10:12:35 -0500

    yeets client-owned OrganizationUserStatusType

--- libs/common/src/admin-console/enums/organization-user-status-type.enum.ts
+++ libs/common/src/admin-console/enums/organization-user-status-type.enum.ts
@@ -1,9 +1,4 @@
-// FIXME: update to use a const object instead of a typescript enum
-// eslint-disable-next-line @bitwarden/platform/no-enums
-export enum OrganizationUserStatusType {
-  Invited = 0,
-  Accepted = 1,
-  Confirmed = 2,
-  Staged = 3,
-  Revoked = -1,
-}
+import { OrganizationUserStatusType as SdkOrganizationUserStatusType } from "@bitwarden/sdk-internal";
+
+// Re-export the SdkOrganizationUserStatusType so we have a single source of truth
+export { SdkOrganizationUserStatusType as OrganizationUserStatusType };
