4dcf7288333f42f20a25c4596ec8d16a80b13a91
Stephon Brown <sbrown@livefront.com>
2026-07-09 11:30:16 -0400

    refactor(common): reorder InitiationPath import

--- libs/common/src/models/request/reference-event.request.ts
+++ libs/common/src/models/request/reference-event.request.ts
@@ -1,6 +1,7 @@
-import { InitiationPath } from "../../billing/enums";
 // FIXME: Update this file to be type safe and remove this and next line
 // @ts-strict-ignore
+import { InitiationPath } from "../../billing/enums";
+
 export class ReferenceEventRequest {
   id: string;
   session: string;
