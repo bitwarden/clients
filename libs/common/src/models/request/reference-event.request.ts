0085e2dc374f9c04485e5b934e3fb61d3d7f22ff
Stephon Brown <sbrown@livefront.com>
2026-07-09 11:18:11 -0400

    chore(common): Ignore strict type errors in ReferenceEventRequest

--- libs/common/src/models/request/reference-event.request.ts
+++ libs/common/src/models/request/reference-event.request.ts
@@ -1,5 +1,6 @@
 import { InitiationPath } from "../../billing/enums";
-
+// FIXME: Update this file to be type safe and remove this and next line
+// @ts-strict-ignore
 export class ReferenceEventRequest {
   id: string;
   session: string;
