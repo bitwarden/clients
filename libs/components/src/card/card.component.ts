49fcddaaa4ac263e3cd3219608716cc1326bbe88
Bryan Cunningham <bryan.cunningham@me.com>
2026-07-16 13:03:04 -0400

    Document bit-card usage and when to use bit-card-segmented

--- libs/components/src/card/card.component.ts
+++ libs/components/src/card/card.component.ts
@@ -2,6 +2,13 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 import { BaseCardDirective } from "./base-card/base-card.directive";
 
+/**
+ * A basic bordered container with assumed padding. Use for grouping related content into a single
+ * block.
+ *
+ * Reach for `bit-card-segmented` instead when the content is multiple rows or sections separated by
+ * full-width dividers.
+ */
 @Component({
   selector: "bit-card",
   template: `<ng-content></ng-content>`,
