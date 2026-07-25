87c8bba31a597953a2adbf3f076d8ef6b07a5798
Colton Hurst <colton@coltonhurst.com>
2026-07-25 10:17:05 -0400

    [PM-40679] Rename the main process Autotype listener registration fn

--- apps/desktop/src/autofill/main/main-desktop-autotype.service.ts
+++ apps/desktop/src/autofill/main/main-desktop-autotype.service.ts
@@ -20,10 +20,10 @@ export class MainDesktopAutotypeService {
   ) {
     this.autotypeKeyboardShortcut = new AutotypeKeyboardShortcut();
 
-    this.registerIpcListeners();
+    this.registerMvpIpcListeners();
   }
 
-  registerIpcListeners() {
+  registerMvpIpcListeners() {
     ipcMain.on(AUTOTYPE_MVP_IPC_CHANNELS.TOGGLE, (_event, enable: boolean) => {
       if (enable) {
         this.enableAutotype();
