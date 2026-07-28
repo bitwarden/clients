ca43b20b0079349e54078e5572a5d658c7dd91e1
Brandon <btreston@bitwarden.com>
2026-07-28 16:13:35 -0400

    fix lint

--- bitwarden_license/bit-web/src/app/dirt/reports/member-access-report/member-access-report.component.ts
+++ bitwarden_license/bit-web/src/app/dirt/reports/member-access-report/member-access-report.component.ts
@@ -29,8 +29,8 @@ import {
   ToastService,
 } from "@bitwarden/components";
 import { KeyService } from "@bitwarden/key-management";
-import { ExportHelper } from "@bitwarden/vault-export-core";
 import { Vfo1I18nPipe } from "@bitwarden/vault";
+import { ExportHelper } from "@bitwarden/vault-export-core";
 import {
   CoreOrganizationModule,
   GroupApiService,
@@ -55,7 +55,14 @@ import { MemberAccessReportView } from "./view/member-access-report.view";
 @Component({
   selector: "member-access-report",
   templateUrl: "member-access-report.component.html",
-  imports: [SharedModule, SearchModule, HeaderModule, CoreOrganizationModule, IconModule, Vfo1I18nPipe],
+  imports: [
+    SharedModule,
+    SearchModule,
+    HeaderModule,
+    CoreOrganizationModule,
+    IconModule,
+    Vfo1I18nPipe,
+  ],
   providers: [
     safeProvider({
       provide: MemberAccessReportServiceAbstraction,
