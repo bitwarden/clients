4fbf3d0723252c37afbae76654af2701570013ec
Brandon <btreston@bitwarden.com>
2026-07-28 15:21:42 -0400

    fix tests

--- apps/web/src/app/admin-console/organizations/members/services/member-dialog-manager/member-dialog-manager.service.spec.ts
+++ apps/web/src/app/admin-console/organizations/members/services/member-dialog-manager/member-dialog-manager.service.spec.ts
@@ -163,6 +163,7 @@ describe("MemberDialogManagerService", () => {
           data: {
             kind: "Edit",
             name: "Test User",
+            profileName: "Test User",
             email: "test@example.com",
             organizationId: mockOrganization.id,
             organizationUserId: mockUser.id,
