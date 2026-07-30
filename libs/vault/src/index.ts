adb42c0411324ea8a78108bcdb9a23ff4e9ec86d
Jackson Engstrom <jengstrom@bitwarden.com>
2026-07-30 13:51:42 -0700

    add show-quick-copy-actions-details-popover component

--- libs/vault/src/index.ts
+++ libs/vault/src/index.ts
@@ -40,6 +40,7 @@ export * from "./components/add-item-dialog/add-item-dialog.component";
 export * from "./components/carousel";
 export * from "./components/new-cipher-menu/new-cipher-menu.component";
 export * from "./components/permit-cipher-details-popover/permit-cipher-details-popover.component";
+export * from "./components/show-quick-copy-actions-details-popover/show-quick-copy-actions-details-popover.component";
 export * from "./components/vault-items-transfer";
 export { VaultItem, compareVaultItems } from "./components/vault-item";
 export { VaultOrganizationUserNotificationsComponent } from "./components/vault-organization-user-notifications/vault-organization-user-notifications.component";
