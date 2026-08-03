465bbf05752d93a4f2dc4dd3d883b529c81a4f07
Brad Deibert <bdeibert@bitwarden.com>
2026-08-03 12:55:02 -0700

    update copy to i18n

--- apps/browser/src/dirt/health/popup/health-intro.component.ts
+++ apps/browser/src/dirt/health/popup/health-intro.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, output } from "@angular/core";
 
 import { TypographyModule, ButtonModule } from "@bitwarden/components";
+import { I18nPipe } from "@bitwarden/ui-common";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "health-intro",
   templateUrl: "./health-intro.component.html",
   standalone: true,
-  imports: [ButtonModule, TypographyModule],
+  imports: [ButtonModule, TypographyModule, I18nPipe],
 })
 export class HealthIntroComponent {
   readonly onTriggerHealthScan = output<void>();
