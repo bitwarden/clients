61b32a8cc1e542cc3d8f404e611aac31e73687f1
Brad Deibert <bdeibert@bitwarden.com>
2026-08-06 16:27:03 -0700

    localize text

--- bitwarden_license/bit-browser/src/popup/dirt/health/health-delete-at-risk-item-dialog.component.ts
+++ bitwarden_license/bit-browser/src/popup/dirt/health/health-delete-at-risk-item-dialog.component.ts
@@ -3,6 +3,7 @@ import { Component, ChangeDetectionStrategy, input, inject } from "@angular/core
 import { firstValueFrom } from "rxjs";
 
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 import type { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 import {
@@ -15,6 +16,7 @@ import {
   IconTileComponent,
   CardComponent,
 } from "@bitwarden/components";
+import { I18nPipe } from "@bitwarden/ui-common";
 
 export interface HealthDeleteAtRiskItemDialogData {
   item: CipherView;
@@ -31,12 +33,14 @@ export interface HealthDeleteAtRiskItemDialogData {
     SectionHeaderComponent,
     IconTileComponent,
     CardComponent,
+    I18nPipe,
   ],
 })
 export class HealthDeleteAtRiskItemDialogComponent {
   readonly accountService = inject(AccountService);
   readonly cipherService = inject(CipherService);
   readonly toastService = inject(ToastService);
+  readonly i18nService = inject(I18nService);
   readonly dialogRef = inject(DialogRef);
   readonly inputData = inject<HealthDeleteAtRiskItemDialogData>(DIALOG_DATA);
 
@@ -51,7 +55,7 @@ export class HealthDeleteAtRiskItemDialogComponent {
     await this.cipherService.softDeleteWithServer(this.item().id, user.id);
 
     this.toastService.showToast({
-      message: "Item deleted",
+      message: this.i18nService.t("deletedItem"),
       variant: "success",
     });
 
