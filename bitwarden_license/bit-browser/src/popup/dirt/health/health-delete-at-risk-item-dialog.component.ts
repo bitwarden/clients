126d3ad4e030f68b7bc1c8678b9770fc6e942ed9
Brad Deibert <bdeibert@bitwarden.com>
2026-08-06 15:52:33 -0700

    implement soft delete item

--- bitwarden_license/bit-browser/src/popup/dirt/health/health-delete-at-risk-item-dialog.component.ts
+++ bitwarden_license/bit-browser/src/popup/dirt/health/health-delete-at-risk-item-dialog.component.ts
@@ -1,6 +1,15 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { DIALOG_DATA } from "@angular/cdk/dialog";
+import { Component, ChangeDetectionStrategy, input, inject } from "@angular/core";
+import { firstValueFrom } from "rxjs";
 
-import { DialogModule, ButtonModule } from "@bitwarden/components";
+import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
+import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
+import type { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
+import { DialogModule, ButtonModule, DialogRef, ToastService } from "@bitwarden/components";
+
+export interface HealthDeleteAtRiskItemDialogData {
+  item: CipherView;
+}
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -8,4 +17,28 @@ import { DialogModule, ButtonModule } from "@bitwarden/components";
   templateUrl: "./health-delete-at-risk-item-dialog.component.html",
   imports: [DialogModule, ButtonModule],
 })
-export class HealthDeleteAtRiskItemDialogComponent {}
+export class HealthDeleteAtRiskItemDialogComponent {
+  readonly accountService = inject(AccountService);
+  readonly cipherService = inject(CipherService);
+  readonly toastService = inject(ToastService);
+  readonly dialogRef = inject(DialogRef);
+  readonly inputData = inject<HealthDeleteAtRiskItemDialogData>(DIALOG_DATA);
+
+  readonly item = input<CipherView>(this.inputData.item);
+
+  readonly onDeleteItem = async () => {
+    const user = await firstValueFrom(this.accountService.activeAccount$);
+    if (!user) {
+      return;
+    }
+
+    await this.cipherService.softDeleteWithServer(this.item().id, user.id);
+
+    this.toastService.showToast({
+      message: "Item deleted",
+      variant: "success",
+    });
+
+    await this.dialogRef.close();
+  };
+}
--- bitwarden_license/bit-browser/src/popup/dirt/health/health-risk-category-detail.component.ts
+++ bitwarden_license/bit-browser/src/popup/dirt/health/health-risk-category-detail.component.ts
@@ -119,6 +119,7 @@ export class HealthRiskCategoryDetailComponent {
   readonly onDeleteItem = async (item: CipherView) => {
     await this.dialogService.open(HealthDeleteAtRiskItemDialogComponent, {
       positionStrategy: new CenterPositionStrategy(),
+      data: { item },
     });
   };
 
