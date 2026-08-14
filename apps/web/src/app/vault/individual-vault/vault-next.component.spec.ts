de3a7a56f53400fb75c8b8270da37393c35dbac4
Nik Gilmore <ngilmore@bitwarden.com>
2026-08-14 11:43:36 -0700

    [PM-40395] Add Import button to new vault table, and move import page into modal

--- apps/web/src/app/vault/individual-vault/vault-next.component.spec.ts
+++ apps/web/src/app/vault/individual-vault/vault-next.component.spec.ts
@@ -126,6 +126,7 @@ describe("VaultNextComponent", () => {
       imports: [VaultNextComponent],
       providers: [
         { provide: AccountService, useValue: accountService },
+        { provide: DialogService, useValue: mock<DialogService>() },
         { provide: CipherRowMenuService, useValue: cipherRowMenuService },
         { provide: CipherService, useValue: cipherService },
         { provide: CollectionService, useValue: collectionService },
