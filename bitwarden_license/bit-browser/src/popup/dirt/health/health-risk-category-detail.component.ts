1d55e2797129bbda209b3761a2c1bb6ce1313d0a
Brad Deibert <bdeibert@bitwarden.com>
2026-08-19 16:59:22 -0700

    use correct svg for weak empty state

--- bitwarden_license/bit-browser/src/popup/dirt/health/health-risk-category-detail.component.ts
+++ bitwarden_license/bit-browser/src/popup/dirt/health/health-risk-category-detail.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { map, switchMap } from "rxjs/operators";
 
 import { IconComponent as AppVaultIconComponent } from "@bitwarden/angular/vault/components/icon.component";
-import { NoCredentialsIcon, ReportExposedPasswords, UnlockedIcon } from "@bitwarden/assets/svg";
+import { NoCredentialsIcon, ReportExposedPasswords, LockIcon } from "@bitwarden/assets/svg";
 import { CipherHealthView } from "@bitwarden/bit-common/dirt/access-intelligence/models/view/cipher-health.view";
 import { RiskCategory } from "@bitwarden/bit-common/dirt/vault-health/models";
 import { VaultHealthReportService } from "@bitwarden/bit-common/dirt/vault-health/services";
@@ -161,7 +161,7 @@ export class HealthRiskCategoryDetailComponent {
       titleKey: "weakPasswordsTitle",
       descriptionKey: "weakPasswordsDescription",
       emptyKey: "weakPasswordsEmpty",
-      emptyIcon: UnlockedIcon,
+      emptyIcon: LockIcon,
     },
     [RiskCategory.Reused]: {
       titleKey: "reusedPasswordsTitle",
