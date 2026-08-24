45c4bd1cb02ee1ea30c8d8dc30c3f5ef29ced7ec
Shane <smelton@bitwarden.com>
2026-08-24 12:03:06 -0700

    [PM-412222] Update sdk decryption failure dialog copy and design

--- libs/components/src/utils/index.ts
+++ libs/components/src/utils/index.ts
@@ -3,4 +3,5 @@ export * from "./function-to-observable";
 export * from "./has-scrollable-content";
 export * from "./i18n-mock.service";
 export * from "./query-param-store";
+export * from "./responsive-utils";
 export * from "./state-mock";
