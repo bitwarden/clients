f561913af71520fdaa14a353a3c0025be824fbcd
Daniel James Smith <djsmith85@users.noreply.github.com>
2026-08-25 21:46:29 +0200

    Migrate usage from deprecated UserVerificationComponent to UserVerificationFormInputComponent

--- apps/web/src/app/auth/settings/webauthn-login-settings/webauthn-login-settings.module.ts
+++ apps/web/src/app/auth/settings/webauthn-login-settings/webauthn-login-settings.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+import { UserVerificationFormInputComponent } from "@bitwarden/auth/angular";
 import { CheckboxModule } from "@bitwarden/components";
 
 import { SharedModule } from "../../../shared/shared.module";
-import { UserVerificationModule } from "../../shared/components/user-verification";
 
 import { CreateCredentialDialogComponent } from "./create-credential-dialog/create-credential-dialog.component";
 import { DeleteCredentialDialogComponent } from "./delete-credential-dialog/delete-credential-dialog.component";
@@ -12,7 +12,13 @@ import { EnableEncryptionDialogComponent } from "./enable-encryption-dialog/enab
 import { WebauthnLoginSettingsComponent } from "./webauthn-login-settings.component";
 
 @NgModule({
-  imports: [SharedModule, FormsModule, ReactiveFormsModule, UserVerificationModule, CheckboxModule],
+  imports: [
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UserVerificationFormInputComponent,
+    CheckboxModule,
+  ],
   declarations: [
     WebauthnLoginSettingsComponent,
     CreateCredentialDialogComponent,
