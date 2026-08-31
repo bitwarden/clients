90de7a631c9fe415a8fa62782ce8742f6e8a123f
Stephon Brown <sbrown@livefront.com>
2026-08-31 18:44:10 -0400

    fix(billing): update date format and spacing

--- libs/subscription/src/components/subscription-card/subscription-card.component.ts
+++ libs/subscription/src/components/subscription-card/subscription-card.component.ts
@@ -64,7 +64,7 @@ export class SubscriptionCardComponent {
   private readonly datePipe = inject(DatePipe);
   private readonly i18nService = inject(I18nService);
 
-  protected readonly dateFormat = "MMM. d, y";
+  protected readonly dateFormat = "MMMM d, y";
 
   readonly title = input.required<string>();
 
