803e2df952b5b20b33247847652581f5bca7b612
Bryan Cunningham <bryan.cunningham@me.com>
2026-09-01 13:56:39 -0400

    Add clearSearch to vault stories i18n mock

--- apps/browser/src/vault/popup/components/vault/vault.stories.ts
+++ apps/browser/src/vault/popup/components/vault/vault.stories.ts
@@ -701,6 +701,7 @@ const buildProviders = (args: StoryArgs) => {
           itemCount: "__$1__ items",
           // Table presentation (flag on)
           resetSearch: "Reset search",
+          clearSearch: "Clear by clicking here or pressing Esc.",
           name: "Name",
           autofillSuggestions: "Autofill suggestions",
           itemSuggestions: "Suggested items",
