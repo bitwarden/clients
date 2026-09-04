27f3b210fbce0d552252dfb995abcc916e65f8af
jaasen-livefront <jaasen@livefront.com>
2026-09-04 13:40:10 -0700

    remove unused provider in spec

--- apps/web/src/app/vault/individual-vault/vault-next.component.spec.ts
+++ apps/web/src/app/vault/individual-vault/vault-next.component.spec.ts
@@ -10,7 +10,6 @@ import { PolicyService } from "@bitwarden/common/admin-console/abstractions/poli
 import { CollectionView } from "@bitwarden/common/admin-console/models/collections";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { Account, AccountService } from "@bitwarden/common/auth/abstractions/account.service";
-import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { CollectionId, OrganizationId, UserId } from "@bitwarden/common/types/guid";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
@@ -216,7 +215,6 @@ describe("VaultNextComponent", () => {
         { provide: CipherRowMenuService, useValue: cipherRowMenuService },
         { provide: CipherService, useValue: cipherService },
         { provide: CollectionService, useValue: collectionService },
-        { provide: ConfigService, useValue: { getFeatureFlag$: () => of(true) } },
         { provide: DialogService, useValue: mock<DialogService>() },
         { provide: FolderService, useValue: folderService },
         { provide: I18nService, useValue: i18nService },
@@ -225,13 +223,6 @@ describe("VaultNextComponent", () => {
         { provide: RestrictedItemTypesService, useValue: restrictedItemTypesService },
         { provide: VaultCopyButtonsService, useValue: copyButtonsService },
         { provide: VaultNavService, useValue: { viewModel$: () => vaultNav$ } },
-        {
-          provide: ConfigService,
-          useValue: {
-            ...mock<ConfigService>(),
-            getFeatureFlag$: jest.fn().mockReturnValue(of(false)),
-          },
-        },
       ],
     })
       .overrideComponent(VaultNextComponent, {
