fe20dd9190a72a39647c0e7e6cf5f06898ee0b25
Ike Kottlowski <ikottlowski@bitwarden.com>
2026-09-03 20:58:16 -0400

    pm-40322 pm-40217 feature flag side nav account menu; fix styling gap

--- apps/web/src/app/layouts/web-side-nav.component.ts
+++ apps/web/src/app/layouts/web-side-nav.component.ts
@@ -1,6 +1,9 @@
 import { AsyncPipe } from "@angular/common";
 import { ChangeDetectionStrategy, Component, inject, input } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
 
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { SideNavVariant, NavigationModule, SideNavService } from "@bitwarden/components";
 
 import { UpgradeCalloutComponent } from "../billing/individual/upgrade/upgrade-nav-button/upgrade-callout/upgrade-callout.component";
@@ -29,4 +32,13 @@ export class WebSideNavComponent {
 
   protected readonly shouldShowPremiumUpgradeButton$ =
     this.productSwitcherService.shouldShowPremiumUpgradeButton$;
+
+  /**
+   * Whether the VFO1 Foundation flag is enabled. While it is off, the legacy header account menu
+   * is shown instead (see `WebHeaderComponent`).
+   */
+  protected readonly vfo1FoundationEnabled = toSignal(
+    inject(ConfigService).getFeatureFlag$(FeatureFlag.VFO1Foundation),
+    { initialValue: false },
+  );
 }
