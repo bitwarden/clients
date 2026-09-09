fc6fe3ddc1b0305a815760811da23d47400fdb55
Mike Amirault <mamirault@bitwarden.com>
2026-09-09 15:24:26 -0400

    [PM-43267] Tools share library to support temporary item sharing

--- libs/tools/share/jest.config.js
+++ libs/tools/share/jest.config.js
@@ -1,10 +1,30 @@
-module.exports = {
-  displayName: "tools-share",
-  preset: "../../../jest.preset.js",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
+const { pathsToModuleNameMapper } = require("ts-jest");
+
+const { compilerOptions } = require("../../../tsconfig.base");
+
+const sharedConfig = require("../../shared/jest.config.angular");
+
+// The Angular preset expects this config file to be exactly two levels
+// below the `libs` directory. We need a custom config to override this
+const { createCjsPreset } = require("jest-preset-angular/presets");
+const presetConfig = createCjsPreset({
+  astTransformers: {
+    before: ["<rootDir>/../../../libs/shared/es2020-transformer.ts"],
   },
-  moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../../coverage/libs/tools/share",
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  ...sharedConfig,
+  ...presetConfig,
+  displayName: "tools/share tests",
+  setupFilesAfterEnv: ["<rootDir>/test.setup.ts"],
+  // oauth4webapi is ESM-only; allow jest-preset-angular's transformer to compile it.
+  transformIgnorePatterns: [
+    "node_modules/(?!(.*\\.mjs$|@angular/common/locales/.*\\.js$|oauth4webapi/.*))",
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions?.paths || {}, {
+    prefix: "<rootDir>/../../../",
+  }),
 };
+module.exports = config;
