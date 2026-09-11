5d51576d77b0c0813a16c09aad469dca876efdfb
maxkpower <m.power@bitwarden.com>
2026-09-12 00:51:16 +0200

    Fix PAM system user translation key to use privilegedControls

--- apps/web/src/app/dirt/event-logs/components/organization-events/events.component.ts
+++ apps/web/src/app/dirt/event-logs/components/organization-events/events.component.ts
@@ -60,7 +60,7 @@ const EVENT_SYSTEM_USER_TO_TRANSLATION: Record<EventSystemUser, string> = {
   [EventSystemUser.DomainVerification]: "domainVerification",
   [EventSystemUser.PublicApi]: "publicApi",
   [EventSystemUser.BitwardenPortal]: "system",
-  [EventSystemUser.Pam]: "pam",
+  [EventSystemUser.Pam]: "privilegedControls",
 };
 
 @Component({
