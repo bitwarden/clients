106aef887ef3fd5217aa13cd261aeb5e17648acb
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2026-09-25 12:08:47 -0400

    fixing sort order and ui of people drop down in SM

--- bitwarden_license/bit-web/src/app/secrets-manager/shared/access-policies/access-policy-selector/models/enums/ap-item.enum.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/shared/access-policies/access-policy-selector/models/enums/ap-item.enum.ts
@@ -1,8 +1,8 @@
 // FIXME: update to use a const object instead of a typescript enum
 // eslint-disable-next-line @bitwarden/platform/no-enums
 export enum ApItemEnum {
-  User,
   Group,
+  User,
   ServiceAccount,
   Project,
 }
